5373182628454e842ea3de16c3950901e8fc2278
humbejeff2116 <humbejeff2116@gmail.com>
2021-11-10 14:05:20 +0000

    wrap uploadproductContext provider over App

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import { AuthContextProvider } from '../Context/authContextProvider';
 import { GetStartedContextProvider } from '../Context/gettingStartedContextProvider';
 import { CartContextProvider } from '../Context/Cart/cartContextProvider';
+import { UploadProductContextProvider } from '../Context/UploadProductContext/uploadProductContextProvider';
 import { ViewContextProvider } from '../Context/viewContext/viewContextProvider';
 import AppRoutes from '../Routes/appRoutes';
 
@@ -16,7 +17,9 @@ export default function App() {
         <GetStartedContextProvider>
         <ViewContextProvider>
         <CartContextProvider>
+        <UploadProductContextProvider>
         <AppRoutes/>
+        </UploadProductContextProvider>
         </CartContextProvider>
         </ViewContextProvider>
         </GetStartedContextProvider>
