ce3c2d4f8fcbd3570fb09b6bff805e82f688d05d
humbejeff2116 <humbejeff2116@gmail.com>
2021-12-01 18:15:52 +0000

    wireframe upload product images selector component

---
+++ src/Components/UserDashboard/Store/UploadProductOrService/ProductImagesSelector/productImagesSelector.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductImagesSelector({ handleInputChange, handleSubmit}) {
+    return (
+        <div>
+            product images selector
+        </div>
+    )
+}
