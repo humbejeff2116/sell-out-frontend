ef03bf5903c1c222edccabab4f8ba90c97cf4b37
humbejeff2116 <humbejeff2116@gmail.com>
2021-12-25 10:16:36 +0000

    modify back button logic

--- src/Components/BackButton/backButton.js
+++ src/Components/BackButton/backButton.js
@@ -6,11 +6,18 @@ import { useHistory } from 'react-router-dom'
 
 
 
-export default function BackButton({ buttonWrapperClassName, buttonIcon }) {
+export default function BackButton({ buttonWrapperClassName, buttonIcon, clearSessionStorageWithKey }) {
     const history = useHistory();
+    const goBack = (history, clearSessionStorageWithKey) => {
+        if (clearSessionStorageWithKey) {
+            sessionStorage.removeItem(clearSessionStorageWithKey);
+            return history.goBack();
+        }
+        return history.goBack();
+    }
     return(
         <div className={buttonWrapperClassName} >
-            <i>{ buttonIcon || '' }</i><button onClick={()=>history.goBack()} > go back </button>
+            <i>{ buttonIcon || '' }</i><button onClick={()=> goBack(history, clearSessionStorageWithKey ) } > go back </button>
         </div>      
     )
 }
