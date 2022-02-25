2591babd5bba0f7b75690c0f35da4c57c3c6df12
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-25 20:50:08 +0000

    replace hard coded data with payments made data from API

--- src/Components/UserDashboard/Payments/MadePayments/paymentsMade.js
+++ src/Components/UserDashboard/Payments/MadePayments/paymentsMade.js
@@ -1,58 +1,82 @@
 
-
-
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import useAuth from '../../../../Context/context';
 import useOrder from '../../../../Context/Order/context';
 import socket from '../../../Socket/socket';
 import { getPayments } from '../../../../Utils/http.services';
-import { SellerOrderProfile } from '../../Orders/SoldProducts/soldProducts';
-import { GoKebabHorizontal} from 'react-icons/go';
+import { SellerOrderProfile, BuyerOrderProfile } from '../../Orders/SoldProducts/soldProducts';
+import { GoKebabHorizontal } from 'react-icons/go';
 import './paymentsMade.css';
 
-const paymentsMade = [
-    {
-        productsBoughtFromSeller:[{},{}]
-    }
-]
 
-export default function PaymentsMade(props) {
+export default function PaymentsMade() {
+
     const { user } = useAuth();
+
     const { paymentsMade, setPayments } = useOrder();
+
     let PaymentsMadeComponent;
+
     useEffect(() => {
+
         let mounted = true;
          // TODO... remove useGetUserFunctionality when ready to use functionality
-         let useGetUserFunctionality = false
+         let useGetUserFunctionality = true
+
         const getUserPayments = async (user) => {
+
             try {
+
                 const payments = await getPayments(user);
+
                 setPayments(payments);
+
             }catch(err) {
+
                 console.error(err.stack)
+
             }  
     
         }
+
+        const userData = { user }
+
          // TODO... remove useGetUserFunctionality when ready to use functionality
         if ( (mounted && user && useGetUserFunctionality ) && !paymentsMade ) {
-            getUserPayments(user);
-        }  
+
+            // getUserPayments(user);
+            socket.emit('getUserPayments', userData);
+
+        } 
+
+        socket.on('getUserPaymentsSuccess', function(response) {
+
+            // alert(JSON.stringify(response, null, 2))
+            setPayments(response.data);
+
+        }) 
+
         socket.on('orderDataChange', function() {
+
             if (mounted && user) {
-                getUserPayments(user);
-            }         
+
+                // getUserPayments(user);
+                socket.emit('getUserPayments', userData);
+
+            } 
+
         });
 
         return ()=> {
+
             mounted = false;
+
         }
+
     }, [user, paymentsMade, setPayments]);
 
     if (paymentsMade && paymentsMade?.length > 0) {
+
         PaymentsMadeComponent = (
             
             <PaymentMadeCompWrapper
@@ -60,7 +84,9 @@ export default function PaymentsMade(props) {
             />
 
         )
+
     } else {
+
         PaymentsMadeComponent = (
 
             <NoPaymentsMade/>
@@ -68,7 +94,9 @@ export default function PaymentsMade(props) {
         )
 
     }
+
     return (
+
         <div className="placed-orders-container">
         <div  className="placed-orders-header">
             <h3> Made Payments</h3>
@@ -76,7 +104,7 @@ export default function PaymentsMade(props) {
         <div className="placed-orders-search-container">
             <div className="placed-orders-search">
                 <form>
-                    <label htmlFor="order-search"> Search by seller name or brand </label>
+                    <label htmlFor="order-search"> Search by username or brand </label>
                     <input type="text" />
                 </form>
             </div>
@@ -88,39 +116,50 @@ export default function PaymentsMade(props) {
             </div>
         </div>
         { PaymentsMadeComponent }
-    </div>
+        </div>
+
     )
+
 }
 
 function PaymentMadeCompWrapper({ paymentsMade }) {
-    return (
-        <>
-          {
-           
-           paymentsMade.map((payment, i) =>
-              <PaymentComp
-              key={i}
-              {...payment}
-              paymentProfile={
-                  <SellerOrderProfile usedInPaymentsPage={true}/>
 
-              } 
-              />
-           )
-       }
+    return (
+
+        <>
+        {
+           
+            paymentsMade.map((payment, i) =>
+
+                <PaymentComp key={i} {...payment} useSellerOrderProfile = { true } />
+
+            )
+
+        }
         </>
+
     )
+
 }
 
+
+
 export function PaymentComp(props) {
+
+    let PaymentProfileComp = (
+
+        <PaymentProfile { ...props } />
+
+    );
+
     return (
+
         <div className="placed-order-wrapper">
+
             <div className="placed-order-intro">
                 <div>
-                {
-                    props.paymentProfile
-                }
-                <ViewPaymentDetails />    
+                { PaymentProfileComp }
+                <ViewPaymentDetails { ...props } />    
                 </div>
             </div> 
 
@@ -135,46 +174,101 @@ export function PaymentComp(props) {
         </div>
 
     )
+    
 }
 
-function ViewPaymentDetails(props) {
+function PaymentProfile(props) {
+
+    if (props.useSellerOrderProfile) {
+
+        return (
+
+            <SellerOrderProfile 
+            usedInPaymentsPage = { true } 
+            sellerUserName = { props.sellerName }
+            sellerId = { props.sellerId }
+            />
+
+        )
+
+    } else if (props.useBuyerOrderProfile) {
+
+        return (
+
+            <BuyerOrderProfile 
+            usedInPaymentsPage = { true } 
+            buyerUserName = { props.buyerName }
+            buyerId = { props.buyerId }
+            />
+
+        )
+
+    }
+
+}
+
+function ViewPaymentDetails({
+    orderTime,
+    sellerRecievedPayment,
+    ref,
+    paymentAmount
+}) {
+
     const [showDetails, setShowDetails] = useState(false);
+
+    const orderDate = new Date(Number(orderTime))
+
+    const paymentReleaseStatus = sellerRecievedPayment ? "Released" : "Unreleased"
+
+    const paymentReleaseStatusSpanClass  = sellerRecievedPayment ? "released" : "unreleased"
+
     return (
+
         <div>
             <div className="placed-order-details-group show-more">
             <div className="placed-order-details-group">
-                <p>Order date: <span>wed 04 sep 2021</span></p>
+                <p>Date: <span>{ orderDate.toUTCString() }</span></p>
             </div>
             <div className="sold-products-profile-image-kebab-icon">
-                <GoKebabHorizontal className="nav-icon" onClick={()=>setShowDetails(prevstate => !prevstate)}/>
+                <GoKebabHorizontal className="nav-icon" onClick={ ()=>setShowDetails(prevstate => !prevstate)}/>
                 {/* View more */}
             </div>
             </div>
 
             {
+
                 showDetails && (
+
                     <>
                     <div className="placed-order-details-group">
-                        <p>Order Id: <span>pss12845zf4</span></p>
+                        <p>Ref: <span>{ ref || 'no ref yet' }</span></p>
                     </div>
                     <div className="placed-order-details-group">
-                        <p>Payment date: <span>wed 04 sep 2021</span></p>
+                        <p>Payment Released: <span className= { paymentReleaseStatusSpanClass }>{ paymentReleaseStatus }</span> </p>
                     </div>
                     <div className="placed-order-details-group">
-                        <p>Payment amount: <span className="diff">£68.00</span></p>
+                        <p>Payment Amount: <span className="diff">£{ paymentAmount }</span></p>
                     </div>
                     </>
+
                 )
-            }           
+
+            }   
+
         </div>
+
     )
 
 }
 
 function NoPaymentsMade(props) {
+
     return (
+
         <div>
             no payments made yet
         </div>
+
     )
+
 }
