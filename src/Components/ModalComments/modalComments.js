6c1109d7e35a80cd6be42a9335533d65bbfa0959
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-26 05:16:21 +0000

    refactor modal comments component

--- src/Components/ModalComments/modalComments.js
+++ src/Components/ModalComments/modalComments.js
@@ -1,51 +1,50 @@
 
-
-
-
-
 import React from 'react';
+import { RiCloseFill } from "react-icons/ri";
 import './modalComments.css';
-import {RiCloseFill} from "react-icons/ri";
-import {CommentBox} from '../Product/CommentBox/commentBox';
-
 
 export default function ModalComment({ handleClose, show,  modalDisplayedProduct, commentBox }) {
-    const showHideClassName = show ? "modal display-block" : "modal display-none";
+
     return(
+
         <div className="modal-comment-product-wrapper">
 
+            <div className="modal-comment-product-comment-bttn-wrapper">
+                <RiCloseFill className="nav-icon"  onClick={ handleClose }/>
+            </div>
+             
             <div className="modal-comment-product-container">
-                <div className="modal-comment-product-comment-bttn-container">
-                    <div className="modal-comment-product-comment-bttn-wrapper">
-                    <RiCloseFill className="nav-icon"  onClick={ handleClose }/>
-                    </div>
-                </div>
 
-                <div className="modal-comment-product-comment-panel">
-                { modalDisplayedProduct}
+                <div className="modal-comment-product-comment-panel">                   
+                { modalDisplayedProduct }
                 { commentBox}
-
                 </div>
+
             </div>
 
         </div> 
 
     )
+
 }
 
 
 
-export function ModalBox(props) {
+export function ModalBox({ modalContainerWrapperName, handleModal, modalContainer, children }) {
+   
     return(
-        <div className={props.modalContainerWrapperName}>
-            <div className={props.modalContainer}>
-                <div className="modal-comment-product-comment-bttn-container">
-                    <div className="modal-comment-product-comment-bttn-wrapper">
-                    <RiCloseFill className="nav-icon"  onClick={ props.handleModal }/>
-                    </div>
-                </div>
-                { props.children}
+
+        <div className={ modalContainerWrapperName || "modal-comment-product-wrapper" }>
+             <div className="modal-comment-product-comment-bttn-wrapper">
+                <RiCloseFill className="nav-icon"  onClick={ handleModal }/>
+            </div>
+            <div className={ modalContainer || "modal-comment-product-container" }>
+               
+                { children }
+
             </div>
         </div>
+
     )
+
 }
