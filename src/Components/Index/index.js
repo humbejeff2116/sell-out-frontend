4d6999b1b366da6b370ee49a5c45e013b770f814
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-26 05:44:18 +0000

    clean index component code

--- src/Components/Index/index.js
+++ src/Components/Index/index.js
@@ -1,41 +1,22 @@
 
-import React,{useState, useEffect} from 'react';
+import React,{ useState, useEffect } from 'react';
 import socket from '../Socket/socket';
 import { SearchProducts } from './searchProductsOrServices';
-import {PostProduct} from './postProduct';
+import { PostProduct } from './postProduct';
 import { ErrorModal } from '../ModalBox/errorModal';
 import { DisplayedProduct } from '../Product/product';
-import './index.css';
 import { getProducts } from '../../Utils/http.services';
 import { NavLink } from 'react-router-dom';
+import Links from '../../Data/links';
+import './index.css';
 
-import {BiHome, BiUser, BiFolder} from "react-icons/bi";
-import {RiBookOpenLine} from "react-icons/ri";
-
-
-const clothingLinks = [
-    { name: "All", href: "/", icon: <BiHome className="nav-icon" /> },
-    { name: "Gowns", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Tops", href: "/about", icon: <BiUser className="nav-icon"/> },
-    { name: "Skirts", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Trousers", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Shoes", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Accessories", href: "/support", icon: <BiFolder className="nav-icon"/> },
-]
-const maleClothingLinks = [
-    { name: "All", href: "/", icon: <BiHome className="nav-icon" /> },
-    { name: "Tops", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Trousers", href: "/about", icon: <BiUser className="nav-icon"/> },
-    { name: "Shoes", href: "/community", icon: <RiBookOpenLine className="nav-icon"/> },
-    { name: "Accessories", href: "/support", icon: <BiFolder className="nav-icon"/> },
-]
 
 
 const mockProducts = [
     {
         userId: 2234343,
         userName: "Humbe jeffrey",
-        userEmail: "humbejeff@gmail.com",
+        userEmail: "jeff@gmail.com",
         userProfilePicture: "",
         productId: 232323,
         productName: "short nikka",
@@ -44,7 +25,7 @@ const mockProducts = [
         productState: "Benue",
         productUsage: "never used",
         productCurrency: "naira",
-        productPrice: "200",
+        productPrice: "214",
         productContactNumber: "334039438493",
         productImages: [{}],
         stars: [],
@@ -96,105 +77,171 @@ const mockProducts = [
 
 
 export default function Index() {
+
     const [insideLoginError, setinsideLoginError] = useState('');
+    
     useEffect(()=> {
+
         let timer = null;
+
         let mounted = true;
-        socket.on('showInterestError', function(response) {
-            const { message } = response;
-            if (mounted) {
-                setinsideLoginError(message);
-            }
-            
-        });
+
         if (insideLoginError && mounted) {
+
             timer = setTimeout(() => {
-                setinsideLoginError('');   
+
+                setinsideLoginError(''); 
+
             }, 12000);
+
         }
+
         return ()=> {
+
             mounted = false;
+
             if (timer) {
+
                 clearTimeout(timer)
-            }   
+
+            } 
+
         }  
+
     }, [insideLoginError]);
+
     return (
+
         <div className="index-container">
             {
                 insideLoginError && (
+
                     <ErrorModal 
-                    errorMessage={insideLoginError}
+                    errorMessage = { insideLoginError }
                     errorContainerClassName={"index-error-container"}
                     panelClassName = {"index--error-modal"}
                     />
+
                 )
             }
            <PostProduct/>
            <SearchProducts/>
            <FilterDisplayedProducts/>
         </div>
+
     )
+
 }
 
 function FilterDisplayedProducts(props) {
+
     const [products, setProducts] = useState([]);
+
     const [queryValues, setQueryValue] = useState({});
+
     const [queryValueChange, setQueryValueChange] = useState(false);
+
     const [showClothingLinks, setShowClothingLinks] = useState(false);
+
+    const clothingLinks = Links.getClothingLinks()
+
+    const maleClothingLinks = Links.getMaleClothingLinks()
    
     useEffect(()=> {
+
         let mounted = true;
+
         async function getAllProducts() {
+
             try {
+
                 const productsResponse = await getProducts();
+
                 const products = productsResponse.data;
+
                 setProducts(products)
-            }catch(err) {
+
+            } catch(err) {
+
                 console.error(err.stack)
-            }  
+
+            } 
+
         }
+
         if (mounted) {
+
             getAllProducts();
+
         }  
+
         socket.on('productDataChange', function() {
+
             if (mounted) {
+
                 getAllProducts();
-            }         
+
+            } 
+
         });
 
         return ()=> {
+
             mounted = false;
+
             socket.off("getProducts");
+
         }
+
     }, []);
 
 
     useEffect(()=> {
+
         if (queryValues?.category === "clothes") {
-            setQueryValue(prevValues => (prevValues.gender ? {...prevValues, gender:"female" } :{...prevValues, gender:"female"}))
+
+            setQueryValue(prevValues => (prevValues.gender ? {...prevValues, gender:"female" } : {...prevValues, gender:"female"}))
+
             setShowClothingLinks(true)
-        }else {
+
+        } else {
+
             setShowClothingLinks(false)
+
         } 
+
     }, [queryValues.category])
    
     const queryProducts = async (queryValues) => {
+
         try {
+
             setQueryValueChange(false);
+
             const productsResponse = await getProducts(queryValues);
+
             const products = productsResponse.data;
+
             setProducts(products)
-        }catch(err) {
+
+        } catch(err) {
+
             console.error(err.stack)
-        }  
+
+        } 
+
     }
 
     const handleInputChange = function(e) {
-        setQueryValue(prevValues => ({ ...prevValues, [e.target.name] : e.target.value }) )
-        setQueryValueChange(true)   
+
+        setQueryValue(prevValues => ({ ...prevValues, [e.target.name] : e.target.value }))
+
+        setQueryValueChange(true) 
+
     }
+
     return (
+
         <>
         <div className="index-products-filter-heading">
             <p>Products</p>
@@ -202,7 +249,7 @@ function FilterDisplayedProducts(props) {
         <div className="index-products-filter-buttons">
             <div className="index-products-filter-select">
                 <label>Gender</label>
-                <select  name="gender" value = {queryValues.gender} onChange = {handleInputChange}>
+                <select  name="gender" value = { queryValues.gender } onChange = { handleInputChange }>
                     <option value="all">Default</option>
                     <option value="female">Female</option>
                     <option value="male">Male</option>
@@ -210,7 +257,7 @@ function FilterDisplayedProducts(props) {
             </div>
             <div className="index-products-filter-select">
                 <label>Category</label>
-                <select name="category" value = {queryValues.category} onChange = {handleInputChange}>
+                <select name="category" value = { queryValues.category } onChange = { handleInputChange }>
                     <option value="all"> Default</option>
                     <option value="electronics"> Electronics </option>
                     <option value="clothes">Clothes</option>
@@ -220,7 +267,7 @@ function FilterDisplayedProducts(props) {
             </div>
             <div className="index-products-filter-select">
                 <label>Usage</label>
-                <select name="usage" value = {queryValues.usage} onChange = {handleInputChange}>
+                <select name="usage" value = { queryValues.usage } onChange = { handleInputChange }>
                     <option value="all"> Default </option>
                     <option value="new"> New </option>
                     <option value="fairly used"> Fairly used </option>
@@ -232,53 +279,74 @@ function FilterDisplayedProducts(props) {
         </div>
         <div className="index-clothing-category-links-container">
             {
+
                 showClothingLinks && (queryValues?.gender === "female" || queryValues?.gender === "all") && 
                 (<ClothingCategoryLinks clothingLinks={clothingLinks}/>)
+
             }
+
             {
+
                 showClothingLinks && queryValues?.gender === "male" && 
                 (<ClothingCategoryLinks clothingLinks={maleClothingLinks}/>)
+
             }
         </div>
         <div className="index-products-container">
-          {
-              mockProducts.map((product,i) =>
-                  <DisplayedProduct 
-                  key={i}  
-                  product={product} 
-                  panelClassName="index-product-panel"
-                  />
-              )
-          }
+        {
+
+            mockProducts.map((product, i) =>
+
+                <DisplayedProduct 
+                key = { i }  
+                product = { product } 
+                panelClassName="index-product-panel"
+                />
+
+            )
+
+        }
         </div>
         </>
+
     )
+
 }
 
 
 function ClothingCategoryLinks(props) {
+
     return (
+
         <div className="index-clothing-category-links">
-            {
-                props.clothingLinks.map((links, i)=>
-                    <ClothingLinks key={i} {...links} />
-                )
-            }
+        {
+            props.clothingLinks.map((links, i) =>
+
+                <ClothingLinks key = { i } { ...links } />
+
+            )
+        }
         </div>
+
     )
+
 }
 
 function ClothingLinks(props) {
+
     return (
+
         <div classname="index-clothing-category-links-item">
             <NavLink
             exact 
-            to={props.href} 
+            to = { props.href } 
             activeClassName="main-link-active"
             className="main-nav-link" 
-            title={props.name} >
-                <i>{props.icon}</i><span>{props.name}</span> 
+            title = { props.name } >
+                <i>{ props.icon }</i><span>{ props.name }</span> 
             </NavLink> 
         </div>
+
     )
+
 }
