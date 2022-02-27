1c346a334fe19df801090aa51f21d578a3d66a65
humbejeff2116 <humbejeff2116@gmail.com>
2022-02-27 12:05:48 +0000

    refactor requireAuthentication code

--- src/Components/Authentication/requireAuthentication.js
+++ src/Components/Authentication/requireAuthentication.js
@@ -10,21 +10,35 @@ import useAuth from '../../Context/context';
 
 
 
-export default function RequireAuthentication(Component, auth) {
+export default function RequireAuthentication(Component, auth, redirectTo) {
     
     function AuthenticatedComponent(props) {
+
         const { isAuthenticated } = useAuth();
-            const auth = isAuthenticated();
+
+        const auth = isAuthenticated();
+
+        const redirectPath = redirectTo ? redirectTo : "/login";
+
 
             if (!auth) {
+
                 localStorage.setItem('route-auth-message','you must be logged in to view this page');
+
                 return(
-                   <Redirect to='/login' />
+
+                   <Redirect to = { redirectPath } />
+
                 )
+
             }
+
             return(  
+
                 <Component {...props}/>
+
             )
+            
     }
     
     return AuthenticatedComponent;
