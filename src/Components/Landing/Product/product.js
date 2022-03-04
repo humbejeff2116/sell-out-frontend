9344784c329146608a5ad618a8ee0488ccc2b653
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-04 10:57:24 +0000

    refactor landing page get products functionality

--- src/Components/Landing/Product/product.js
+++ src/Components/Landing/Product/product.js
@@ -1,7 +1,5 @@
 
 
-
-
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { DisplayedProduct } from '../../Product/product';
@@ -10,53 +8,124 @@ import socket from '../../Socket/socket';
 import './product.css';
 
 
-
-
-
 export default function LandingProducts(props) {
+
     const [products, setProducts] = useState([]);
-   
+
+    const [socketConnected, setSocketConnected] = useState(false);
     useEffect(()=> {
+
         let mounted = true;
+
         async function getAllProducts() {
+
             try {
+
                 const productsResponse = await getProducts();
+
                 const products = productsResponse.data;
+
                 setProducts(products)
-            }catch(err) {
+
+            } catch(err) {
+
                 console.error(err.stack)
-            }  
+
+            }
+
         }
-        if (mounted) {
+
+        if (socket.connected) {
+
+            setSocketConnected(true)
+           
+        } else {
+
+            socket.on('connect', ()=> {
+
+                setSocketConnected(true)
+               
+            })
+
+        }
+
+        if (socketConnected && mounted) {
+           
             getAllProducts();
-        }  
-        socket.on('productDataChange', function() {
-            if (mounted) {
-                getAllProducts();
-            }         
-        });
+
+        }
+   
+        
 
         return ()=> {
+
             mounted = false;
-            socket.off("getProducts");
+            
         }
-    }, []);
+
+    }, [socketConnected]);  
+
+    useEffect(()=> {
+
+        let mounted = true;
+
+        async function getAllProducts() {
+
+            try {
+
+                const productsResponse = await getProducts();
+
+                const products = productsResponse.data;
+
+                setProducts(products)
+
+            } catch(err) {
+
+                console.error(err.stack)
+
+            }
+
+        }
+
+        socket.on('productDataChange', function() {
+
+            if (mounted) {
+
+                getAllProducts();
+
+            } 
+
+        });
+
+
+        return ()=> {
+
+            mounted = false;
+            
+        }
+
+    }, [])
    
     return (
+
         <>
         <div  className="landing-product-heading">
             <h3>Products</h3>
         </div>
         <div  className="landing-product-container">
         {
-            products.map((product,i) =>
+
+           products.length > 0 && products.map((product,i) =>
+
                 <DisplayedProduct 
                 key={i} 
                 product={product} 
                 panelClassName="landing-product-panel"
                 productCommentPanelName="landing-product-comment-panel"
                 />
+
             )
+
         }
         </div>
         <div className="landing-view-products-btn-cntr">
@@ -65,57 +134,7 @@ export default function LandingProducts(props) {
             </div>
         </div>
         </>
-    )
-}
 
-export function LandingServices(props) {
-    const [services, setServices] = useState([]);
-   
-    useEffect(()=> {
-        let mounted = true;
-        if(mounted){
-            getServicesData();
-        }
-        socket.on('gottenServices', function(response) {
-            const services = response.data;
-            if(mounted) {
-                setServices(services);
-            }
-            
-        })
-        socket.on('productDataChange', function() {
-            if(mounted){
-                getServicesData(); 
-            }    
-        });
-       
-        return ()=> {
-            mounted = false
-        } 
-    }, []);
-    const getServicesData = ( ) => {  
-        socket.emit('getServices');
-
-    }
-    return (
-        <>
-        <div className="landing-product-heading">
-            <h3>Services</h3>
-        </div>
-        <div  className="landing-product-container">
-        {
-            services.map((service ,i) =>
-            <DisplayedProduct 
-                key={i} 
-                product={service} 
-                panelClassName="landing-product-panel"
-                />
-            )
-        }
-        </div>
-        <div className="landing-product-heading">
-            <div><button>View more services</button></div>
-        </div>
-        </>
     )
+
 }
