98d66ce12c1a3ee30085ba034b8da64c521f3ea0
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-04 11:02:58 +0000

    refactor product comments functionality

--- src/Components/Product/CommentBox/commentBox.js
+++ src/Components/Product/CommentBox/commentBox.js
@@ -1,46 +1,151 @@
 
-
-
 import React, { useState, useEffect } from 'react';
 import socket from '../../Socket/socket';
 import useAuth from '../../../Context/context';
-import { BiDislike,BiLike, BiReply, BiMessageAltDots, BiMessageAlt, BiMessage} from "react-icons/bi";
+import { BiDislike, BiLike, BiReply } from "react-icons/bi";
 import image from '../../../Images/avatar.jpg';
+import { getProductReviews } from '../../../Utils/http.services';
+import { Loader } from '../../Loader/loader';
 import './commentBox.css';
 
 
 export function CommentBox(props) {
+
     const [reviewValue, setReviewValue] = useState('');
+
     const [reviewError,  setReviewError] = useState(null);
+
+    const [productReviews, setProductReviews] = useState(null);
+
+    const [productReviewsError, setProductReviewsError] = useState(false)
+
     const { user } = useAuth();
-    const {commentBoxPanelClassName, closeCommentBox, product,productCommentPanelName } = props;
+
+    const { commentBoxPanelClassName, product, productCommentPanelName } = props;
+
     const textBox = React.createRef();
 
+    let CommentsPanel;
+
     useEffect(() => {
-        textBox.current.focus();  
+       
+        textBox.current.focus(); 
+
     }, [textBox]);
 
+    useEffect(() => { 
+
+        getCurrentProductReviews(product.productId) 
+
+    }, [product]);
+
+    useEffect(() => { 
+        let mounted = true;
+
+        socket.on('reviewDataChange', function() {
+
+            if (mounted) {
+
+                getCurrentProductReviews(product.productId)
+
+            } 
+
+        })
+
+        return ()=> {
+
+            mounted = false;
+
+        }
+
+    }, [product]);
+
     const makeReview = (product, user, reviewMessage) => {
+
         const message = reviewMessage.toString().trim();
+
         if (!message.length) {
+
             textBox.current.focus();
-            return setReviewError('please type in a comment')     
+
+            return setReviewError('please type in a review') 
+
         }
 
         const data = {
-            productOrService: product,
+            product: product,
             user: user,
             reviewMessage: reviewMessage
         }
-        socket.emit('reviewProductOrService', data);
+
+        socket.emit('reviewProduct', data);
+
         textBox.current.value = '';
+
     }
 
     const handleInputChange = e => {
+
         setReviewValue( e.target.value )
+
+    }
+
+    const getCurrentProductReviews = async (productId) => {
+
+        try {
+
+            const productReviews = await getProductReviews(productId);
+
+            setProductReviews(productReviews.data);
+
+        } catch(err) {
+
+            setProductReviewsError(true)
+
+        }
+        
+    }
+
+    if (!productReviews) {
+
+        CommentsPanel = (
+           
+            <Loader 
+            loaderContainer = {"comments-loader-container"}
+            loader = {"comments-loader"}
+            />
+
+        )    
+
+    } else if (!productReviews.length >0) {
+   
+        CommentsPanel = (
+
+            <EmptyComments/>
+
+        )
+
+    } else if (productReviewsError) {
+
+        CommentsPanel = (
+            // TODO... return an error component
+            <div>Error</div>
+        )
+
+    } else {
+
+        CommentsPanel = (
+
+            < ReviewsWrapper
+            productCommentPanelName = { productCommentPanelName } 
+            productReviews = { productReviews }
+            />
+        )
+
     }
 
     return (
+
         <div className={commentBoxPanelClassName}>
             <div className="product-comment-container">
                
@@ -54,16 +159,7 @@ export function CommentBox(props) {
                     </div>
                 </div>
                 <div className="product-comments-container">
-                    {
-                        ( product.comments.length > 0 )  && 
-                        (product.comments.map((comment, i) =>
-                            <Comment
-                            productCommentPanelName={productCommentPanelName} 
-                            key={i} 
-                            {...comment} 
-                            />
-                        ))
-                    }
+                    { CommentsPanel }
                 </div>  
             </div> 
 
@@ -80,137 +176,259 @@ export function CommentBox(props) {
 }
 
 function Comment(props) {
-    const { user } = useAuth();
+
     const [likesCommentRecieved, setLikesCommentRecieved] = useState([]);
-    const [unlikesCommentRecieved, setUnlikesCommentRecieved] = useState([]);
+
+    const [dislikesCommentRecieved, setDislikesCommentRecieved] = useState([]);
+
     const [showReplies, setShowReplies] = useState(false);
+
     const [replyMessage, setReplyMessage] = useState('');
+
     const [userLikedComment, setUserLikedComment] = useState(false);
+
     const [userDislikedComment, setUserDislikedComment] = useState(false);
+
+    const { user } = useAuth();
+
     const replyTextBox = React.createRef();
+
     const { userName, userId, profileImage, comment, _id, replies } = props;
+
     const likeIconClassName = userLikedComment ? "product-comment-bttn fill" : "product-comment-bttn"
+
     const dislikeIconClassName = userDislikedComment ? "product-comment-bttn fill" : "product-comment-bttn"
     
     useEffect(() => {
-        setLikesCommentRecieved(props.likesCommentRecieved);
-        setUnlikesCommentRecieved(props.unlikesCommentRecieved);
-        
-    }, [user, props.likesCommentRecieved, props.unlikesCommentRecieved]);
+
+        if (props?.likesCommentRecieved.length > 0) {
+
+            setLikesCommentRecieved(props.likesCommentRecieved);
+
+        }
+
+        if (props?.dislikesCommentRecieved.length > 0) {
+            
+            setDislikesCommentRecieved(props.dislikesCommentRecieved);
+
+        }
+
+    }, [user, props.likesCommentRecieved, props.dislikesCommentRecieved]);
 
     useEffect(() => {
-        const checkIfUserLikedOrDislikedComment = (user, likes, disLikes) => {
+
+        const checkIfUserLikedOrDislikedComment = (user, likes, dislikes) => {
+
+            let i;
+
+            const likesLen = likes.length;
+
+            const dislikesLen = dislikes.length;
+
             const { userEmail } = user;
-            for(let i = 0; i < likes.length; i++ ) {
-                if(likes[i].likeGiverEmail === userEmail) {
+
+            for (i = 0; i < likesLen; i++) {
+
+                if (likes[i].userEmail === userEmail) {
+
                     setUserLikedComment(true);
+
                     break;
                 }
+
             }
-            for(let i = 0; i < disLikes.length; i++ ) {
-                if(disLikes[i].unlikeGiverEmail === userEmail) {
+
+            for ( i = 0; i < dislikesLen; i++) {
+
+                if (dislikes[i].userEmail === userEmail) {
+
                     setUserDislikedComment(true);
+
                     break;
                 }
+
             }
+
         }
-        if(user) {
-            checkIfUserLikedOrDislikedComment(user, props.likesCommentRecieved, props.unlikesCommentRecieved );
-        }    
-    }, [user, props.likesCommentRecieved, props.unlikesCommentRecieved ]);
+
+        if (user && (props?.likesCommentRecieved.length > 0 ||  props?.dislikesCommentRecieved.length > 0)) {
+
+            checkIfUserLikedOrDislikedComment(user, props.likesCommentRecieved, props.dislikesCommentRecieved );
+
+        }  
+
+    }, [user, props.likesCommentRecieved, props.dislikesCommentRecieved ]);
     
     
     const viewProfile = () => {
         // TODO... save reviewId in a context or local storage and redirect to view profile page
     }
+
     const replyComment = (commentId, user, replyMessage) => {
+
         const message = replyMessage.toString().trim();
+
         if (!message.length) {
+
             replyTextBox.current.focus();
+
             return;
             // return setReviewError('please type in a comment')     
         }
+
         const replyReviewData = {
             commentId,
             user,
             replyMessage: message
         }
-        socket.emit("replyReviewProductOrService", replyReviewData);
+
+        socket.emit("replyReviewProduct", replyReviewData);
+
         replyTextBox.current.value = '';
+
         replyTextBox.current.focus();
+
     }
+
     const handleInputChange = e => {
+
         setReplyMessage(e.target.value)
+
     }
+
     const toggleReply = () => {
+
         return setShowReplies(currentState => !currentState);
+
     }
 
     const likeComment = (commentId, user) => {
+
+        let i;
+
+        const commentLikes = likesCommentRecieved;
+
+        const commentLikesLen = commentLikes.length;
+
         const data = {
             commentId: commentId,
              user: user,
         }
-        const commentLikes = likesCommentRecieved;
+
         if (user) {
-            const like = { likeGiverEmail: user.userEmail, likeGiverId: user.id, likeGiverFullName: user.fullName }
+
+            const like = { userEmail: user.userEmail, userId: user.id, userFullName: user.fullName }
+
             let likedComment = false;
-            for (let i = 0; i < commentLikes.length; i++) {
-                if (commentLikes[i].likeGiverEmail === user.userEmail) {
+
+            for (i = 0; i < commentLikesLen; i++) {
+
+                if (commentLikes[i].userEmail === user.userEmail) {
+
                     likedComment = true;
+
                     break;
+
                 }
+
             }
+
             if (likedComment) {
+
                 setUserLikedComment(false);
-                setLikesCommentRecieved(currentState => currentState.filter(like => like.likeGiverEmail !== user.userEmail ));
-                setUnlikesCommentRecieved(currentState => currentState.filter(dislike => dislike.unlikeGiverEmail !== user.userEmail ));
-                socket.emit('likeComment', data );
+
+                setLikesCommentRecieved(currentState => currentState.filter(like => like.userEmail !== user.userEmail ));
+
+                setDislikesCommentRecieved(currentState => currentState.filter(dislike => dislike.userEmail !== user.userEmail ));
+
+                socket.emit('likeReview', data );
+
                 return;
+
             }
+
             setUserLikedComment(true);
+
             setUserDislikedComment(false);
+
             setLikesCommentRecieved([...likesCommentRecieved, like]);
-            setUnlikesCommentRecieved(currrentState => currrentState.filter(dislike => dislike.unlikeGiverEmail !== user.userEmail ));
-            socket.emit('likeComment', data );
+
+            setDislikesCommentRecieved(currrentState => currrentState.filter(dislike => dislike.userEmail !== user.userEmail ));
+
+            socket.emit('likeReview', data );
+            
             return;
+
         }
-        socket.emit('likeComment', data );
+
+        socket.emit('likeReview', data );
+
     }
 
-    const disLikeComment = (commentId, user) => {   
+    const disLikeComment = (commentId, user) => { 
+        let i;
+    
+        const commentDislikes = dislikesCommentRecieved;
+
+        const commentDislikesLen = commentDislikes.length;
+
         const data = {
             commentId: commentId,
             user: user,
         }
-        const commentDislikes = unlikesCommentRecieved;
+
         if (user) {
-            const dislike = { unlikeGiverEmail: user.userEmail, unlikeGiverId: user.id, unlikeGiverFullName: user.fullName }
+
+            const dislike = { userEmail: user.userEmail, userId: user.id, userFullName: user.fullName }
+
             let dislikedComment = false;
-            for (let i = 0; i < commentDislikes.length; i++) {
-                if (commentDislikes[i].unlikeGiverEmail === user.userEmail) {
+
+            for (i = 0; i < commentDislikesLen; i++) {
+
+                if (commentDislikes[i].userEmail === user.userEmail) {
+
                     dislikedComment = true;
+
                     break;
+
                 }
+
             }
+
             if (dislikedComment) {
+
                 setUserDislikedComment(false);
-                setLikesCommentRecieved(currrentState => currrentState.filter(like => like.likeGiverEmail !== user.userEmail ));
-                setUnlikesCommentRecieved(currrentState => currrentState.filter(dislike => dislike.unlikeGiverEmail !== user.userEmail ));
-                socket.emit('unLikeComment', data );
+
+                setLikesCommentRecieved(currrentState => currrentState.filter(like => like.userEmail !== user.userEmail ));
+
+                setDislikesCommentRecieved(currrentState => currrentState.filter(dislike => dislike.userEmail !== user.userEmail ));
+
+                socket.emit('dislikeReview', data);
+
                 return;
+
             }
-             setUserDislikedComment(true);
-             setUserLikedComment(false);
-            setUnlikesCommentRecieved([...unlikesCommentRecieved, dislike]);
-            setLikesCommentRecieved(currrentState => currrentState.filter(like => like.likeGiverEmail !== user.userEmail ));
-            socket.emit('unLikeComment', data ); 
-            return;   
+
+            setUserDislikedComment(true);
+
+            setUserLikedComment(false);
+
+            setDislikesCommentRecieved([...dislikesCommentRecieved, dislike]);
+
+            setLikesCommentRecieved(currrentState => currrentState.filter(like => like.userEmail !== user.userEmail ));
+
+            socket.emit('dislikeReview', data);
+
+            return; 
+
         }
-        socket.emit('unLikeComment', data );   
+
+        socket.emit('dislikeReview', data); 
+
     }
 
     return (
+
         <div className={ props.productCommentPanelName ? props.productCommentPanelName : "product-comment-panel" }>
        
         <div className="product-comment-info">
@@ -241,8 +459,8 @@ function Comment(props) {
                 <div className="product-comment-bttn-count">
                     <span>
                     {
-                        (unlikesCommentRecieved && unlikesCommentRecieved.length > 0) ? 
-                        unlikesCommentRecieved.length : ''
+                        (dislikesCommentRecieved && dislikesCommentRecieved.length > 0) ? 
+                        dislikesCommentRecieved.length : ''
                     }
                     </span>
                 </div>
@@ -275,14 +493,21 @@ function Comment(props) {
             )   
         }
         </div>
+
     )
+
 }
 
 function Replies(props) {
+
     const { textBox, repliesCommentRecieved, handleInputChange, replyComment, commentId, user, replyMessage } = props;
+
     useEffect(() => {
-        textBox.current.focus();
+        // textBox.current.focus();
     }, [textBox]);
+
+
+    // TODO... implement like and dislike review reply
     const likeReply = ( ) => {
 
     }
@@ -292,6 +517,7 @@ function Replies(props) {
     }
    
     return (
+
         <div className="product-comment-reply-container">
             {
                 ( repliesCommentRecieved && repliesCommentRecieved.length > 0 ) && (
@@ -329,7 +555,7 @@ function Replies(props) {
 function Reply (props) {
     const {replyMessage, userName, viewProfile, user , userId, likesReplyRecieved, _id, dislikesReplyRecieved } = props;
     return (
-        <>
+        <div className="product-comment-reply-wrapper">
         {/* reply profile */}
         <div className="product-comment-reply-profile">
             <img src={image} alt="profile" />
@@ -357,7 +583,7 @@ function Reply (props) {
             </div>
            
             <div className="product-comment-bttn">
-            <BiDislike title="Dislike" className="nav-icon"onClick={()=> props.disLikeReply(props._id, user)}/>
+            <BiDislike title="Dislike" className="nav-icon"onClick={()=> props.dislikeReply(props._id, user)}/>
                 <div className="product-comment-bttn-count">
                     <span>
                     {
@@ -370,6 +596,41 @@ function Reply (props) {
 
         </div>
 
+        </div>
+    )
+}
+
+
+function EmptyComments(props) {
+    return (
+        <div>
+            <div>
+                Product has no reviews yet 
+            </div>
+
+        </div>
+    )
+}
+
+function ReviewsWrapper(props) {
+
+    return (
+
+        <>
+        {
+            
+            (props.productReviews.map((review, i) =>
+
+                <Comment
+                productCommentPanelName = { props.productCommentPanelName } 
+                key = { i } 
+                {...review} 
+                />
+
+            ))
+            
+        }
         </>
+
     )
 }
