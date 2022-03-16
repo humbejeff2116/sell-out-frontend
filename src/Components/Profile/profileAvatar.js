067776882fd405330ecec1914234db52638c32f8
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-16 15:36:13 +0000

    consume user context and refactor Avatar code

--- src/Components/Profile/profileAvatar.js
+++ src/Components/Profile/profileAvatar.js
@@ -1,35 +1,68 @@
 
-
-
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import useAuth from '../../Context/context';
+import profileAvatar from '../../Images/avatar4.png';
 import './profileAvatar.css';
-import image from '../../Images/avatar.jpg';
 
 
 
 
 export default function ProfileAvatar(props) {
+
+    const { user } = useAuth();
+
     return (
+
         <div className="profile-avatar-container">
-           < Avatar/>
+           < Avatar
+           userName ={ user?.fullName }
+           userAvatarSrc={user?.profileImage}
+           />
         </div>
+
     )
+
 }
 
 
-function Avatar(props) {
+function Avatar({ userAvatarSrc, userName, ...props }) {
+
+    const formatName = (name) => {
+
+        const [first, second]= name.split(' ');
+
+        if (first && second) {
+
+            if (second.length > 6) {
+
+                return `${ first } ${ second.slice(0, 1).toUpperCase() }.`
+
+            }
+
+            return `${ first } ${ second }`
+
+        }
+
+        return `${ first }`
+        
+    }
+
+    const imageSrc = (userAvatarSrc === "no-image" || !userAvatarSrc ) ? null : userAvatarSrc 
+
     return (
+
         <>
             <div className="profile-avatar-image">
-            <img src={image} alt="profile avatar" />
+            <img src={ imageSrc || profileAvatar } alt="profile avatar" />
             </div>
             <div className="profile-avatar-username">
-                <span>Humbe J.</span>
+                <span>{ userName ? formatName(userName) : "" }</span>
             </div>
             {/* <div className="profile-avatar-username location">
                 <span>Cross River, Nigeria</span>
             </div> */}
         </>
-    )
+
+    ) 
+     
 }
