64924b5cb3e23280d2527441c94922a0113887d1
humbejeff2116 <humbejeff2116@gmail.com>
2022-03-24 21:39:18 +0000

    add percentageOff product property and clean code

--- src/Context/Cart/cartPayloads.js
+++ src/Context/Cart/cartPayloads.js
@@ -1,52 +1,63 @@
 
 function addToCartActionPayload(product, productQty, productSize="") {
+
     const {
         productId,
         userId,
         userName,
         userEmail,
-        userProfilePicture, 
+        userProfileImage, 
         productName,
         productImages,
         productPrice,
+        percentageOff,
         // productQty,
         // productSize 
     } = product;
+
     return ({
         userId,
         userName,
         userEmail,
-        userProfilePicture,
+        userProfileImage,
         productId,
         productName,
         productImages,
         productQty,
         productPrice,
+        percentageOff,
         productSize
-    });
+    })
+
 }
 
 function removeFromCartActionPayload(sellerEmail, productId) {
+
     return ({
         sellerEmail,
         productId
     })
+
 }
 
 function addCartProductQuantityActionPayload(sellerEmail, productId, productQty) {
+
     return ({
         sellerEmail,
         productId,
         productQty,
     })
+
 }
 
 function reduceCartProductActionPayload(sellerEmail, productId, productQty) {
+
     return ({
         sellerEmail,
         productId,
         productQty
     })
+    
 }
 
 export {
