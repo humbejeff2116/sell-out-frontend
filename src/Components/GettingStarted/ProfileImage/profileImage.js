f46675f79883fe57c0499594fb6722871829ca1f
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-10 15:54:10 +0100

    modify getting started profile image form

--- src/Components/GettingStarted/ProfileImage/profileImage.js
+++ src/Components/GettingStarted/ProfileImage/profileImage.js
@@ -1,68 +1,129 @@
 
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import { Redirect, useLocation, useHistory } from 'react-router-dom';
 import { useGetStartedContext } from '../../../Context/context';
 import { ImWarning } from 'react-icons/im';
-import useAuth from '../../../Context/context';
+//avatar SVG image gotten from https://svgrepo.com
+import image from '../../../Images/avatar2.png';
+import { FiCamera } from 'react-icons/fi';
+// import { BsExclamationCircle } from 'react-icons/bs';
 import './profileImage.css';
 import '../Contact/contact.css';
 
-
-
-
 export default function ProfileImage(props) {
+
     const [redirect, setRedirect] = useState('');
-    const [imageFile, setImageFile] = useState(null);
-    const [displayImage, setDisplayImage] = useState(null);
+
+    // const [imageFile, setImageFile] = useState(null);
+
+    const [displayImageURL, setDisplayImageURL] = useState(null);
+
     const [profileError, setProfileError] = useState('');
+
+    const [showImageSelector, setShowImageSelector] = useState(false);
+
     const location = useLocation();
+
     const history = useHistory();
-    const {user, setUserData, setTokenData} = useAuth();
-    const {profileImage, setProfile, setIsLocationDataSet, setIsProfileDataSet, handleSubmit, getDetails} = useGetStartedContext();
+
+    const { 
+        profileImageFile,
+        profileImageURL, 
+        setProfileImageFile,
+        setProfileImageURL,
+        setIsLocationDataSet, 
+        setIsProfileDataSet, 
+    } = useGetStartedContext();
+
     useEffect(() => {  
+
         window.scrollTo(0, 0);
+
     }, []);
+
     const goBack = ( ) => {
+
         setIsLocationDataSet(false)
+
         setIsProfileDataSet(false)
-        setProfile(displayImage); 
+
+        setProfileImageURL(displayImageURL); 
+
         history.push(location.pathname);
-        setRedirect('/getting-started/location');   
+
+        setRedirect('/getting-started/application/shipping-and-operations');
+
     }
+
+    const handleSubmit = (profileImageFile) => {
+
+        if (!profileImageFile) {
+
+            return setProfileError('Profle picture required')
+
+        }
+
+        history.push(location.pathname);
+
+        setRedirect('/getting-started/application/confirmation');
+
+    }
+
     const handleImageChange = (e) => {
-        setImageFile({
-            [e.target.name] : e.currentTarget.files[0] 
-        });
-        setDisplayImage(URL.createObjectURL(e.currentTarget.files[0]))
-        setProfile(URL.createObjectURL(e.currentTarget.files[0]));
+
+        setProfileImageFile({
+
+            [e.target.name] : e.target.files[0] 
+
+        })
+
+        setDisplayImageURL(URL.createObjectURL(e.target.files[0]))
+
+        setProfileImageURL(URL.createObjectURL(e.target.files[0]));
+
         setProfileError('')
-    }
-    const profileImageError = (errorMssg) => {
-        window.scrollTo(0, 0)
-        setProfileError(errorMssg)
-    }
-    const userUpdatedSuccessful = (response) => {
-        const { data, token } = response;
-        history.push(location.pathname);
-        setUserData(data)
-        setTokenData(token);
-        setRedirect('/home');
+
     }
     
-    if(redirect) {
-        return (
-            <Redirect to={redirect} />
-        )
-    }
-    const imageError = (<><ImWarning/>{profileError}</>);
-    const formDetails = getDetails();
+    if (redirect) {
 
+        return (
+
+            <Redirect to={redirect} />
+
+        )
+
+    }
+
+    const imageError = ( <> <ImWarning/> { profileError } </> )
+
+    const gettingStartedInputClassName = showImageSelector ? "getting-started-input visible" : "getting-started-input"
+
+    const showImageIcon = (e) => {
+
+        setShowImageSelector(true)
+
+    }
+
+    const hideImageIcon = (e) => {
+
+        setShowImageSelector(false)
+
+    }
+    
     return (
+
         <div className="getting-started-contact-container">
+            {
+                profileError && (
+
+                    <div className="getting-started-error">
+                    <span>{ profileError ? imageError : ''}</span>
+                    </div>
+
+                )
+            }
+            
         <div className="getting-started-contact-panel">
             {/* avatar */}   
             <div className="getting-started-contact-avatar">
@@ -72,42 +133,48 @@ export default function ProfileImage(props) {
                     </p>
                 </div>
                 <div className="getting-started-profile-image">
+                   
+                    <div 
+                    className={ gettingStartedInputClassName }
+                    onMouseEnter ={showImageIcon}
+                    onMouseLeave = {hideImageIcon}   
+                    >
+
+                        <ProfileImageSelector
+                        // label="Select Product Images"
+                        labelClassName={ "getting-started-input-label" }
+                        labelSpanClassName="title"
+                        name="productImages"
+                        type="file"
+                        // TODO... replace icon with a proper image icon 
+                        icon= { <FiCamera/> }
+                        onChange = { handleImageChange }                
+                        />
+                           
+                    </div>
+
                     <img 
                     src={
-                        displayImage ? displayImage : 
-                        profileImage ? profileImage : ""
+                        displayImageURL ? displayImageURL : 
+                        profileImageURL ? profileImageURL : image 
                     } 
-                    alt="profile avatar"
+                    alt=""
+                    onMouseEnter ={showImageIcon}
+                    onMouseLeave = {hideImageIcon}
                     />
-                    <div className="getting-started-input">
-                        {/* <div className="getting-started-input-icon">
-                            <span>icon<span>Edit</span></span>
-                        </div> */}
-                        <input type="file"  name="profileImage" onChange={ handleImageChange }  />
-                    </div>
-                    <div className="getting-started-error">
+
+                    {/* <div className="getting-started-error">
                         <span>{ profileError ? imageError : ''}</span>
-                    </div>
+                    </div> */}
+
                 </div>  
             </div>
             {/* body */}
             <div className="getting-started-contact-body">
-                <div className="getting-started-profile-image-info">
-                {
-                    <>
-                    <div><span className="getting-started-info-group">FULL NAME: </span><span>{user?.fullName} john adakole</span></div>
-                    <div><span className="getting-started-info-group">CONTACT EMAIL: </span><span>{formDetails?.contactEmail} john@gmail.com</span></div>
-                    <div><span className="getting-started-info-group">CONTACT NUMBER: </span><span>{formDetails?.contactNumber} 0805557758557</span></div>
-                    <div><span className="getting-started-info-group">COUNTRY: </span><span>{formDetails?.country} Nigeria</span></div>
-                    <div><span className="getting-started-info-group">CITY: </span><span>{formDetails?.city} Makurdi</span></div>
-                    <div><span className="getting-started-info-group">CONTACT ADDRESS: </span><span>{formDetails?.address} Behind child evangelical ministries off goerge akume way makurdi benue state</span></div>
-                    </>
-                }
+            <div className="getting-started-profile-image-info">
                 <span className="brand-name">
-                    All data entered in this proccess by you can be modified, or deleted at anytime, 
-                    and by submitting these information you are giving us consent to share this data
-                    wherever your profile appears. Kindly go through our privacy policy if you haven't 
-                    to know how we use your information or how you can manage your data. 
+                    This image is usually tagged on all your products you put up for sale, 
+                    to enable buyers identify products which are being sold by you.
                 </span>
                 </div>
                 {/* buttons */}
@@ -117,12 +184,35 @@ export default function ProfileImage(props) {
                             Back
                         </button>
                     </div>
-                    <div className="getting-started-contact-next-button">
-                    <button onClick={ ()=> handleSubmit(imageFile, user, profileImageError, userUpdatedSuccessful)}>Submit details</button>
+                    <div className= { `getting-started-contact-next-button` } >
+                    <button 
+                    onClick={ ()=> handleSubmit(profileImageFile) }
+                    // disabled = { disableButton }
+                    >
+                    Continue
+                    </button>
                     </div>
                 </div>
             </div>  
         </div>
         </div>
+
     )
+
 }
+
+
+const ProfileImageSelector = ({labelClassName, labelSpanClassName, label, ...props}) => {
+
+    return (
+
+        <label className={ labelClassName }  >
+
+            <i>{props.icon}</i>
+
+            <input className={ props.inputClassName || "product-images" } {...props} />
+
+        </label>
+    )
+
+} 
