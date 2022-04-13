436cf526aba7dc5528c40fffc0ae85a7a7cb0985
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-13 22:14:04 +0100

    modify getting started context provider

--- src/Context/gettingStartedContextProvider.js
+++ src/Context/gettingStartedContextProvider.js
@@ -1,90 +1,93 @@
 
-
-import {useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { GetStartedContext } from './context';
-import {updateUser} from '../Utils/http.services';
+import { updateUser } from '../Utils/http.services';
 
 
 export function GetStartedContextProvider(props) {
-    const [contactData, setContactData] = useState(null);
-    const [locationData, setLocationData] =  useState(null);
-    const [profileImage, setProfileImage] = useState(null);
-    const [isContactDataSet, setIsContactDataSet] = useState(false);
-    const [isLocationDataSet, setIsLocationDataSet] = useState(false);
-    const [isProfileDataSet, setIsProfileDataSet] = useState(false);
-   
 
-    useEffect(()=> {
-       
-       
-    }, [contactData, locationData, profileImage]);
+    // const [personalDetailsData, setPersonalDetailsData] = useState(null);
 
+    const [profileImageFile, setProfileImageFile] = useState(null);
 
-    const setContact = (data) => {
-        setContactData(data);
-        setIsContactDataSet(true);
-    }
-    const setLocation = (data) => {
-        setLocationData(data);
-        setIsLocationDataSet(true);
-    }
-    const setProfile = (data) => { 
-        setProfileImage(data);  
-    }
+    const [profileImageURL, setProfileImageURL] = useState(null);
+
+    const [registeredCompanyOrBusinessData, setRegisteredCompanyOrBusinessData] = useState(null);
+
+    const [unregisteredBusinessData, setUnregisteredBusinessData] = useState(null);
+
+    const [legalAddressData, setLegalAddressData] = useState(null);
+
+    const [shippingAndOperationsData, setShippingAndOperationsData] = useState(null);
+
+    const [submittedFormPaths, setSubmittedFormPaths] = useState([])
+
     const getDetails = () => {
-        return ({
-            contactEmail: contactData ? contactData.contactEmail : '',
-            contactNumber: contactData ? contactData.contactNumber : '',
-            country: locationData ? locationData.country : '',
-            city: locationData ? locationData.city : '',
-            address: locationData ? locationData.address : '',
-        })
+
+        return ({ })
+
     }
 
-    const handleSubmit = (profileImage, user, errorCallback, successCallback) => {
-        if (!profileImage) {
-            return  errorCallback("please provide a profile picture")
-        }
-        const values = {...contactData,...locationData,...profileImage}
-        const formData = new FormData();
-        formData.append('userEmail' , user.userEmail);
-        formData.append('userId' , user.id);
-        const formValuesArr = Object.keys(values).map( keys => ({name: keys, value: values[keys]}));
-        console.log("values are",formValuesArr)
-        for (let i = 0; i < formValuesArr.length; i++) {
-            formData.append(formValuesArr[i].name , formValuesArr[i].value);
-        }
-        updateUser(formData)
-        .then(response => {
-            
-        })
-        .then(user => {
-            // TODO... pass user response to succescallback here
+    const handleSubmit = async (user, updateUserCallback, setCreatingProfile) => {
 
-        })
-        .catch(e => console.error(e.stack));
+        setCreatingProfile(true)
+        // TODO... add other form values
+        const values = { ...profileImageFile }
+
+        console.log("values are", values)
+
+        const formData = new FormData();
+
+        formData.append('userEmail', user.userEmail);
+
+        formData.append('userId', user.id);
+        
+        for (let keys in values) {
+
+            formData.append(keys, values[keys]);
+
+        }
+
+        const updatedUser = await updateUser(formData)
+
+        updateUserCallback(updatedUser)
+        
+    }
+
+    const removePathName = (pathname = "") => {
+
+        const filteredPathNames = submittedFormPaths.filter(path => path.href !== pathname);
+
+        return setSubmittedFormPaths(filteredPathNames)
+
     }
 
     const values = {
-        contactData: contactData,
-        locationData: locationData,
-        profileImage: profileImage,
-        isContactDataSet: isContactDataSet,
-        isLocationDataSet: isLocationDataSet,
-        isProfileDataSet: isProfileDataSet,
-        setContact: setContact,
-        setLocation: setLocation,
-        setProfile: setProfile,
-        setIsContactDataSet: setIsContactDataSet,
-        setIsLocationDataSet: setIsLocationDataSet,
-        setIsProfileDataSet: setIsProfileDataSet,
+        profileImageURL: profileImageURL,
+        profileImageFile: profileImageFile,
+        registeredCompanyOrBusinessData: registeredCompanyOrBusinessData,
+        unregisteredBusinessData: unregisteredBusinessData,
+        legalAddressData: legalAddressData,
+        shippingAndOperationsData: shippingAndOperationsData,
+        submittedFormPaths: submittedFormPaths,
+        setRegisteredCompanyOrBusinessData: setRegisteredCompanyOrBusinessData,
+        setUnregisteredBusinessData: setUnregisteredBusinessData,
+        setLegalAddressData: setLegalAddressData,
+        setShippingAndOperationsData: setShippingAndOperationsData,
+        setProfileImageFile: setProfileImageFile,
+        setProfileImageURL: setProfileImageURL,
+        setSubmittedFormPaths: setSubmittedFormPaths,
         handleSubmit: handleSubmit,
-        getDetails: getDetails
+        getDetails: getDetails,
+        removePathName: removePathName,  
     }
 
     return(
-        <GetStartedContext.Provider value={values} >
-            {props.children}
+
+        <GetStartedContext.Provider value = { values } >
+            { props.children }
         </GetStartedContext.Provider>
+
     )
+
 }
