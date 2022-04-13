7c2266114cd6a157bf58a85a73550d5508ca1934
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-13 22:25:18 +0100

    develop unregisteredBusinessOrBrand getting started application form component

--- src/Components/GettingStarted/CompanyOrBusiness/UnregisteredBusinessOrBrand/unregisteredBusinessOrBrand.js
+++ src/Components/GettingStarted/CompanyOrBusiness/UnregisteredBusinessOrBrand/unregisteredBusinessOrBrand.js
@@ -1,118 +1,135 @@
 
-
-
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { TextInput, TextAreaInput, Select } from '../../../Formik/formik';
+import { Options } from '../RegisteredCompanyOrBusiness/registeredCompanyOrBusiness';
 import { useGetStartedContext } from '../../../../Context/context';
+import Countries from '../../../../Data/countries';
 
 
 export default function UnregisteredBusinessOrBrand(props) {
 
-    const {
-        unregisteredBusinessData,
-    } = useGetStartedContext();
+    const { unregisteredBusinessData } = useGetStartedContext();
+
+     const africanCountries = Countries.getCountries({continent: "Africa"});
+
+    const nigeraianStates = Countries.getCountryStates({ continent: "Africa", country: "Nigeria" });
 
     return (
+
         <Formik
-                    initialValues = {{
+        initialValues = {{
 
-                        countryFoundIn: unregisteredBusinessData ?
-                        unregisteredBusinessData?.countryFoundIn : '',
+            countryFoundIn: unregisteredBusinessData ?
+            unregisteredBusinessData?.countryFoundIn : '',
 
-                        stateFoundIn: unregisteredBusinessData ?
-                        unregisteredBusinessData?.stateFoundIn : '',
+            stateFoundIn: unregisteredBusinessData ?
+            unregisteredBusinessData?.stateFoundIn : '',
 
-                        businessOrBrandName: unregisteredBusinessData ?
-                        unregisteredBusinessData?.businessOrBrandName : '',
+            businessOrBrandName: unregisteredBusinessData ?
+            unregisteredBusinessData?.businessOrBrandName : '',
 
-                        YearOfFoundation: unregisteredBusinessData ?
-                        unregisteredBusinessData?.YearOfFoundation : '',
+            YearOfFoundation: unregisteredBusinessData ?
+            unregisteredBusinessData?.YearOfFoundation : '',
 
-                    }}
+        }}
 
-                    validationSchema = { Yup.object({
-                        
-                        countryFoundIn: Yup.string().required('Country founded in is required'),
-                        stateFoundIn: Yup.string().required('State founded in is required'),
-                        businessOrBrandName: Yup.string().required('Business or Brand Name is required'),
-                        YearOfFoundation: Yup.string().required('Year of foundation is required'),
+        validationSchema = { Yup.object({
+            
+            countryFoundIn: Yup.string().required('Country founded in is required'),
+            stateFoundIn: Yup.string().required('State founded in is required'),
+            businessOrBrandName: Yup.string().required('Business or Brand Name is required'),
+            YearOfFoundation: Yup.string().required('Year of foundation is required'),
 
-                    })}
+        })}
 
-                    onSubmit = { props.handleSubmit }
+        onSubmit = { props.handleSubmit }
+        >
+        <Form id="contactForm">
+            <div className="getting-started-contact-form-inputs">
+
+                <Select
+                label="Country Founded In"
+                labelClassName="company-form-group"
+                name="countryFoundIn"
+                errorClass="contact-form-error"
                 >
-                <Form id="contactForm">
-                    <div className="getting-started-contact-form-inputs">
+                <option value="">Select</option>
+                {
+                    africanCountries.map((country, i) =>
 
-                        <Select
-                        label="Country Founded In"
-                        labelClassName="company-form-group"
-                        name="countryFoundIn"
-                        errorClass="contact-form-error"
-                        >
-                            <option value="">Select</option>
-                            <option value="Nigeria">Nigeria</option>
-                            <option value="Ghana">Ghana</option>
-                            <option value="Congo">Congo</option>
-                        </Select>
-
-                        <Select
-                        label="State Founded In"
-                        labelClassName="company-form-group"
-                        name="stateFoundIn"
-                        errorClass="contact-form-error"
-                        >
-                            <option value="">Select</option>
-                            <option value="Nigeria">Nigeria</option>
-                            <option value="Ghana">Ghana</option>
-                            <option value="Congo">Congo</option>
-                        </Select>
-
-                        <TextInput
-                        label="Business/Brand Name"
-                        labelClassName="company-form-group"
-                        // labelText=" (optional)"
-                        name="businessOrBrandName"
-                        type="text"
-                        errorClass="contact-form-error"
+                        <Options 
+                        key = { i }
+                        { ...country }
                         />
 
-                        <TextInput
-                        label="Year of Foundation"
-                        labelClassName="company-form-group"
-                        // labelText=" (optional)"
-                        name="YearOfFoundation"
-                        type="text"
-                        errorClass="contact-form-error"
+                    )
+
+                }
+                </Select>
+
+                <Select
+                label="State Founded In"
+                labelClassName="company-form-group"
+                name="stateFoundIn"
+                errorClass="contact-form-error"
+                >
+                <option value="">Select</option>
+                {
+                    nigeraianStates.map((state, i) =>
+
+                        <Options 
+                        key = { i }
+                        { ...state }
                         />
 
-                    <span className="brand-name">
-                        Each information provided here and here off is owned by you and as such, 
-                        can be changed, modified or removed at any time under your settings. 
-                        Kindly go through our privacy policy if you haven't to know how
-                        we use your information or how you can manage your data. 
-                    </span>
+                    )
+                    
+                }
+                </Select>
 
-                    </div>
-                
-                <div className="getting-started-contact-buttons">
-                <div className="getting-started-contact-back-button">
-                    <button onClick={()=> props.goBack()}>
-                        Back
-                    </button>
-                </div>
-                
-                <div className="getting-started-contact-next-button">
-                    <button type="submit" >
-                     Continue
-                    </button>
-                </div>
+                <TextInput
+                label="Business/Brand Name"
+                labelClassName="company-form-group"
+                name="businessOrBrandName"
+                type="text"
+                errorClass="contact-form-error"
+                />
 
-                </div>
-                </Form>
-                </Formik>
+                <TextInput
+                label="Year of Foundation"
+                labelClassName="company-form-group"
+                name="YearOfFoundation"
+                type="text"
+                errorClass="contact-form-error"
+                />
+
+            <span className="brand-name">
+                Each information provided here and here off is owned by you and as such, 
+                can be changed, modified or removed at any time under your settings. 
+                Kindly go through our privacy policy if you haven't to know how
+                we use your information or how you can manage your data. 
+            </span>
+
+            </div>
+        
+            <div className="getting-started-contact-buttons">
+            <div className="getting-started-contact-back-button">
+                <button onClick={()=> props.goBack()}>
+                    Back
+                </button>
+            </div>
+            
+            <div className="getting-started-contact-next-button">
+                <button type="submit" >
+                    Continue
+                </button>
+            </div>
+
+            </div>
+        </Form>
+        </Formik>
     )
 
 }
