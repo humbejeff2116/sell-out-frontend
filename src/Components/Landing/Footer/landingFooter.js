58889abaf0f4c01d431c281c0071d4245023f618
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-26 21:29:22 +0100

    clean landingFooter component code

--- src/Components/Landing/Footer/landingFooter.js
+++ src/Components/Landing/Footer/landingFooter.js
@@ -1,20 +1,20 @@
 
-
-
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Links from '../../../Data/links';
 import './landingFooter.css';
 
 
+export default function LandingFooter({ footerClassName, ...props }) {
 
-export default function LandingFooter(props){
     const landingSocialLinks = Links.getFooterSocialLinks();
+
     return (
-        <footer className ={props.footerClassName ? props.footerClassName : 'landing-footer-container'}>
+
+        <footer className ={ footerClassName ? footerClassName : 'landing-footer-container' }>
             <div className="landing-footer-top">
                 <section className="landing-footer-main-section">
-                    <SellOut/>
+                    <Fling/>
                 </section>
                 <section className="landing-footer-main-section">
                     <Resources/>
@@ -33,29 +33,38 @@ export default function LandingFooter(props){
                 <nav className="landing-footer-social-nav" >              
                 {
                     landingSocialLinks.map((link, i) =>
+
                         <SocailLinks  key={i} {...link} />
+
                     )
                 }
                 </nav>
                 </section>
+
                 {/* copyright */}
                 <section className="landing-footer-copyright-section">
-                    <div className="landing-footer-copyright"> 
-                    <div className ="landing-footer-copyright-list">
+                    <div className="landing-footer-developer"> 
+                    <div className ="landing-footer-developer-list">
                         <span><a href="/humbe-jeffrey.netlify.app" target="_blank">Developed by Humbe Jeffrey</a></span> 
                     </div>
+                    </div>
+                </section>
+
+                <section className="landing-footer-copyright-section">
+                    <div className="landing-footer-copyright"> 
                     <div  className ="landing-footer-copyright-list">
-                        <span><a href='/#'>&copy;{ new Date().getFullYear()} Copyright @jeff.codes</a></span>
+                        <span><a href='/#'>&copy;{ new Date().getFullYear() } Copyright @jeff.codes</a></span>
                     </div>  
                     </div>
                 </section>
+
             </div>
         </footer>
     )
 }
 
 
-function SellOut(props) {
+function Fling(props) {
     return (
         <div className="landing-footer-main-content">
             <span className="landing-footer-content-header">Sell out</span>
@@ -68,6 +77,7 @@ function SellOut(props) {
 }
 
 function Resources(props) {
+
     return (
         <div className="landing-footer-main-content">
             <span className="landing-footer-content-header">Resources</span>
@@ -77,8 +87,11 @@ function Resources(props) {
 
         </div>
     )
+
 }
+
 function Policies(props) {
+
     return (
         <div className="landing-footer-main-content">
             <span className="landing-footer-content-header">Policies</span>
@@ -87,26 +100,38 @@ function Policies(props) {
             <span>Cookies</span>
         </div>
     )
+
 }
+
 function Help(props) {
+
     return (
+
         <div className="landing-footer-main-content">
              <span className="landing-footer-content-header">Help</span> 
             <span>Using Sell out</span>
             <span>Manage Account</span>
-            <span>Contact us</span>
-             
+            <span>Contact us</span>    
         </div>
+
     )
+
 }
 
+function SocailLinks({ href, title, icon, ...props }) {
 
-
-
-function SocailLinks(props) {
     return (
+
         <div className="landing-footer-social-item">
-          <Link to={props.href} title={props.title} className="landing-footer-social-link">  <i>{props.icon} </i> </Link>
+          <Link 
+          to = { href } 
+          title = { title } 
+          className="landing-footer-social-link"
+          >  
+          <i>{ icon }</i> 
+          </Link>
         </div> 
+
     )
+
 }
