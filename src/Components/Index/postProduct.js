714bb4e8796782c02973f14fd528e5252d821b2a
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-26 21:04:38 +0100

    re structure index postProduct component

--- src/Components/Index/postProduct.js
+++ src/Components/Index/postProduct.js
@@ -1,26 +1,25 @@
 
-
-
 import React from 'react';
 import {Link} from 'react-router-dom';
 
 
 export function PostProduct(props) {
     return (
-        <div className="index-post-container">
+        <section className="index-post-container">
             <div className="index-post-writeup-container">
-                <div className="index-post-writeup">
-                    <p>
-                        Got product('s) you wish to sell ? 
-                       {/* You can start here. */}
-                    </p>
-                    
 
+                <h4>Sell your products using Fling</h4>
+
+                <div className="index-post-writeup">     
+                Join this amazing community of enterprenuers, connect share ideas and sell your products? 
+                </div>
+
+                <div className="index-post-getting-started">
+                <Link to="/home/dashboard/store/upload-product">
+                    Get Started
+                </Link>
                 </div>
             </div>
-            <div className="index-post-upload">
-            <Link to="/home/dashboard/store/upload-product"><button>Sell Product</button></Link>
-            </div>
-        </div>
+        </section>
     )
 }
