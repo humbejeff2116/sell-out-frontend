9af788b6bb25bf0cf463a2ce9fe06b13973c9d7c
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-26 20:50:25 +0100

    redesign/develop and re-implement store products component and functionality

--- src/Components/UserDashboard/Store/Products/products.js
+++ src/Components/UserDashboard/Store/Products/products.js
@@ -1,12 +1,12 @@
 
-
-
 import React, { useState } from 'react';
-import { Redirect, useLocation, useHistory } from 'react-router-dom';
+import { Redirect, Link, useLocation, useHistory } from 'react-router-dom';
 import { DisplayedProduct } from '../../../Product/product';
 import { ModalBox } from '../../../ModalComments/modalComments';
 import { deleteProduct } from '../../../../Utils/http.services';
 import { BiTrash, BiEdit } from "react-icons/bi";
+import { FilterButtonComponent } from '../../../Index/filter';
+import Collections from '../Collections/collections';
 import useEditProductContext from '../../../../Context/EditProduct/context';
 import './products.css';
 
@@ -97,134 +97,268 @@ const mockProducts = [
 export default function StoreProducts(props) {
    
     const [deleteProductResponseMessage, setDeleteProductResponseMessage] = useState('');
-    const [redirect, setRedirect] = useState('')
+
+    const [showAllProducts, setShowAllProducts] = useState(true);
+
+    const [redirect, setRedirect] = useState('');
+
+    let storeProductsComponent;
+
+    const displayAllProducts = () => {
+
+        setShowAllProducts(true);
+            
+    }
+
+    const displayMyCollections = () => {
+
+        setShowAllProducts(false);
+
+    }
+
+    if (showAllProducts) {
+
+        storeProductsComponent = (
+
+            <AllStoreProducts
+            products = { mockProducts }
+            setDeleteProductResponseMessage = { setDeleteProductResponseMessage }
+            setRedirect = { setRedirect }
+            />
+        )
+
+    } else { 
+
+        storeProductsComponent = (
+
+            <Collections/>
+
+        )
+
+    }
     
     if (redirect) {
+
         return (
+
             <Redirect to = {redirect} />
+
         )
+
     }
+    
     return (
         
         <div className="placed-orders-container">
-            
-        <div className="placed-orders-header">
-            <h3> Store Products</h3>
-        </div>
-        <div className="store-products-search-container">
-            <div className="store-products-search">
-                <form>
-                    <label htmlFor="order-search"> Search by product name</label>
-                    <div className="store-products-search-input">
-                        <input type="text" />
-                        <button>Search</button>
+
+        <div className="store-products-stats-container">
+            <div className="store-products-stats-child left">
+
+                <div className="store-products-stats-showcase-product">
+                    <h3>List a product</h3>
+                    <div className="store-products-showcase-writeup">
+                     Showcase more of your products to potential cutomers
                     </div>
-                   
-                </form>
+                    <div className="store-products-showcase-link-wrapper">
+                        <Link to="/home/dashboard/store/upload-product">
+                            Upload Product Details
+                        </Link>
+                    </div>
+                </div>
+
+            </div>
+            <div className="store-products-stats-child">
+
             </div>
         </div>
 
-        <div className="store-products-container">
+        <div className="placed-orders-header">
+            <h3> Store Items</h3>
+        </div>
+
+        <div className="store-products-filter-container">
+            <div className="store-products-filter-nav">
+
+                <div 
+                className={ showAllProducts ? "store-products-filter-nav-item active" : "store-products-filter-nav-item"}
+                onClick = { displayAllProducts }
+                >
+                All products 
+                </div>
+
+                <div 
+                className={ !showAllProducts ? "store-products-filter-nav-item active" : "store-products-filter-nav-item"}
+                onClick = { displayMyCollections }
+                >
+                My collections  
+                </div>
+
+            </div>
+            <FilterButtonComponent
+            filterButtonClassName={"store-products-filter"}
+            filterIconClassName={"store-products-filter-icon"}
+            title={"Filter products"}
+            />
+        </div>
+
+        { storeProductsComponent }
+
+        </div>
+
+    )
+
+}
+
+export function AllStoreProducts({ 
+    products, 
+    storeContainerClassName, 
+    productEditPanel, 
+    setDeleteProductResponseMessage, 
+    setRedirect, 
+    ...props
+}) {
+    
+    return (
+
+        <div className={ storeContainerClassName || "store-products-container"}>
         {
-            mockProducts.map((product, i)=>
+            products?.length > 0 && products.map((product, i)=>
                 <StoreProduct 
-                key = {i}
-                product = {product}
-                setDeleteProductResponseMessage={setDeleteProductResponseMessage}
-                setRedirect={setRedirect}
-                deleteProduct = {deleteProduct}
+                key = { i }
+                product = { product }
+                setDeleteProductResponseMessage = { setDeleteProductResponseMessage }
+                setRedirect = { setRedirect }
+                productEditPanel = { productEditPanel }
                 />
             )
         }
         </div>
-        </div>
+
     )
+
 }
 
 
+export function StoreProduct({ setDeleteProductResponseMessage, productEditPanel, setRedirect, product }) {
 
-function StoreProduct({ setDeleteProductResponseMessage, setRedirect, product }) {
     const [showDeleteProductModal, setShowDeleteProductModal] = useState(false);
+
     const [deletingProduct, setDeletingProduct] = useState(false);
+
     const location = useLocation();
+
     const history = useHistory();
+
     const { setProductToEdit } = useEditProductContext();
+
     let deleteModalChild;
+
     const deleteStoreProduct = async (product) => {
         
         try {
+
             setDeletingProduct(true);
+
             const deleteProductResponse = await deleteProduct(product);
+
             setDeletingProduct(false);
+
             setShowDeleteProductModal(false);
+
             setDeleteProductResponseMessage(deleteProductResponse.message);
+
         } catch(err) {
         
 
-        }   
+        } 
+
     }
     const editProduct = (product) => {
 
         setProductToEdit([product]);
+
         history.push(location.pathname);
+
         setRedirect("/home/dashboard/store/edit-product");
+
     }
+
     const closeModal = () => {
+
         setShowDeleteProductModal(false);
+
     }
+
     const openModal =() => {
+
         setShowDeleteProductModal(true);
+
     }
+
     if (deletingProduct) {
+
         deleteModalChild = (
+
             <DeleteModalChildLoader />
+
         )
+
     } else {
+
         deleteModalChild = (
-            <DeleteModalChild removeProduct = {()=>deleteStoreProduct(product)} cancel={closeModal} />
+
+            <DeleteModalChild 
+            removeProduct = { ()=> deleteStoreProduct(product) } 
+            cancel = { closeModal } />
+
         )
 
     }
     
 
-   
     return (
-        <div className="store-product-edit-panel">
+        <div className={ productEditPanel || "store-product-edit-panel" }>
             {
                 (showDeleteProductModal) && (
+
                     <ModalBox 
-                    handleModal={closeModal} 
-                    modalContainerWrapperName={"store-products-modal-container-wrapper"}
-                    modalContainer={"store-products-modal-container"}
+                    handleModal = { closeModal } 
+                    // modalContainerWrapperName={"store-products-modal-container-wrapper"}
+                    modalContainer = { "store-products-modal-container" }
                     >
-                        {deleteModalChild}
+                        { deleteModalChild }
                     </ModalBox>
+
                 )
 
             }
             <div className="store-product-edit-icon-panel">
                 <div className="store-product-edit-group">
-                    <div className="store-product-edit-icon">
-                        <BiEdit title="Edit" className="store-icon" onClick={()=> editProduct(product)}/>
+                    <div className="store-product-edit-icon" onClick={ ()=> editProduct(product) }>
+                        <BiEdit title="Edit" className="store-icon" />
                     </div>
                 </div>
 
                 <div className="store-product-edit-group">
-                    <div className="store-product-edit-icon">
-                    <BiTrash title="Delete" className="store-icon" onClick={openModal}/>
+                    <div className="store-product-edit-icon" onClick = { openModal }>
+                    <BiTrash title="Delete" className="store-icon"/>
                     </div>
                 </div>
             </div>
             <DisplayedProduct 
-            product={product}
+            product = { product }
             panelClassName="store-product-panel"
             />
         </div>
+
     )
+
 }
 
-function DeleteModalChild({cancel, removeProduct}) {
+function DeleteModalChild({ cancel, removeProduct }) {
+
     return (
+
         <div className="store-products-modal-body-container">
         <div className="store-products-modal-content">
             <p>
@@ -232,21 +366,28 @@ function DeleteModalChild({cancel, removeProduct}) {
             </p>
         </div>
         <div className="store-products-modal-button-container">
-            <div className="store-products-modal-button">
-                <button onClick={cancel}>Cancel</button>
+            <div className="store-products-modal-button left">
+                <button onClick = { cancel }>Cancel</button>
             </div>
 
             <div className="store-products-modal-button">
-                <button onClick={removeProduct}>Delete</button>
+                <button onClick = { removeProduct }>Delete</button>
             </div>
         </div>
         </div>
+
     )
+
 }
-function DeleteModalChildLoader({onClick}) {
+
+function DeleteModalChildLoader({ onClick }) {
+
     return (
+
         <div className="store-products-modal-body-container">
         deleting...
         </div>
+
     )
+
 }
