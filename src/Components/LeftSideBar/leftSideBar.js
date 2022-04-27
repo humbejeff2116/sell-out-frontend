48fcc9c9f8112f34b175bb40751d3b54ecc4b645
humbejeff2116 <humbejeff2116@gmail.com>
2022-04-27 21:40:34 +0100

    refactor leftSidebar container className logic

--- src/Components/LeftSideBar/leftSideBar.js
+++ src/Components/LeftSideBar/leftSideBar.js
@@ -1,19 +1,28 @@
 
-
-
-
-
-
 import React from 'react';
 import './leftSideBar.css';
 
 
-
-
 export default function LeftSideBar(props) {
+
+    let leftSidebarClassName;
+
+    if (props.fixed && props.className) {
+
+        leftSidebarClassName = `${props.className} fixed`;
+
+    } else if(props.fixed && !props.className) {
+
+        leftSidebarClassName = `left-side-bar-container fixed`;
+
+    } else {
+
+        leftSidebarClassName = `left-side-bar-container`;
+
+    }
     
     return (
-        <section className={props.fixed ? "left-side-bar-container fixed": "left-side-bar-container" }>
+        <section className = { leftSidebarClassName }>
             <div className="left-side-bar-content">
                 <section className="left-side-bar-top">{props.top}</section>
                 <section className="left-side-bar-center">{props.center}</section>
