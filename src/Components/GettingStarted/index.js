a26f2c9ce5962daa68a86a448abec553fcaa74e5
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-01 07:51:00 +0100

    export gettingStarted components from index file

---
+++ src/Components/GettingStarted/index.js
@@ -0,0 +1,18 @@
+
+import GettingStarted from './gettingStarted';
+import ProfileImage from './ProfileImage/profileImage';
+import CompanyOrBusiness from './CompanyOrBusiness/companyOrBusiness';
+import LegalAdress from './LegalAddress/legalAddress';
+import ShippingAndOperations from './ShippingAndOperations/shippingAndOperations';
+import Confirmation from './Confirmation/confirmation';
+import './Template/template.css';
+import './ProfileImage/profileImage.css';
+
+export {
+    GettingStarted,
+    ProfileImage,
+    CompanyOrBusiness,
+    LegalAdress,
+    ShippingAndOperations,
+    Confirmation
+}
