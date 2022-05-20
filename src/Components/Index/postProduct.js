6add9ffd67fd9aa054751a9f9043bbc7a5e321f2
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-20 19:49:04 +0100

    modify post product component write up

--- src/Components/Index/postProduct.js
+++ src/Components/Index/postProduct.js
@@ -1,23 +1,21 @@
 
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 export function PostProduct(props) {
     return (
         <section className="index-post-container">
             <div className="index-post-writeup-container">
-
                 <h4>Sell your products using Fling</h4>
-
                 <div className="index-post-writeup">     
-                Join this amazing community of enterprenuers, connect share ideas and sell your products? 
+                    Join this amazing community of enterprenuers, connect 
+                    share ideas and make cool cash while selling your products 
                 </div>
-
                 <div className="index-post-getting-started">
-                <Link to="/home/dashboard/store/upload-product">
-                    Get Started
-                </Link>
+                    <Link to="/home/dashboard/store/upload-product">
+                        Get Started
+                    </Link>
                 </div>
             </div>
         </section>
