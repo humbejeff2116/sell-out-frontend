6ccc2aad883df8a6acb041fbc8f587433150467c
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-22 10:02:46 +0100

    re structure NotFound component

--- src/Components/NotFound/notFound.js
+++ src/Components/NotFound/notFound.js
@@ -1,26 +1,30 @@
 
-
-
-
-
-
-
-
 import React from 'react';
+import eror404Image from '../../Images/error404.png';
 import './notFound.css';
 
 
-export default function NotFound() {
+export default function NotFound({ usedForHomeRoutes }) {
+
+    let wrapperClassName = usedForHomeRoutes ? "page-notfound-wrapper home-routes" : "page-notfound-wrapper";
+
     return (
-        <div>
-            <div>
-            <h2>
-                 Seems resource not found at that address, but nothing to worry about
-            </h2>
-            </div>
-            <p>
-               A good reason could be that The link you entered or followed is either broken, removed or does not exist
-            </p>             
+
+        <div className= "page-notfound-container">
+            <div className= { wrapperClassName }>
+                <div className="page-notfound-details">
+                    <div className="page-notfound-image">
+                        <img src={ eror404Image } alt=""/>
+                    </div>
+                    <h3 className="page-notfound-heading">
+                        Oops! We could not find that page.
+                    </h3>
+                    <div className="page-notfound-para">
+                        The link you entered or followed is either broken, removed or does not exist.
+                    </div>
+                </div>
+            </div>         
         </div>
+
     )
 }
