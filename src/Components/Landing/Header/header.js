8777beceb534091584e69e6c88dfd94e3e912409
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-22 09:44:26 +0100

    replace mobile menu icon and clean landing Header component code

--- src/Components/Landing/Header/header.js
+++ src/Components/Landing/Header/header.js
@@ -1,38 +1,36 @@
 
-
-
-
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { BiMenu } from "react-icons/bi";
+import Links from '../../../Data/links';
 import fling from '../../../Images/fling8.png';
-import Links, { open } from '../../../Data/links';
 import './header.css';
-const landingMainLinks = Links.getLandingMainLinks()
 
+const landingMainLinks = Links.getLandingMainLinks();
 
-
-export default function Header(props) {
+export default function Header({ showLoginModal, ...props }) {
+   
     return (
         <header className="landing-header">
-            {/* logo */}
             <div className="landing-header-logo">
-                <div  className="landing-header-logo-img"><img src={fling} alt="fling" /></div>
+                <div  className="landing-header-logo-img">
+                    <img src={ fling } alt="fling" />
+                </div>
             </div>
-            {/* main navigation */}
             <div className="landing-header-navigation">
                 <nav>
                 {
                     landingMainLinks.map((link, i) =>
-                        <NavLinks key={i} {...link} />
+
+                        <NavLinks key={ i } { ...link }/>
+
                     )
                 }
                 </nav>
             </div>
-            {/* login/signup */}
             <div className="landing-header-login">
                 <div className="landing-login-item" >
-                    <button onClick={()=> props.showLoginModal(true)}>Login</button>
+                    <button onClick={ ()=> showLoginModal(true) }>Login</button>
                     <MobileNavIcon/>
                 </div>
             </div>
@@ -40,29 +38,29 @@ export default function Header(props) {
     )
 }
 
-export function MobileNavIcon(props) {
+export function MobileNavIcon({ ...props }) {
     return (
+
         <div className="landing-header-mobile-nav-container">
-           {/* <div className="header-mobile-nav-search-icon">
-               {open}
-           </div> */}
-           <div className="landing-header-mobile-nav-icon">
-           {open}
-           </div>
+            <div className="landing-header-mobile-nav-icon">
+                <BiMenu className="nav-icon"/>
+            </div>
         </div>
+
     )
 }
 
-function NavLinks(props) {
+function NavLinks({ href, name, ...props }) {
     return (
         <div className="landing-nav-item" >
             <NavLink
             exact 
-            to={props.href} 
+            to={ href } 
             activeClassName="landing-nav-link-active"
             className="landing-nav-link" 
-            title={props.name} >
-                {props.name} 
+            title={ name } 
+            >
+                { name } 
             </NavLink> 
         </div>
     ) 
