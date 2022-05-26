81ac0e901aa7891eee2139d5068a0bb07f645878
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-26 12:45:44 +0100

    refactor cartpage to use IndexTemplateChildrenWithFooterAndNoRightSideBar instead of SettingsPageTemplate

--- src/Pages/CartPage/cartPage.js
+++ src/Pages/CartPage/cartPage.js
@@ -1,12 +1,6 @@
 
-
-
-
-
 import React, { useEffect } from 'react';
-import { SettingsPageTemplate } from '../../Components/Template/template';
-import IndexSideNav from '../../Components/IndexSideNav/indexSideNav';
-import IndexFooter from '../../Components/IndexFooter/indexFooter';
+import { IndexTemplateChildrenWithFooterAndNoRightSideBar } from '../../Components/Template/template';
 import Cart from '../../Components/Cart/cart';
 import RequireAuthentication from '../../Components/Authentication/requireAuthentication';
 import { isAuthenticated } from '../../Services/services';
@@ -15,14 +9,12 @@ import { isAuthenticated } from '../../Services/services';
     useEffect(()=> {
         window.scrollTo(0,0);
     },[]);
+
     return (
-        <SettingsPageTemplate
-        leftSideBarCenter={<IndexSideNav/>}
-       leftSideBarBottom={<IndexFooter/>}
-        >
+        <IndexTemplateChildrenWithFooterAndNoRightSideBar>
             <Cart/> 
-        </SettingsPageTemplate>
-    )
+        </IndexTemplateChildrenWithFooterAndNoRightSideBar>
+    )    
 }
 
 const CartPage = RequireAuthentication(CartPageComp, isAuthenticated);
