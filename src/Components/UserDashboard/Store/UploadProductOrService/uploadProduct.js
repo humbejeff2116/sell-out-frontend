73fe92d4893a39970050e5f1dbbc9f98430e8c8e
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-26 12:24:09 +0100

    implement image preview/edit functionality, refactor UploadProduct component, extract UploadProductDetailsTemplate component

--- src/Components/UserDashboard/Store/UploadProductOrService/uploadProduct.js
+++ src/Components/UserDashboard/Store/UploadProductOrService/uploadProduct.js
@@ -1,308 +1,215 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Redirect, useLocation, useHistory  } from 'react-router-dom';
+import { RiEyeLine } from 'react-icons/ri';
 import ProductImagesSelector from './ImageSelector/imageSelector';
 import UploadProductDetailsForm from './ProductDetailsForm/productDetailsForm';
-import { createProduct } from '../../../../Utils/http.services';
+import ImageEditor from './ImageEditor/imageEditor';
 import useUploadProductContext from '../../../../Context/UploadProductContext/context';
 import useAuth from '../../../../Context/context';
+import { createProduct } from '../../../../Utils/http.services';
 import './uploadProduct.css';
 
-
 export default function UploadProduct() {
-
     const [uploadingProduct , setUploadingProduct ] = useState(false);
-
     const [uploadingError , setUploadingError] = useState(false);
-
     const [response, setResponse] = useState('');
-
     const [redirect, setRedirect] = useState('');
-
     const [formValues, setFormValues] = useState({});
-
     const [resizedImages, setResizedImages] = useState([]);
-
     const [noProductImageHasBeenSelected, setNoProductImageHasBeenSelected] = useState(false);
-
+    const [showImageEditorModal, setShowImageEditorModal] = useState(false);
     const { user } = useAuth();
-
     const { productValues, setProductFormValues } = useUploadProductContext();
-
     const location = useLocation();
-
     const history = useHistory();
+    let isMounted = useRef(false);
+
+    useEffect(()=> {
+        isMounted.current = true;
+
+        return ()=> {
+            isMounted.current = false;
+        }
+    }, []);
    
     useEffect(() => {
-
         let timer = null;
 
-        let mounted = true;
-
-        if (response && mounted) {
-
+        if (response && isMounted.current) {
             timer = setTimeout(() => {
-
                 setResponse('');
-
                 history.push(location.pathname);
-
                 setRedirect("/home/dashboard/store/products");
-
             }, 3000);
-
         }
 
         return () => {
-
-            mounted = false;
-
             if (timer) {
-
                 clearTimeout(timer);
-
             }
-
         }
-
     }, [response, history, location]);
-
-    useEffect(()=> {
-
-        if (formValues.productImages && formValues.productImages.length > 0) { 
-
-            getResizedImages(formValues.productImages, setResizedImages);
-
-        }
-
-    },[formValues.productImages])
 
 
     const handleImageInputChange = (e) =>  setFormValues({ [e.target.name] : [...e.target.files ] }); 
 
-    const getResizedImages = async (imageFiles = [], callback = f =>f) => {
-
-        const imagesDataUrl = [];
-
-        try {
-
-            for (let i = 0; i < imageFiles.length; i++) { 
-
-                const dataUrl = await resizeImage(imageFiles[i]);
-
-                imagesDataUrl.push(dataUrl);
-
-            }
-
-            return callback(imagesDataUrl)
-            
-        } catch (error) {
-            // TODO... handle error
-            
-        }
-        
-        function resizeImage(imageFile = {}) {
-
-            return new Promise((res, rej) => {
-            
-                try {
-
-                    const reader = new FileReader();
-
-                    // Set the image once loaded into file reader
-                    reader.onload = function(e) {
-
-                        const img = document.createElement("img");
-
-                        img.src =  e.target.result;
-
-                        // resize image once it loads
-                        img.addEventListener("load", (evt) => {
-
-                            const canvas = document.createElement("canvas");
-
-                            const ctx = canvas.getContext("2d");
-
-                            let width = img.width;
-
-                            let height = img.height;
-
-                            let dataurl = null;
-
-                            const MAX_WIDTH = 300;
-
-                            const MAX_HEIGHT = 300; 
-    
-                            if (width > height) {
-
-                                if (width > MAX_WIDTH) {
-
-                                    height *= MAX_WIDTH / width;
-
-                                    width = MAX_WIDTH;
-
-                                }
-
-                            } else {
-
-                                if (height > MAX_HEIGHT) {
-
-                                    width *= MAX_HEIGHT / height;
-
-                                    height = MAX_HEIGHT;
-
-                                }
-
-                            }
-
-                            canvas.width = width;
-
-                            canvas.height = height;
-
-                            ctx.drawImage(img, 0, 0, width, height);
-
-                            dataurl = canvas.toDataURL(imageFile.type);
-
-                            // resolve resized image dataurl
-                            res({dataurl});
-
-                        })
-
-                    }
-
-                    reader.readAsDataURL(imageFile);
-
-                } catch(err) {
-
-                    rej(err);
-
-                }
-
-            })
-
-        }
-
-    }
 
     const handleSubmit = async (values) => {
-
         const { productImages:files } = formValues;
-
         const formData = new FormData();
 
+        if (!user) {
+            // TODO... remove alert
+            alert("no user")
+            return;
+        }
+
+        if (!files) {
+            setNoProductImageHasBeenSelected(true)
+            return;
+        }
+
+        setUploadingError(false);
+        setUploadingProduct(true);
+        setResponse('');
+        // TODO... remove console.log
+        console.log("files are", files)
+
+        for (let i = 0; i < files.length; i++) { 
+            formData.append(`file${i}`, files[i]) 
+        }
+
+        for (let keys in user) { 
+            formData.append(keys, user[keys]) 
+        }
+
+        for (let keys in values) { 
+            formData.append(keys, values[keys]) 
+        }
+
         try {
-
-            if(!user) {
-                // TODO... remove alert
-                alert("no user")
-
-                return;
-
-            }
-
-            if (!files) {
-                setNoProductImageHasBeenSelected(true)
-                return;
-            }
-
-            setUploadingError(false);
-
-            setUploadingProduct(true);
-
-            setResponse('');
-
-            // TODO... remove console.log
-            console.log("files are", files)
-
-            for (let i = 0; i < files.length; i++) { formData.append(`file${i}`, files[i]) }
-
-            for (let keys in user) { formData.append(keys, user[keys]) }
-
-            for (let keys in values) { formData.append(keys, values[keys]) }
-
             const { error } = await createProduct(formData);
 
             if (error) {
-
                 setUploadingError(true);
-
                 setResponse("An error occured while uploading product details. Please wait and try again");
-
                 return;
-
             } 
 
             setResponse("Product added successfully");
-
             return;
-
-        } catch(e) {
-
+        } catch(err) {
+            // TODO... remove console.log
+            console.error(err)
             setUploadingError(true);
-
             setResponse("An error occured please wait and try again"); 
-
         }
-
     }
+
+    const closeModal = () => {
+        setShowImageEditorModal(false);
+    }
+
+    const openProductImagesEditor = () => {
+        setShowImageEditorModal(true);
+    }
+
+    const ProductPreviewImagesButtonComp = (
+        <PreviewImagesButton
+        numberofimages = { formValues?.productImages?.length  || 0  }
+        handleButtonClick = { openProductImagesEditor }
+        />
+    )
 
     if (redirect) {
-
         return (
-
-            <Redirect to={redirect} />
-
+            <Redirect to={ redirect }/>
         )
-
     }
 
-    return (
-       
+    return ( 
          <div className="upload-product-container">
             <div className="upload-details-heading">
                 <h3> Upload Product</h3>
             </div>
             <div className="upload-form-container">
-            <UploadFormTemplate
-            formComponent = {
-
-                <UploadProductDetailsForm 
-                handleSubmit = { handleSubmit }  
+                {
+                    showImageEditorModal && (
+                        <ImageEditor
+                        closeModal = { closeModal }
+                        images = { formValues?.productImages }
+                        />
+                    )
+                }
+                <UploadFormTemplate
+                formComponent = {
+                    <UploadProductDetailsForm 
+                    handleSubmit = { handleSubmit }  
+                    />
+                }
+                imageSelectorComponent = { 
+                    <ProductImagesSelector
+                    handleInputChange = { handleImageInputChange }
+                    numberofimages = { formValues?.productImages?.length  || 0  }
+                    noProductImageHasBeenSelected = { noProductImageHasBeenSelected }
+                    previewImagesButton = { ProductPreviewImagesButtonComp }
+                    />
+                }
                 />
-
-            }
-            imageSelector = { 
-
-                <ProductImagesSelector
-                handleInputChange = { handleImageInputChange }
-                numberofimages = { formValues?.productImages?.length  || 0  }
-                noProductImageHasBeenSelected = { noProductImageHasBeenSelected }
-                />
-            }
-            />
             </div>
         </div>
     )
 }
 
+function UploadFormTemplate({
+    formComponent, 
+    imageSelectorComponent, 
+    ...props
+}) {
+    return (
+        <>
+            <div className="upload-form-panel">
+                <div className="upload-form-panel-left">
+                    { formComponent }
+                </div>
+                <div className="upload-form-panel-right">
+                    { imageSelectorComponent }
+                </div>
+            </div>
+        </>   
+    )
+}
 
-function UploadFormTemplate(props) {
+function PreviewImagesButton ({
+    numberofimages, 
+    handleButtonClick, 
+    ...props
+}) {
+    const buttonText = numberofimages > 1 ?  "Preview images" : "Preview image";
 
     return (
-
-        <>
-        <div className="upload-form-panel">
-
-            <div className="upload-form-panel-left">
-            { props.formComponent }
-            </div>
-
-            <div className="upload-form-panel-right">
-            { props.imageSelector }
-            </div>
-
+        <div className="file-input-preview-button-container">
+           <div className="file-input-preview-button-wrapper">
+               <button type ="button" onClick = { handleButtonClick }>
+                   <RiEyeLine className="file-input-preview-button-icon"/>
+                   { buttonText }
+                </button>
+           </div>
         </div>
-        </>
-        
     )
+}
 
+export function UploadProductDetailsTemplate({
+    formComponent, 
+    imageSelectorComponent, 
+    ...props 
+}) {
+    return (
+        <div className="upload-form-container">
+            <UploadFormTemplate
+            formComponent = { formComponent }
+            imageSelectorComponent = { imageSelectorComponent }
+            />
+        </div>
+    )
 }
