fc8d21501b0aa0e87655a8befaef44263c20783e
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-26 12:47:35 +0100

    refactor CheckoutPage to use IndexTemplateChildrenWithFooterAndNoRightSideBar instead of IndexPageTemplate

--- src/Pages/CheckoutPage/checkoutPage.js
+++ src/Pages/CheckoutPage/checkoutPage.js
@@ -1,27 +1,17 @@
 
-
-
-
-
-import React, {useEffect} from 'react';
-import { IndexPageTemplate} from '../../Components/Template/template';
-import IndexSideNav from '../../Components/IndexSideNav/indexSideNav';
-import IndexFooter from '../../Components/IndexFooter/indexFooter';
+import React, { useEffect } from 'react';
+import { IndexTemplateChildrenWithRightSideBar } from '../../Components/Template/template';
 import Checkout from '../../Components/Checkout/checkout';
 import RequireAuthentication from '../../Components/Authentication/requireAuthentication';
-
 
  function CheckoutPageComp() {
     useEffect(()=> {
         window.scrollTo(0,0);
     },[]);
     return (
-        <IndexPageTemplate
-        leftSideBarCenter={<IndexSideNav/>}
-        leftSideBarBottom={<IndexFooter/>}
-        >
+        <IndexTemplateChildrenWithRightSideBar>
             <Checkout/> 
-        </IndexPageTemplate>
+        </IndexTemplateChildrenWithRightSideBar>
     )
 }
 
