e30c5aa3c8faf8dae14b026f24754771e11eb41b
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-28 14:45:45 +0100

    add HomeRoutesUsingIndexTemplate to appRoutes

--- src/Routes/appRoutes.js
+++ src/Routes/appRoutes.js
@@ -7,7 +7,7 @@ import {
     LoginAndSignupSuspenseLoader,
     GettingStartedSuspenseLoader 
 } from '../Components/SuspenseLoader/suspenseLoader';
-import HomeRoutes from './homeRoutes'
+import HomeRoutesUsingIndexTemplate from './homeRoutes';
 
 const LandingPage = React.lazy(()=> import('../Pages/LandingPage/landingPage'));
 const LoginPage = React.lazy(()=> import('../Pages/LoginPage/loginPage'));
@@ -16,16 +16,16 @@ const NotFoundPage = React.lazy(()=> import('../Pages/NotFoundPage/notFoundPage'
 const GettingStartedPage = React.lazy(()=> import('../Pages/GettingStartedPage/gettingStartedPage'));
 
 const appRoutes = [
-    {path: "/", exact: true, suspense: true,SuspenseComponent: OutsideLoginSuspenseLoader, Component: LandingPage, },
-    {path: "/login",  suspense: true,SuspenseComponent: LoginAndSignupSuspenseLoader, Component: LoginPage, },
-    {path: "/signup", suspense: true,  SuspenseComponent: LoginAndSignupSuspenseLoader, Component: SignupPage,},
-    {path: "/home",  suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: HomeRoutes, },
-    {path: "/getting-started", suspense: true,SuspenseComponent: GettingStartedSuspenseLoader, Component: GettingStartedPage, },
-    {path: "/*", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: NotFoundPage,},
+    {path: "/", exact: true, suspense: true, SuspenseComponent: OutsideLoginSuspenseLoader, Component: LandingPage},
+    {path: "/login", suspense: true, SuspenseComponent: LoginAndSignupSuspenseLoader, Component: LoginPage},
+    {path: "/signup", suspense: true, SuspenseComponent: LoginAndSignupSuspenseLoader, Component: SignupPage},
+    {path: "/home", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: HomeRoutesUsingIndexTemplate},
+    {path: "/getting-started", suspense: true,SuspenseComponent: GettingStartedSuspenseLoader, Component: GettingStartedPage},
+    {path: "/*", suspense: true, SuspenseComponent: OutsideLoginSuspenseLoader, Component: NotFoundPage},
 ]
 
-export default function AppRoutes(props){
+export default function AppRoutes({ routes, ...props }) {
     return (
-        <Router routes={props.routes || appRoutes} />
+        <Router routes={ routes || appRoutes }/>
     )
 }
