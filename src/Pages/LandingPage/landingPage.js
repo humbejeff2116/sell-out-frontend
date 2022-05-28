eb6d4efc2015eea456ea334f5be11cb4f1e7d81d
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-28 13:52:45 +0100

    refactor landingPage to use LandingTemplate

--- src/Pages/LandingPage/landingPage.js
+++ src/Pages/LandingPage/landingPage.js
@@ -1,22 +1,22 @@
 
-
-
-
-
-
-
-import React, { useEffect} from 'react';
-import LandingComponent from '../../Components/Landing/landing';
-
-
+import React, { useEffect } from 'react';
+import { 
+    LandingTemplate, 
+    LandingProducts, 
+    TopInfoCardComponent 
+} from '../../Components/Landing/landing';
 
 
 export default function LandingPage() {
     useEffect(()=> {
-        window.scrollTo(0,0)       
+        window.scrollTo(0,0)          
     }, []);
 
     return (
-        <LandingComponent/>
+        <LandingTemplate
+        landingTopChild ={ <TopInfoCardComponent/> }
+        landingCenterChild = { <LandingProducts/> }
+        >     
+        </LandingTemplate>
     )
 }
