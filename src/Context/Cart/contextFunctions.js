10f0b0bd700a6ba4cd374fb740e58c4cccd97e51
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-28 17:30:55 +0100

    fix createSellerPaymentData bug and clean code

--- src/Context/Cart/contextFunctions.js
+++ src/Context/Cart/contextFunctions.js
@@ -1,383 +1,256 @@
 
+import { calculateSellerProductsSoldTotalAmount } from './Lib/library';
+
+
 async function addProductToCart(state = [], action) {
-
-    let productId = action.productId;
-
-    let productQty = action.productQty;
-
-    let sellerEmail = action.userEmail;
-
+    const {
+        userName,
+        userId,
+        userEmail,
+        userProfileImage,
+        productId,
+        productName,
+        productImages,
+        productPrice,
+        percentageOff,
+        productQty,
+        productSize,
+    } = action
     let sellerExist = false;
-
     let sellerProductAlreadyExist = false;
-
     let sellerProducts;
+    let i = 0;
+    let len = state.length;
 
     if (state.length) {
-
-        for (let i = 0; i < state.length; i++) {
-
-            if ( state[i].sellerEmail === sellerEmail) {
-
+        for (i; i < len; i++) {
+            if ( state[i].sellerEmail === userEmail) {
                 sellerExist = true;
-
-                sellerProducts = state[i].productsUserBoughtFromSeller;
-
+                sellerProducts = state[i].products;
                 break;
-
             } 
-
         }
 
         if (sellerExist) {
+            let i = 0
+            let len = sellerProducts.length
 
-            for (let i = 0; i < sellerProducts.length; i++) {
-
+            for (i; i < len; i++) {
                 if (sellerProducts[i].productId === productId) {
-
                     sellerProductAlreadyExist = true;
-
                     sellerProducts[i].productQty += productQty;
-
                     return state;
-
                 }
-
             }
 
             if (!sellerProductAlreadyExist) {
-
                 sellerProducts[sellerProducts.length] = { 
-                    sellerName: action.userName,
-                    sellerEmail: action.userEmail,
-                    sellerProfileImage: action.userProfileImage,
-                    productId: action.productId,
-                    productName: action.productName,
-                    productImages: action.productImages,
-                    productPrice: action.productPrice,
-                    percentageOff: action.percentageOff,
-                    productQty: action.productQty,
-                    productSize: action.productSize,
+                    productId,
+                    productName,
+                    productImages,
+                    productPrice,
+                    percentageOff,
+                    productQty,
+                    productSize,
                 }
 
                 return state;
-
             }
-
         }
-
     }
 
     // add new cart product if seller does not exist
     return [...state, { 
-        sellerName: action.userName,
-        sellerId: action.userId,
-        sellerEmail: action.userEmail,
+        sellerName: userName,
+        sellerId: userId,
+        sellerEmail: userEmail,
+        sellerProfileImage: userProfileImage,
         productsDelivered: false,
-        productsUserBoughtFromSeller:[{
-            sellerName: action.userName,
-            sellerEmail: action.userEmail,
-            sellerProfileImage: action.userProfileImage,
-            productId: action.productId,
-            productName: action.productName,
-            productImages: action.productImages,
-            productPrice: action.productPrice,
-            percentageOff: action.percentageOff,
-            productQty: action.productQty,
-            productSize: action.productSize,  
+        products:[{
+            productId,
+            productName,
+            productImages,
+            productPrice,
+            percentageOff,
+            productQty,
+            productSize,  
         }],
     }]
-
 }
 
 async function removeProductFromCart(state = [], action) {
-
-    let productId = action.productId;
-
-    let sellerEmail = action.sellerEmail;
-
+    const { productId, sellerEmail } = action;
     let sellerProducts;
-
     let sellerExist = false;
+    let i = 0;
+    let len = state.length 
 
-    for (let i = 0; i < state.length; i++) {
-
+    for (i; i < len; i++) {
         if (state[i].sellerEmail === sellerEmail) {
-
             sellerExist = true;
-
-            sellerProducts = state[i].productsUserBoughtFromSeller;
-
+            sellerProducts = state[i].products;
             break;
-
         }   
-
     }
-
     //  return initial state if seller does not exist
     if (!sellerExist || !sellerProducts.length > 0) {
-
         return state;
-
     }
-
     //   filter product with product id
     const newSellerProducts = sellerProducts.filter(product => {
-
         return product.productId !== productId;
-
     })
-
     //  loop through the cart state and attach the new filtered seller product
     if (newSellerProducts.length < 1) {
-
         const newState = state.filter(product => {
-
             return product.sellerEmail !== sellerEmail
-
         })
 
         return newState;
-
     }
 
-    for (let i = 0; i < state.length; i++) {
-
+    for (i = 0; i < len; i++) {
         if (state[i].sellerEmail === sellerEmail) {
-
-            state[i].productsUserBoughtFromSeller = newSellerProducts;
-
+            state[i].products = newSellerProducts;
             break;
-
-
         }   
     }
 
     return state;
-
 }
     
 async function addCartProductQuantity(state = [], action) {
-
-    let productId = action.productId;
-
-    let sellerEmail = action.sellerEmail;
-
-    let updatedProductQuantity = action.productQty;
-
+    const { productId, sellerEmail, updatedProductQuantity } = action;
     let sellerProducts;
-
     let sellerExist = false;
+    let i = 0;
+    let len = state.length
 
-    if (updatedProductQuantity < 1) {
-
-        return state;
-
-    }
-
-    for (let i = 0; i < state.length; i++) {
-
+    if (updatedProductQuantity < 1) return state;
+    
+    for (i; i < len; i++) {
         if (state[i].sellerEmail === sellerEmail) {
-
             sellerExist = true;
-
-            sellerProducts = state[i].productsUserBoughtFromSeller;
-
+            sellerProducts = state[i].products;
             break;
-
         }
-
     }
 
-    //  return initial state if seller does not exist
     if (!sellerExist || !sellerProducts.length) {
-
         return state;
-
     }
-
     //   loop through seller products and add quantity
-    for (let i = 0; i < sellerProducts.length; i++) {
-
+    for (i = 0; i < sellerProducts.length; i++) {
         if (sellerProducts[i].productId === productId) {
-
             sellerProducts[i].productQty += updatedProductQuantity || 1;
-
             break;
-
         }
-
     }
 
     return state;
-
 }
     
 async function reduceCartProductQuantity(state=[], action) {
-
-    let productId = action.productId;
-
-    let sellerEmail = action.sellerEmail;
-
-    let productQuantity = action.productQty;
-
+    const { productId, sellerEmail, productQuantity } = action;
     let sellerProducts;
-
     let sellerExist = false;
+    let i = 0;
+    let len = state.length;
 
-    if (productQuantity < 1) {
+    if (productQuantity < 1) return state;
 
-        return state;
-
-    }
-
-    for (let i = 0; i < state.length; i++) {
-
+    for (i; i < len; i++) {
         if (state[i].sellerEmail === sellerEmail) {
-
             sellerExist = true;
-
-            sellerProducts = state[i].productsUserBoughtFromSeller;
-
+            sellerProducts = state[i].products;
             break;
-
         } 
-
     }
 
-    //  return initial state if seller does not exist
-    if (!sellerExist || !sellerProducts.length) {
-
-        return state;
-
-    }
-
-    //   loop through seller products and add quantity
-    for (let i = 0; i < sellerProducts.length; i++) {
-
-        if ( (sellerProducts[i].productId === productId) && !(productQuantity > sellerProducts[i].productQty) ) {
-            
+    if (!sellerExist || !sellerProducts.length)  return state;
+    //   loop through seller products and reduce quantity
+    for (i = 0; i < sellerProducts.length; i++) {
+        if ((sellerProducts[i].productId === productId) && !(productQuantity > sellerProducts[i].productQty)) { 
             if( sellerProducts[i].productQty <= 1) {
-
                 break;
-
             }
 
             sellerProducts[i].productQty -= 1;
-
             break;
-
         }
-
     }
 
     return state;
-
 }
     
 async function clearCart(state = [], action) {
-
     return state = [];
-
 }
     
 function calculateCartTotalPrice(state = []) {
-
-    let allCartProducts = state.flatMap(product => product.productsUserBoughtFromSeller);
-    
+    const allCartProducts = state.flatMap(({ products }) => products); 
     let totalCartSum = 0.00;
+    let i = 0;
+    let len = allCartProducts.length
 
-    for (let i = 0; i < allCartProducts.length; i++) {
-
+    for (i; i < len; i++) {
         if (allCartProducts[i].percentageOff) {
-
-            const percentOffPrice = (allCartProducts[i].percentageOff / 100) * Number(allCartProducts[i].productPrice)
-
-            const newPrice = Number(allCartProducts[i].productPrice) - percentOffPrice;
-
+            const percentOffPrice = (allCartProducts[i].percentageOff / 100) * parseFloat(allCartProducts[i].productPrice)
+            const newPrice = parseFloat(allCartProducts[i].productPrice) - percentOffPrice;
             totalCartSum += newPrice * allCartProducts[i].productQty;
-
         } else {
-
             totalCartSum += allCartProducts[i].productPrice * allCartProducts[i].productQty;
-        }
-       
-       
+        }  
     }
 
-    return totalCartSum.toFixed(2);
-
+    return totalCartSum.toFixed(2, 10);
 }
     
     
 
 function createSellerPaymentData(state = [], buyer) {
-
-    function calculateIndividualSellerPriceSum(product) {
-
-        let total = 0;
-
-        for (let i = 0; i < product.productsUserBoughtFromSeller.length; i++) {
-
-            
-            total += product.productsUserBoughtFromSeller[i].productPrice * product.productsUserBoughtFromSeller[i].productQty
-        
-        }
+    const sellersProductSoldTotalAmount = state.map(cartProduct => {
+        const total = calculateSellerProductsSoldTotalAmount(cartProduct);
+        const { sellerName, sellerId, sellerEmail, products } = cartProduct;
+        const { fullName, id, userEmail }  = buyer;
 
         return ({
-            sellerName: product.sellerName,
-            sellerId: product.sellerId,
-            sellerEmail: product.sellerEmail,
-            buyerName: buyer.fullName,
-            buyerId: buyer.id,
-            buyerEmail: buyer.userEmail,
-            productsSellerSold: product.productsUserBoughtFromSeller,
+            sellerName,
+            sellerId,
+            sellerEmail,
+            buyerName: fullName,
+            buyerId: id,
+            buyerEmail: userEmail,
+            products,
             totalAmount: total
         })
-        
-    } 
-
-    const individualTotal = state.map(product => {
-
-       
-        return calculateIndividualSellerPriceSum(product);  
-    
     });
     
-    return individualTotal;
-
+    return sellersProductSoldTotalAmount;
 }
 
 function calculateTotalNumberOfProductsInCart(state = []) {
-
     let totalProducts = 0;
-
-    let allCartProducts = state.flatMap(product => product.productsUserBoughtFromSeller);
+    const allCartProducts = state.flatMap(({ products }) => products);
     
-    totalProducts += allCartProducts.length;
-    
+    totalProducts += allCartProducts.length;   
     return totalProducts;
-
 }
 
 async function createOrderData(productsUserBought, sellerPaymentData, user, orderId, orderTime) {
-    
-    const newCartState = productsUserBought.filter(product => product.productsUserBoughtFromSeller.length > 0);
+    const newCartState = productsUserBought.filter(({ products }) => products.length > 0);
+    let i = 0;
+    let newCartStateLen = newCartState.length;
+    let sellerPaymentDataLen = sellerPaymentData.length;
 
-    for (let i = 0; i < newCartState.length; i++) {
-
+    for (i; i < newCartStateLen; i++) {
         newCartState[i].orderTime = orderTime;
-
         newCartState[i].orderId = orderId;
-
     }
 
-    for (let i = 0; i < sellerPaymentData.length; i++) {
-
+    for (i = 0; i < sellerPaymentDataLen; i++) {
         sellerPaymentData[i].orderTime = orderTime;
-
         sellerPaymentData[i].orderId = orderId
-
     }
 
     return ({
@@ -385,7 +258,6 @@ async function createOrderData(productsUserBought, sellerPaymentData, user, orde
         order: newCartState,
         payments: sellerPaymentData
     })
-
 }
     
 export {
