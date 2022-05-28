eef2f12f3e336379ffd3f8792fd24f0285f42b19
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-28 14:41:34 +0100

    rename HomeRoutes to HomeRoutesUsingIndexTemplate, modify componentv functionality to use RouterwithTemplate and add messages route

--- src/Routes/homeRoutes.js
+++ src/Routes/homeRoutes.js
@@ -1,46 +1,53 @@
 
 import React from 'react';
-import Router from '../Components/Router/router';
-import {
-    SettingsSuspenseLoader, 
-    InsideLoginSuspenseLoader, 
-} from '../Components/SuspenseLoader/suspenseLoader';
-import LogoutPage from  '../Pages/LogoutPage/logoutPage'
+import { RouterWithTemplate } from '../Components/Router/router';
+import { IndexPageTemplate } from '../Components/Template/template';
+import { InsideLoginSuspenseLoader } from '../Components/SuspenseLoader/suspenseLoader';
+import LogoutPage from  '../Pages/LogoutPage/logoutPage';
 
 const IndexPage = React.lazy(()=> import('../Pages/IndexPage/indexPage'));
-const Settingspage = React.lazy(()=> import('../Pages/SettingsPage/settingsPage'));
-const NotFoundPage = React.lazy(()=> import('../Pages/NotFoundPage/notFoundPage'));
-const InterestPage = React.lazy(()=> import('../Pages/InterestPage/interestPage'));
-const ConfirmationsPage = React.lazy(()=> import('../Pages/ConfirmationsPage/confirmationsPage'));
+const SettingsPage = React.lazy(()=> import('../Pages/SettingsPage/settingsPage'));
+const NotFoundPage = React.lazy(()=> import('../Pages/HomeNotFoundPage/homeNotFoundPage'));
 const NotificationsPage = React.lazy(()=> import('../Pages/NotificationsPage/notificationsPage'));
-const GettingStartedPage = React.lazy(()=> import('../Pages/GettingStartedPage/gettingStartedPage'));
 const ViewProfilePage = React.lazy(()=> import('../Pages/ViewProfilePage/viewProfilePage'));
 const CartPage = React.lazy(()=> import('../Pages/CartPage/cartPage'));
 const ViewProductPage = React.lazy(()=> import('../Pages/ViewProductPage/viewProductPage'));
-// const IndexOrdersPage = React.lazy(()=> import('../Pages/IndexOrdersPage/indexOrdersPage'));
 const UserDashboardPage = React.lazy(()=> import('../Pages/UserDashboardPages/userDashboard'));
 const CheckoutPage = React.lazy(()=> import('../Pages/CheckoutPage/checkoutPage'));
+const MessagesPage = React.lazy(()=> import('../Pages/MessagesPage/messagesPage'))
 
-
-const homeRoutes = [
-    {path: "/home", exact: true, suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: IndexPage, },
-    {path: "/home/settings",  suspense: true,SuspenseComponent: SettingsSuspenseLoader, Component: Settingspage, },
-    {path: "/home/interests",  suspense: true,SuspenseComponent: SettingsSuspenseLoader, Component: InterestPage, },
-    {path: "/home/confirmations",  suspense: true,SuspenseComponent: SettingsSuspenseLoader, Component: ConfirmationsPage, },
-    {path: "/home/notifications",  suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: NotificationsPage, },
-    {path: "/home/getting-started", suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: GettingStartedPage, },
-    {path: "/home/view-profile",suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: ViewProfilePage,},
-    {path: "/home/cart", suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: CartPage, },
-    {path: "/home/view-product",suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: ViewProductPage, },
-    {path: "/home/checkout", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: CheckoutPage, },
-    // {path: "/home/orders", suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: IndexOrdersPage, },
-    {path: "/home/dashboard", suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: UserDashboardPage, },
-    {path: "/home/logout", Component: LogoutPage, },
-    {path: "/home/*", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: NotFoundPage,},
+const homeRoutesUsingIndexTemplate = [
+    { 
+        template: IndexPageTemplate,
+        routes: [
+            {path: "/home", exact: true, suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: IndexPage},
+            {path: "/home/notifications",  suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: NotificationsPage},
+            {path: "/home/view-profile",suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: ViewProfilePage,},
+            {path: "/home/checkout", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: CheckoutPage},
+            {path: "/home/cart", exact: true, suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: CartPage},
+            {path: "/home/dashboard", suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: UserDashboardPage},
+            {path: "/home/logout", Component: LogoutPage, },
+            {path: "/home/settings",  suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: SettingsPage},
+            {path: "/home/view-product",suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: ViewProductPage},
+            {path: "/home/messages",suspense: true,SuspenseComponent: InsideLoginSuspenseLoader, Component: MessagesPage},
+            {path: "/home/*", suspense: true, SuspenseComponent: InsideLoginSuspenseLoader, Component: NotFoundPage},
+        ]
+    } 
 ]
 
-export default function HomeRoutes(props) {
+export default function HomeRoutesUsingIndexTemplate({ ...props }) {
     return (
-        <Router routes={props.routes || homeRoutes}/>
+       <>
+        {
+            homeRoutesUsingIndexTemplate?.map(({ template, routes }, i) => 
+                <RouterWithTemplate
+                key = { i }
+                template = { template } 
+                routes = { routes }     
+                />
+            )
+        }
+       </>
     )
+
 }
