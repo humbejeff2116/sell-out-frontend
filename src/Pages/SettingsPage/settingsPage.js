da35b590a41d4f803c3d60142b07e5f7d5db02eb
humbejeff2116 <humbejeff2116@gmail.com>
2022-05-28 14:13:59 +0100

    refactor settingsPage to use IndexTemplateChildrenWithFooterAndNoRightSideBar

--- src/Pages/SettingsPage/settingsPage.js
+++ src/Pages/SettingsPage/settingsPage.js
@@ -1,29 +1,23 @@
 
-
-
-
-
-
-import React, {useEffect} from 'react';
-import { SettingsPageTemplate} from '../../Components/Template/template';
+import React, { useEffect } from 'react';
+import { 
+    IndexTemplateChildrenWithFooterAndNoRightSideBar 
+} from '../../Components/Template/template';
 import Settings from '../../Components/Settings/settings';
 import RequireAuthentication from '../../Components/Authentication/requireAuthentication';
 
 
 function SettingsPageComp() {
-   
     useEffect(()=> {
         window.scrollTo(0,0);
     },[]);
-    return (
-        <SettingsPageTemplate>
-            <Settings/>
-        </SettingsPageTemplate>
-    )
 
+    return (
+        <IndexTemplateChildrenWithFooterAndNoRightSideBar>
+            <Settings/>
+        </IndexTemplateChildrenWithFooterAndNoRightSideBar>
+    )
 }
-function isAuthenticated() {
-    return true;
-}
-const SettingsPage = RequireAuthentication(SettingsPageComp, isAuthenticated);
+
+const SettingsPage = RequireAuthentication(SettingsPageComp);
 export default SettingsPage;
