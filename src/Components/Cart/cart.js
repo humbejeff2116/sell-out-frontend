51052e499259a2e37a2bab6947060339ab9d4b3f
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-01 19:30:41 +0100

    extract and restyle CartProductProfile component and remove excessive white spaces

--- src/Components/Cart/cart.js
+++ src/Components/Cart/cart.js
@@ -1,127 +1,84 @@
 
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import useAuth from '../../Context/context';
-import image from '../../Images/avatar4.png';
-import image2 from '../../Images/product3.webp';
+import { GoKebabVertical } from 'react-icons/go';
 import { ModalBox } from '../ModalComments/modalComments';
-import useCartContext from '../../Context/Cart/cartContext';
 import {
     reduceCartProductActionPayload,
     addCartProductQuantityActionPayload,
     removeFromCartActionPayload,
 } from '../../Context/Cart/cartPayloads';
 import { Price } from '../Product/product';
+import useCartContext from '../../Context/Cart/cartContext';
+import useAuth from '../../Context/context';
+import image from '../../Images/avatar4.png';
+import image2 from '../../Images/product3.webp';
 import './cart.css';
 
 export default function Cart() {
-
     const { cartItems, cartState } = useCartContext();
-
     let CartProductsComponent;
     
     if (cartItems.length > 0) {
-
         CartProductsComponent = (
-
-            <CartProductsWrapper cartData = { cartState } />
-
+           <CartProductsWrapper cartData = { cartState } />
         )
-
     } else {
-
         CartProductsComponent = (
-
             <EmptyCart/>
-
         )
-
     }
 
     return (
-
         <div className="cart-container">
             { CartProductsComponent }
         </div>
-
     )
-
 }
 
 function CartProductsWrapper({ cartData }) {
-
     const [showCartModal, setShowCartModal] = useState(false);
-
     const [offlinePaymentMethod, setOfflinePaymentMethod] = useState(false);
-
     const [confirmOfflinePaymentMethod, setConfirmOfflinePaymentMethod] = useState(false);
-
     const [placingOrder, setPlacingOrder] = useState(false);
-
     const [placeOrderUsingOfflinePayment, setPlaceOrderUsingOfflinePayment] = useState(false);
-
     const [placedOrderSuccess, setPlacedOrderSuccess] = useState(false);
-
     const [redirect, setRedirect] = useState("");
-
     const { cartState, cartItems, totalSum } = useCartContext();
-
     const { user } = useAuth();
-
     let CheckoutModalChildComp;
 
     useEffect(()=> {
-
         return ()=> {
-        
             if (user) {
-
                 if (cartItems?.length > 0 && cartState?.length > 0) {
-
                     const currentCartState = {
                         cartState,
                         currentUser: user,
                     }
     
                     localStorage.setItem(`${user.userEmail}-cart`, JSON.stringify(currentCartState));
-
                 } else {
-
                     localStorage.removeItem(`${user.userEmail}-cart`);
-
-                }
-                
-                
+                }    
             }
-
         }
-
     },[cartState, cartItems, user]);
 
     const closeCartModal = () => {
-
         setShowCartModal(false);
-
         setOfflinePaymentMethod(false);
-
         setConfirmOfflinePaymentMethod(false)
-
         setPlacingOrder(false)
-
         setPlacedOrderSuccess(false)
-
     }
 
     const openCartModal =() => {
-
         setShowCartModal(true);
-
     }
 
     const handleOfflinePayment = () => {
-
         setOfflinePaymentMethod(true)
-
     }
 
     const handleOnlinePayment = () => {
@@ -129,81 +86,56 @@ function CartProductsWrapper({ cartData }) {
     }
 
     const goBack = () => {
-
         setOfflinePaymentMethod(false);
-
         setConfirmOfflinePaymentMethod(false)
-
     }
 
     const confirmOfflinePayment= () => {
-
         setConfirmOfflinePaymentMethod(true)
-
         setOfflinePaymentMethod(false)
-
     }
 
     const placeOrder = () => {
-
         // TODO... emit order to server here;
         setPlacingOrder(true)
-
     }
 
     if (offlinePaymentMethod) {
-
         CheckoutModalChildComp = (
-
             <CheckoutOfflinePaymentMethod
             goBack={goBack}
             confirmOfflinePayment={confirmOfflinePayment}
             />
-
         )
-
     } else if(confirmOfflinePaymentMethod) {
-
         CheckoutModalChildComp = (
-
             <ConfirmOfflinePaymentOrder
             goBack = { goBack }
             placeorder = { placeOrder }
             placingOrder = { placingOrder }
             placedOrderSuccess = { placedOrderSuccess }
             />
-
         )
-
     }else {
-
         CheckoutModalChildComp = (
-
             <CheckoutModalChild 
             handleOfflinePayment = { handleOfflinePayment }
             handleOnlinePayment = { handleOnlinePayment }
             />
-
         )
-
     }
 
     return (
-
         <div className="cart-products-wrapper">
             {
-
                 (showCartModal) && (
-
                     <ModalBox 
                     handleModal = { closeCartModal } 
                     modalContainer={"cart-checkout-modal-container"}
                     >
                        { CheckoutModalChildComp }
                     </ModalBox>
-
                 )
-
             }
             <div className="cart-products-container">
                 <div className="cart-header">
@@ -211,21 +143,15 @@ function CartProductsWrapper({ cartData }) {
                 </div>
                 <div className="cart-products-panel">
                 { 
-
                     cartData.map(({ products, ...rest }) => (
-
                         products.map((product, i) => 
-
                             <CartProduct 
                             key={ i } 
                             product={ product }
                             { ...rest }
                             />
-
                         )
-
                     ))
-
                 }
                 </div>
                 <div className="cart-products-total-container">
@@ -244,13 +170,10 @@ function CartProductsWrapper({ cartData }) {
             </div>
         </div>
     )
-
 }
-
 
 function CheckoutModalChild({ handleOfflinePayment, handleOnlinePayment }) {
     return (
-
         <div className="cart-checkout-modal-body-container">
             <div className="cart-checkout-modal-content">
                 <p>
@@ -270,16 +193,12 @@ function CheckoutModalChild({ handleOfflinePayment, handleOnlinePayment }) {
                 </div>
             </div>
         </div>
-
     )
 }
 
 function CheckoutOfflinePaymentMethod({ goBack, confirmOfflinePayment }) {
-
     return(
-
-        <div className="cart-checkout-modal-body-container">
-           
+        <div className="cart-checkout-modal-body-container">  
            <div className="cart-checkout-modal-offline-payment-content">
                 <p>
                    By using the offline payment method, all financial transactions 
@@ -296,93 +215,64 @@ function CheckoutOfflinePaymentMethod({ goBack, confirmOfflinePayment }) {
                 </div>
            </div>
         </div>
-
     )
-    
 }
 
 function ConfirmOfflinePaymentOrder({ placingOrder, placedOrderSuccess, goBack, placeorder, ...props }) {
-
     let ConfirmOfflinePaymentOrderComp;
 
     if (placingOrder) {
-
         ConfirmOfflinePaymentOrderComp = (
-
             <PlacingOrder/>
-
         )
-
     } else if(placedOrderSuccess) {
-
         ConfirmOfflinePaymentOrderComp = (
-
             <PlacedOrderSuccess/>
-
         )
-
     } else {
-
         ConfirmOfflinePaymentOrderComp = (
-
             <>
-            <div className="cart-checkout-modal-offline-payment-content">
-                <p>
-                    Are you sure you want to place order using offline payment method?
-                </p>
-            </div>
-            <div className="cart-checkout-modal-place-order-button-container">
-                <div className="cart-checkout-modal-place-order-button back">
-                   <button  onClick = { goBack }>Go back</button>
+                <div className="cart-checkout-modal-offline-payment-content">
+                    <p>
+                        Are you sure you want to place order using offline payment method?
+                    </p>
                 </div>
-               <div className="cart-checkout-modal-place-order-button">
-                   <button onClick = { placeorder }>Place Order</button>
+                <div className="cart-checkout-modal-place-order-button-container">
+                    <div className="cart-checkout-modal-place-order-button back">
+                        <button  onClick = { goBack }>Go back</button>
+                    </div>
+                    <div className="cart-checkout-modal-place-order-button">
+                        <button onClick = { placeorder }>Place Order</button>
+                    </div>
                 </div>
-            </div>
             </>
-
         )
-
     }
 
     return (
-
         <div className="cart-checkout-modal-body-container">
-            {ConfirmOfflinePaymentOrderComp}
+            { ConfirmOfflinePaymentOrderComp }
         </div>
-
     )
-
 }
 
 function PlacingOrder(props) {
-
     return (
-
         <div>placing order...</div>
-
     )
-
 }
 
 function PlacedOrderSuccess(props) {
-
     return (
-
         <div>
             Order placed successfully
         </div>
-
     )
-
 }
 
 function CartProduct({ product, ...props }) {
-
     // const [quantity, setQuantity] = useState(""); 
-
     const { user } = useAuth();
-
     const {
         cartState,
         cartItems,
@@ -392,89 +282,53 @@ function CartProduct({ product, ...props }) {
         updateCartContextState,
     } = useCartContext();
 
-
     const addProductQuantity = async (cartState, sellerEmail, productId, user) => {
-
         // setQuantity(prevState => (parseInt(prevState) + 1).toString());
         const updatedState = await addCartProductQuantity(cartState, addCartProductQuantityActionPayload(sellerEmail, productId));
-
         updateCartContextState(updatedState, user);
-
     }
 
     const reduceProductQuantity = async (cartState, sellerEmail, productId, user) => {
-
         // setQuantity(prevState => (prevState - 1).toString());
         const updatedState = await  reduceCartProductQuantity(cartState,  reduceCartProductActionPayload(sellerEmail, productId));
-
         updateCartContextState(updatedState, user);
-
     }
 
     const removeProduct = async (cartState, cartItems, sellerEmail, productId, user) => {
-
         const updatedState = await removeProductFromCart(cartState, removeFromCartActionPayload(sellerEmail, productId));
-
         const updatedCartItems = updatedState.flatMap(item => item.productsUserBoughtFromSeller);
-
         if (updatedCartItems.length < 1) {
-
             return updateCartContextState([], user);
-
-        }
-        
+        } 
         updateCartContextState(updatedState, user);
 
         if (cartItems.length > 0) {
-
             window.scrollTo(0,0)
-
         }
-
     }
 // TODO... move code to library to avoid duplication
     const calculateProductSubTotal = ({ percentageOff, productPrice, productQty }) => {
-
         if (percentageOff) {
-
             const percentOffPrice = (percentageOff / 100) * Number(productPrice)
-
             const newPrice = Number(productPrice) - percentOffPrice;
 
             return ( newPrice *  productQty).toFixed(2, 10)    
-
         } 
 
         return (productPrice *  productQty).toFixed(2, 10)
-
     }
 
     return (
-
-            <div className="cart-product-panel">
-                {/* product profile image */}
-            <div className="cart-product-profile" >
-                <div  className="cart-product-profile-image">
-                    <img src = { product.sellerProfileImage || image} alt="avatar" />
-                </div>
-
-                <div className="cart-product-profile-info">
-                    <div> <span>{ product.sellerName || ''}</span></div>
-                </div>
-            </div>
-            {/* product image */}
+        <div className="cart-product-panel">
+            <CartProductProfile { ...props } />
             <div className="cart-product-images-panel">
                 <div className="cart-product-images">
                 {
-
                     [product.productImages[0]].map((image, i) =>
-
                         <div key = { i } className="cart-product-image-group">
                             <img src = { image.src || image2 } alt="product" />
                         </div>
-
                     )
-
                 }
                 </div>
                 {/* product information */}
@@ -484,9 +338,7 @@ function CartProduct({ product, ...props }) {
                         { product.productName }
                         </p>
                     </div>
-
                     <Price {...product} className="cart-product-info-span-group" showPriceTag = { true } />
-
                     <div className="cart-product-info-span-group">
                         <p>Quantity: <span>{ product.productQty }</span></p>
                     </div>
@@ -503,14 +355,12 @@ function CartProduct({ product, ...props }) {
                                 <div className="cart-product-add-button-icon">
                                 <button onClick={ () =>reduceProductQuantity(cartState, props.sellerEmail, product.productId, user) }>-</button>
                                 </div>
-                            </div>
-                        
+                            </div>                       
                             <input  
                             value = { product.productQty }
-                           onChange = { f => f }
+                            onChange = { f => f }
                             className="cart-product-input" type="text" 
                             />
-
                             <div className="cart-product-reduce-button">
                                 <div className="cart-product-add-button-icon">
                                 <button onClick = { () => addProductQuantity(cartState, props.sellerEmail, product.productId, user) }>+</button>
@@ -523,21 +373,34 @@ function CartProduct({ product, ...props }) {
                             </div>
                         </div>
                     </div>
-                </div>
-                
+                </div>  
             </div>
-            </div>
-
+        </div>
     )
-
 }
 
+function CartProductProfile({ sellerProfileImage, sellerName, ...props}) {
+    return (
+        <div className="cart-product-avatar-Wrapper">
+            <div className="cart-product-avatar" >
+                <img src = { sellerProfileImage || image } alt="avatar" />
+                <div className="cart-product-avatar-user-name">
+                    <div> 
+                        <span>{ sellerName }</span>
+                    </div>
+                </div>
+                <div className= "cart-product-avatar-kebab">
+                    <GoKebabVertical className="nav-icon"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+ 
 function CartCheckoutComp({ onClick }) {
-
     const { totalSum, cartTotalNumberOfProducts } = useCartContext();
 
     return (
-
         <div className="cart-checkout-panel">
             <div className="cart-checkout-info">
                 <div className="cart-checkout-span-group">
@@ -555,15 +418,11 @@ function CartCheckoutComp({ onClick }) {
                 </div>
             </div>
         </div>
-
     )
-
 }
 
 function EmptyCart(props) {
-
-    return (
-        
+    return (    
         <div className="empty-cart-container">
             <div className="cart-header">
                 <h3>Cart</h3>
@@ -579,7 +438,5 @@ function EmptyCart(props) {
                 </div>
             </div>
         </div>
-
     )
-
 }
