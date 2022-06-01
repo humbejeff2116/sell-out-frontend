88966bc80b32cda7b41962b6315a324806b397bd
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-01 19:35:08 +0100

    rename RequireGettingStartedAuthentication component sessionStorage key name and remove excessive white spaces

--- src/Components/Authentication/requireViewStateAuthentication.js
+++ src/Components/Authentication/requireViewStateAuthentication.js
@@ -4,64 +4,43 @@ import useViewContext from '../../Context/viewContext/context';
 import {  Redirect } from 'react-router-dom';
 
 export default function  RequireViewStateAuthentication(RequireAuthentication, Component) {
-
     function AuthenticatedComponent(props) {
-
         const { viewState } = useViewContext();
-
         let AuthComponent = RequireAuthentication(Component);
         
         if (!viewState) {
-
             return (
-
                 <Redirect to='/home' />
-
             ) 
-
         }
 
         return (
-
            <AuthComponent {...props} />
-
         )
-
     }
 
-    return AuthenticatedComponent;
-    
+    return AuthenticatedComponent;  
 }
 
 
 export function  RequireGettingStartedAuthentication(RequireAuthentication, Component, redirectTo) {
-
     function AuthenticatedComponent(props) {
-
-        const { user, canAccessGettingStarted } =  sessionStorage.getItem('access-getting-started') ? JSON.parse(sessionStorage.getItem('access-getting-started')) : {}
-
+        const { user, canAccessGettingStarted } = sessionStorage.getItem('access-getting-started-page') ? (
+            JSON.parse(sessionStorage.getItem('access-getting-started-page'))
+        ) : {}
         const AuthComponent = RequireAuthentication(Component);
-
         const redirectPath = redirectTo ? redirectTo : "/home";
         
         if (!canAccessGettingStarted  || !user) {
-
             return (
-
                 <Redirect to = { redirectPath } />
-
             )  
-
         }
 
         return (
-
            <AuthComponent {...props} />
-
         )
-
     }
 
     return AuthenticatedComponent;
-
 }
