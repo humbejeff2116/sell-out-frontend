fbedd591d5751eb8106f9d25559e2af62276f5be
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-01 20:35:49 +0100

    remove excessive white spaces from module

--- src/Components/Authentication/requireAuthentication.js
+++ src/Components/Authentication/requireAuthentication.js
@@ -1,44 +1,26 @@
 
-
-
-
-
-
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import useAuth from '../../Context/context';
 
 
-
 export default function RequireAuthentication(Component, auth, redirectTo) {
-    
     function AuthenticatedComponent(props) {
-
         const { isAuthenticated } = useAuth();
-
         const auth = isAuthenticated();
-
         const redirectPath = redirectTo ? redirectTo : "/login";
 
+        if (!auth) {
+            localStorage.setItem('route-auth-message','you must be logged in to view this page');
 
-            if (!auth) {
-
-                localStorage.setItem('route-auth-message','you must be logged in to view this page');
-
-                return(
-
-                   <Redirect to = { redirectPath } />
-
-                )
-
-            }
-
-            return(  
-
-                <Component {...props}/>
-
+            return (
+                <Redirect to={ redirectPath } />
             )
-            
+        }
+
+        return (  
+            <Component { ...props }/>
+        )    
     }
     
     return AuthenticatedComponent;
