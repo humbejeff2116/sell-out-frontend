dc07499c4e812c453a044efa89710677c4e7ffa4
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-03 20:49:01 +0100

    replace back button text with icon

--- src/Components/BackButton/backButton.js
+++ src/Components/BackButton/backButton.js
@@ -1,23 +1,31 @@
 
 import React from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
+// import { TiArrowBack, TiArrowBackOutline } from 'react-icons/ti'
+import { RiArrowGoBackFill } from 'react-icons/ri'
 
 
-
-
-
-export default function BackButton({ buttonWrapperClassName, buttonIcon, clearSessionStorageWithKey }) {
+export default function BackButton({ 
+    buttonWrapperClassName, 
+    buttonIcon, 
+    buttonIconClassName, 
+    sessionStorageKey 
+}) {
     const history = useHistory();
-    const goBack = (history, clearSessionStorageWithKey) => {
-        if (clearSessionStorageWithKey) {
-            sessionStorage.removeItem(clearSessionStorageWithKey);
+
+    const goBack = (history, sessionStorageKey) => {
+        if (sessionStorageKey) {
+            sessionStorage.removeItem(sessionStorageKey);
             return history.goBack();
         }
         return history.goBack();
     }
+
     return(
         <div className={buttonWrapperClassName} >
-            <i>{ buttonIcon || '' }</i><button onClick={()=> goBack(history, clearSessionStorageWithKey ) } > go back </button>
+            <button onClick={ ()=> goBack(history, sessionStorageKey) } >
+                { buttonIcon  || <RiArrowGoBackFill className={ buttonIconClassName || "index-search-filter-icon" }/> }
+            </button>
         </div>      
     )
 }
