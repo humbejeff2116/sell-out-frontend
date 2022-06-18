77df6d3cddfbbb5c54fea926784be32356bcd0e3
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-18 16:57:31 +0100

    redesign modal reviews component

--- src/Components/ModalComments/modalReviews.js
+++ src/Components/ModalComments/modalReviews.js
@@ -1,48 +1,63 @@
 
 import React from 'react';
 import { RiCloseFill } from "react-icons/ri";
-import './modalComments.css';
+import { DisplayedProduct } from '../Product/product';
+import { ReviewsBox } from './ReviewsBox/reviewsBox';
+import styles from './ModalReviews.module.css';
 
-export default function ModalComment({ handleClose, show,  modalDisplayedProduct, commentBox }) {
-
+export default function ModalReviews({ handleClose, show, ...props }) {
     return(
-
-        <div className="modal-comment-product-wrapper">
-
-            <div className="modal-comment-product-comment-bttn-wrapper">
+        <div className={ styles.reviewsContainer }>
+            <div className={ styles.bttnWrapper }>
                 <RiCloseFill className="nav-icon"  onClick={ handleClose }/>
-            </div>
-             
-            <div className="modal-comment-product-container">
-
-                <div className="modal-comment-product-comment-panel">                   
-                { modalDisplayedProduct }
-                { commentBox}
+            </div> 
+            <div className={ styles.wrapper }>
+                <div className={ styles.panel }>                   
+                    <DisplayedProduct 
+                    product = { props.product }
+                    dontShowModal
+                    dontShowReviews
+                    // panelClassName = { styles.productPanel }
+                    />
+                    <ReviewsBox
+                    product = { props.product }
+                    closeReviewsBox = { props.closeReviewBox }
+                    reviewsBoxPanelClassName = { styles.reviewsPanel }
+                    />
                 </div>
-
             </div>
-
         </div> 
-
     )
-
 }
 
 
 
-export function ModalBox({ modalContainerWrapperName, handleModal, modalContainer, children }) {
+export function ModalBox({ 
+    modalContainerWrapperName, 
+    handleModal, 
+    dontUseDefaultModalChildContainer,
+    dontShowCloseButton, 
+    modalContainer, 
+    children,
+    ...props 
+}) {
    
     return(
-
-        <div className={ modalContainerWrapperName || "modal-comment-product-wrapper" }>
-             <div className="modal-comment-product-comment-bttn-wrapper">
-                <RiCloseFill className="nav-icon"  onClick={ handleModal }/>
-            </div>
-            <div className={ modalContainer || "modal-comment-product-container" }>
-               
-                { children }
-
-            </div>
+        <div className={ modalContainerWrapperName || styles.reviewsContainer }>  
+        {
+            dontShowCloseButton  ? children : (
+                <div className={ styles.bttnWrapper }>
+                    <RiCloseFill className="nav-icon"  onClick={ handleModal }/>
+                </div>
+            )
+        }
+        {
+            dontUseDefaultModalChildContainer  ? children : (
+                <div className={ modalContainer || styles.modalBoxContainer }>
+                    { children }
+                </div> 
+            )
+        }
         </div>
 
     )
