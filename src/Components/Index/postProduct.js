3a38a537f5b977496e31b47332ffca1e2cef693a
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-28 14:46:15 +0100

    change PostProduct component writeup

--- src/Components/Index/postProduct.js
+++ src/Components/Index/postProduct.js
@@ -7,10 +7,11 @@ export function PostProduct(props) {
     return (
         <section className="index-post-container">
             <div className="index-post-writeup-container">
-                <h4>Sell your products using Fling</h4>
+                <h4>Let Fling help you sell your products</h4>
                 <div className="index-post-writeup">     
-                    Join this amazing community of enterprenuers, connect 
-                    share ideas and make cool cash while selling your products 
+                    Selling your products doesn't need to be complex, in simple steps, 
+                    upload details about your asset('s) and 
+                    let our systems handle the sales for you. 
                 </div>
                 <div className="index-post-getting-started">
                     <Link to="/home/dashboard/store/upload-product">
