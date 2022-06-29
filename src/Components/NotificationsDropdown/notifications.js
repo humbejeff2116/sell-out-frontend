5485d79df9e116f5a67eab90249a2b5c90f8223b
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-29 19:32:30 +0100

    add functionality to close NotificationDropdown component when user clicks outside and clean code

--- src/Components/NotificationsDropdown/notifications.js
+++ src/Components/NotificationsDropdown/notifications.js
@@ -1,249 +1,249 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { RiNotification3Line } from 'react-icons/ri';
 import socket from '../Socket/socket';
 import {  getUserNotifications } from '../../Utils/http.services';
-import { RiNotification3Line } from 'react-icons/ri';
-import image from '../../Images/avatar.jpg';
+import profileAvatar from '../../Images/avatar4.png';
+import bell from '../../Images/bell3.png';
+import styles from './Notifications.module.css';
 import './notifications.css';
 
 
 export default function Notifications(props) {
-
     const [notifications, setNotifications] = useState([]);
-
     const [showNotifications, setShowNotifications] = useState(false);
+    const notificationContainer = useRef();
+    let timer = null;
 
     useEffect(()=> {
-
         let mounted = true;
-
         const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-        
-        const getNotifications = async (user, mounted) => {
-
-            try {
-
-                const { data } = await getUserNotifications(user)
-
-                if (mounted) {
-
-                    setNotifications(data);
-
-                }
-                // TODO... handle error
-            } catch(err) {
-
-            }  
-        }
 
         if (user && mounted ) {
-
-            getNotifications(user, mounted);
-
+            getNotifications(user, setNotifications);
         }
-
         socket.on('userDataChange', function() {
-
             if (mounted) {
-
-                getNotifications(user, mounted);
-
+                getNotifications(user, setNotifications);
             } 
-
         });
-
         socket.on('seenNotificationsSuccess', function() {
-
             if (mounted) {
-
-                getNotifications(user, mounted);
-
+                getNotifications(user, setNotifications);
             } 
-
         });
         
         return () => {
-
             mounted = false;
+        }  
+    }, []);
+
+    useEffect(()=> {  
+        return () => {
+            if (timer) clearTimeout(timer);
+        }  
+    }, [timer]);
+
+    const getNotifications = async (user, setNotifications) => {
+        try {
+            const { data } = await getUserNotifications(user)
+            setNotifications(data);
+        } catch(err) {
 
         }  
-
-    }, []);
+    }
 
     const toggleNotifications = () => {
-
         setShowNotifications(prevState => !prevState)
+    }
 
+    const onBlurHandler = () => { 
+        alert(true)   
+        timer = setTimeout(() => setShowNotifications(false));  
+    }
+    // If a child receives focus, do not close the popover.  
+    const onFocusHandler = () => { 
+        alert(true)   
+        clearTimeout(timer);  
+    }
+
+    const onClickOutsideHandler = (e) => { 
+        const { current } = notificationContainer;   
+        if (showNotifications && current && !current.contains(e.target)) {      
+            setShowNotifications(false);    
+        }  
     }
 
     return (
-
         <>
-        <div className="notifications-container">
-            <NotificationIcon
-            openNotifications= {toggleNotifications}
-            notifications={notifications}
-            />
-        </div>
-        {
-            showNotifications && (
-                <NotificationsDropDown
-                notifications ={notifications}
-                /> 
-            )
-        }
+            <div className="notifications-container">
+                <NotificationIcon
+                openNotifications= { toggleNotifications }
+                notifications={ notifications }
+                />
+            </div>
+            {
+                showNotifications && (
+                    <NotificationsDropDown
+                    notifications ={ notifications }
+                    onBlur = { onBlurHandler }
+                    onFocus = { onFocusHandler }
+                    onClickOutside = { onClickOutsideHandler }
+                    ref = { notificationContainer }
+                    show = { showNotifications }
+                    /> 
+                )
+            }
         </>
-
     )
-
 }
 
-function NotificationsDropDown({ notifications, ...props }) {
-
+const  NotificationsDropDown = React.forwardRef(({ 
+    notifications, 
+    onClickOutside,
+    onBlur,
+    onFocus, 
+    show,
+    ...props 
+}, ref) => {   
     useEffect(()=> {
-
         const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-        
+
         checkIfNotificationsIsSeen(notifications)
         .then(notSeen => {
-
             if (notSeen) {
-
                return  socket.emit('seenNotifications', user);
-            }
-            
+            }  
         })
+    }, [notifications]);
 
-    }, []);
+    useEffect(()=> {
+        window.addEventListener('click', onClickOutside);
+        return () => window.removeEventListener('click', onClickOutside);
+    }, [onClickOutside]);
+
 
     const checkIfNotificationsIsSeen = async (notifications) => {
-
-         let notSeen = false;
-
+        let notSeen = false;
         for (let i = 0; i < notifications.length; i++) {
-
-            if(notifications[i].seen === false) {
-
+            if (notifications[i].seen === false) {
                notSeen = true;
-
                break;
-
             }
-
         }
-
         return notSeen;
-
     }
 
-    return (
-
-        <div className="notifications-dropdown-wrapper">
-           
-        <div className="notifications-dropdown-container">
-            {
-                notifications.length > 0 ? notifications.map((notification, i) =>
-                    
-                    <NotificationsBox key = { i }  { ...notification } />
-                
-                ) : (
-
-                    <div>
-                    <p>no notifications yet</p>
-                    </div>
-                
-                ) 
-            
-            }
-        </div> 
-        </div> 
-
+    const dropdownContainer = show ? (
+        "notifications-dropdown-container"
+    ) : (
+        "notifications-dropdown-container"
     )
 
+    return (
+        <div 
+        className= { dropdownContainer }
+        onBlur={ onBlur }           
+        onFocus={ onFocus }
+        ref={ ref }
+        > 
+            <div className="notifications-dropdown-header">
+                Notifications
+            </div> 
+            <div className="notifications-dropdown-wrapper">
+            {
+                notifications.length > 0 ? notifications.map((notification, i) =>   
+                    <NotificationsBox key = { i }  { ...notification } />
+                ) : (
+                   <EmptyNotifications/>
+                ) 
+            }
+            </div> 
+        </div> 
+    )
+})
+
+function EmptyNotifications(props) {
+    return (
+        <div className={ styles.emptyContainer }>
+            <div className={ styles.emptyWrapper }>
+                <div className={ styles.emptyImage }>
+                    <img src ={ bell } alt ="" />
+                </div>
+                <div className={ styles.emptyHeader }>
+                    No Notifications
+                </div>
+                <div className={ styles.emptyBody }>
+                    You currently do not have any notification at the moment. 
+                </div>
+            </div>
+            
+        </div> 
+    )
 }
 
 function NotificationIcon({ openNotifications, notifications, ...props }) {
-
     const notSeenNotificationsCount = (notifications) => {
-
         let j = 0;
-
-        for (let i = 0; i < notifications.length; i++) {
-
+        let i = 0;
+        let len = notifications.length;
+        for (i; i < len; i++) {
             if (notifications[i].seen === false) {
-
-                j++
+                j += 1
             }
-
         }
-
         return j;
-
     }
 
     return (
-
-        <div className="notifications-icon-wrapper" onClick = { openNotifications }>
+        <div 
+        className="notifications-icon-wrapper" 
+        onClick = { openNotifications }
+        >
             <RiNotification3Line className="notification-icon" />
             {
                 notifications && (
-
                     notSeenNotificationsCount(notifications) > 0  ?
                     ( <NotificationAlert className="notifications-icon-alert"/> ) : ''
-
                 ) 
             }
         </div>
-
     )
-
 }
 
 export function NotificationAlert({ className, ...props }) {
-
     return(
-
         <div className={ className }>
 
         </div>
-
     )
-
 }
 
 function NotificationsBox({ userName, userId, userProfileImage, action, ...props }) {
-
-    // TODO... implement vie profile functionality
+    // TODO... implement view profile functionality
     const viewProfile =() => {
 
     }
 
     return (
-
         <div className="notification-container">
-
-        <div className="notification-header">
-            <div className="notification-image">
-                <img src={  userProfileImage || image } alt="profile" /> 
+            <div className="notification-header">
+                <div className="notification-image">
+                    <img src={  userProfileImage || profileAvatar } alt="profile" /> 
+                </div>
+                <div className="notification-name">
+                    <span onClick = { ()=> viewProfile(userId) }>{ userName }</span>
+                    <div className="notification-time">  
+                        <span>2 weeks ago</span>
+                    </div>
+                </div>
+                <div className="notification-remove">  
+                    x
+                </div>
             </div>
-
-            <div className="notification-name">
-                <span onClick = { ()=> viewProfile(userId) }>{ userName }</span>
-                <div className="notification-time">  
-                <span>2 weeks ago</span>
-            </div>
-            </div>
-
-            <div className="notification-remove">  
-                x
-            </div>
+            <div className="notification-details">
+                <span> { action } </span>
+            </div>    
         </div>
-
-        <div className="notification-details">
-            <span> { action } </span>
-        </div>
-            
-        </div>
-
     )
 }
