c69a5964fc1cd454c110dc6ecbff72f0740bc13c
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-29 18:39:55 +0100

    add getReviews effect to ModalReviews component and clean module code

--- src/Components/ModalReviews/modalReviews.js
+++ src/Components/ModalReviews/modalReviews.js
@@ -1,35 +1,59 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { RiCloseFill } from "react-icons/ri";
 import { DisplayedProduct } from '../Product/product';
 import { ReviewsBox } from './ReviewsBox/reviewsBox';
 import styles from './ModalReviews.module.css';
 
-export default function ModalReviews({ handleClose, show, ...props }) {
+export default function ModalReviews({ 
+    handleClose, 
+    showModalChild, 
+    product, 
+    closeReviewBox,
+    ...props 
+}) {
+    const [getReviews, setGetReviews] = useState(false);
+
+    useEffect(() => {
+        let timer = null;
+        timer = setTimeout(() => setGetReviews(true), 2000);
+        return () => {
+            if (timer) clearTimeout(timer);
+        }
+    }, [getReviews]);
+
+    const modalChilldClassName = showModalChild ? (
+        `${styles.wrapper} ${styles.show}`
+    ) :(
+        `${styles.wrapper}`
+    )
+
     return(
         <div className={ styles.reviewsContainer }>
             <div className={ styles.bttnWrapper }>
                 <RiCloseFill className="nav-icon"  onClick={ handleClose }/>
             </div> 
-            <div className={ styles.wrapper }>
-                <div className={ styles.panel }>                   
-                    <DisplayedProduct 
-                    product = { props.product }
-                    dontShowModal
-                    dontShowReviews
-                    // panelClassName = { styles.productPanel }
-                    />
+            <div className={ modalChilldClassName }>
+                <div className={ styles.panel }> 
+                    <div className={ styles.productWrapper }>
+                        <DisplayedProduct 
+                        product = { product }
+                        dontShowModal
+                        dontShowReviews
+                        panelClassName = { styles.productPanel }
+                        /> 
+                    </div>                                 
                     <ReviewsBox
-                    product = { props.product }
-                    closeReviewsBox = { props.closeReviewBox }
+                    product = { product }
+                    closeReviewsBox = { closeReviewBox }
                     reviewsBoxPanelClassName = { styles.reviewsPanel }
+                    getReviews = { getReviews }
                     />
                 </div>
             </div>
         </div> 
     )
 }
-
 
 
 export function ModalBox({ 
@@ -40,13 +64,12 @@ export function ModalBox({
     modalContainer, 
     children,
     ...props 
-}) {
-   
+}) {  
     return(
         <div className={ modalContainerWrapperName || styles.reviewsContainer }>  
         {
             dontShowCloseButton  ? children : (
-                <div className={ styles.bttnWrapper }>
+                <div className={ styles.modalBoxBttnWrapper }>
                     <RiCloseFill className="nav-icon"  onClick={ handleModal }/>
                 </div>
             )
@@ -59,7 +82,5 @@ export function ModalBox({
             )
         }
         </div>
-
     )
-
 }
