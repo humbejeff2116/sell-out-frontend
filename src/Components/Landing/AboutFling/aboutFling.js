57342d107b10fcbfcd1e4ab6a71a779ad82576ee
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-15 10:22:45 +0100

    add dummy data to AboutFling component and style component

--- src/Components/Landing/AboutFling/aboutFling.js
+++ src/Components/Landing/AboutFling/aboutFling.js
@@ -13,8 +13,23 @@ import styles from './AboutFling.module.css';
 export default function AboutFling(props) {
     return (
         <div className={ styles.container }>
-            <div className={ `${styles.panel} ${styles.leftPanel}` }></div>
-            <div className={ `${styles.panel} ${styles.rightPanel}` }></div>
+            <div className={ styles.wrapper }>
+                <div className={ `${styles.panel} ${styles.left}` }>
+                    <h2 className={ styles.heading }>Lorem ispium do lus set me at per</h2>
+                    <div className={ styles.text }>
+                        Lorem ispium do lus set me at per
+                        Lorem ispium do lus set me at per
+                        Lorem ispium do lus set me at per
+                        Lorem ispium do lus set me at per
+                        Lorem ispium do lus set me at per 
+                    </div>
+                </div>
+                <div className={ `${styles.panel} ${styles.right}` }>
+                    {/* image here */}
+                    {/* <img /> */}
+                    
+                </div>
+            </div>
         </div>
     )
 }
