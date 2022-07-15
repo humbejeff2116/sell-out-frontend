360fede4290665def18f4ac86ea2a651fceceb16
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-15 10:27:21 +0100

    attach scroll event to stick landing header to top when a particular offset is reached

--- src/Components/Landing/Header/header.js
+++ src/Components/Landing/Header/header.js
@@ -1,6 +1,6 @@
 
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 import { BiMenu } from "react-icons/bi";
 import Links from '../../../Data/links';
 import fling from '../../../Images/fling8.png';
@@ -8,29 +8,51 @@ import './header.css';
 
 const landingMainLinks = Links.getLandingMainLinks();
 
-export default function Header({ showLogin, ...props }) {
+export default function Header({ stickToTop, showLogin, ...props }) {
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(()=> {
+        window.addEventListener('scroll', handleScroll);
+        return ()=> {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    });
+
+    const handleScroll = ( ) => {
+        const offset = window.scrollY;
+        if (offset > 50) {
+           setScrolled(true)
+        } else {
+           setScrolled(false)
+        }
+    }
+
+    const headerClassName = stickToTop && scrolled ? (
+        "landing-header scrolled"
+    ) : (
+        "landing-header"
+    )
    
     return (
-        <header className="landing-header">
+        <header className={headerClassName}>
             <div className="landing-header-logo">
                 <div  className="landing-header-logo-img">
-                    <img src={ fling } alt="fling" />
+                    <Link to="/">
+                        <img src={ fling } alt="Fling"/>
+                    </Link>
                 </div>
             </div>
             <div className="landing-header-navigation">
                 <nav>
                 {
                     landingMainLinks.map((link, i) =>
-
                         <NavLinks key={ i } { ...link }/>
-
                     )
                 }
                 </nav>
             </div>
             <div className="landing-header-login">
                 <div className="landing-login-item" >
-                    <button onClick={ ()=> showLogin(prevState => !prevState) }>Login</button>
+                    <button onClick={ showLogin }>Login</button>
                     <MobileNavIcon/>
                 </div>
             </div>
@@ -59,7 +81,7 @@ function NavLinks({ href, name, ...props }) {
             to={ href } 
             activeClassName="landing-nav-link-active"
             className="landing-nav-link" 
-            title={ name } 
+            // title={ name } 
             >
                 { name } 
             </NavLink> 
