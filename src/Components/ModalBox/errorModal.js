b6297001ad9e1b9c706cbaa0db05567fb8acff16
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-16 17:52:55 +0100

    clean ErrorModal component and add proptypes to it

--- src/Components/ModalBox/errorModal.js
+++ src/Components/ModalBox/errorModal.js
@@ -1,20 +1,46 @@
 
-
-
-
-
 import React from 'react';
+import PropTypes from 'prop-types'
+import { RiCloseFill } from "react-icons/ri";
+import styles from './ModalBox.module.css'
 
-
-
-export function ErrorModal(props) {
+export function ErrorModal({ 
+    errorContainerClassName,
+    errorContainerShowClassName,
+    panelClassName,
+    closeClassName,
+    closeIconClassName, 
+    message,
+    closePopUp,
+    showPopUp,
+    ...props 
+}) {
     return (
-        <div className={props.errorContainerClassName}>
-            <div className={props.panelClassName}>
-            <span>
-                {props.errorMessage}
-            </span>
+        <div className={ 
+            showPopUp ? (
+                `${styles.popupContainer} ${styles.showPopUp}`
+            ) :(
+                `${styles.popupContainer}`
+            )
+        }>
+            <div className={ panelClassName || styles.popupTextWrapper }>
+                { message }
+            </div>
+            <div className={ closeClassName || styles.closeContainer } onClick={ closePopUp } >
+                <RiCloseFill className={ closeIconClassName || styles.closeIconWrapper }/>
             </div>
         </div> 
     )
+}
+
+ErrorModal.propTypes = {
+    errorContainerClassName: PropTypes.string,
+    errorContainerShowClassName: PropTypes.string,
+    panelClassName: PropTypes.string,
+    closeClassName: PropTypes.string,
+    closeIconClassName: PropTypes.string,
+    message: PropTypes.string,
+    closePopUp: PropTypes.func,
+    showPopUp: PropTypes.bool,
+    props: PropTypes.object
 }
