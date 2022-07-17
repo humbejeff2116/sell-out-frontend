72c85a1d021773fb2a83430aa7a30189843d7e8b
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-17 17:44:17 +0100

    develop BottomSpiner component

--- src/Components/Loader/loader.js
+++ src/Components/Loader/loader.js
@@ -1,16 +1,46 @@
 
 import React from 'react';
+import PropTypes from 'prop-types'
+import styles from './Loader.module.css';
 import './loader.css';
 
 
 export function Loader({loaderContainer, loader, ...props}) {
-
     return (
-
         <div className={loaderContainer} >
             <div className={loader}> </div>
         </div>
+    ) 
+}
 
+export function BottomSPinner({ 
+    showLoader, 
+    loaderContainerClass, 
+    spinnerContainerClass, 
+    spinnerClass, 
+    children
+}) {
+    return (
+        <div className={ 
+            showLoader ? (
+                `${ loaderContainerClass || styles.loaderContainer} ${styles.showloader}` 
+            ) : ( 
+                `${ loaderContainerClass || styles.loaderContainer}`
+            )
+        }>
+        <Loader
+        loaderContainer={ spinnerContainerClass || styles.spinnerContainer }
+        loader ={ spinnerClass || styles.spinner }
+        />
+        { children }
+      </div>
     )
-    
+}
+
+BottomSPinner.propTypes = {
+    showLoader: PropTypes.bool, 
+    loaderContainerClass: PropTypes.string, 
+    spinnerContainerClass: PropTypes.string, 
+    spinnerClass: PropTypes.string,
+    children: PropTypes.any
 }
