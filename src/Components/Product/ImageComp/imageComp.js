9119fe3c08a27d7ffcbfdd55f29ad53855946851
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-22 11:26:29 +0100

    refactor SingleImageComponent to work when used in Product component for un-authenticated routes and clean module code

--- src/Components/Product/ImageComp/imageComp.js
+++ src/Components/Product/ImageComp/imageComp.js
@@ -1,61 +1,80 @@
 
-
-
-import React, {useState} from 'react';
-import {  Redirect, useLocation, useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Redirect, useLocation, useHistory } from 'react-router-dom';
 import useViewContext from '../../../Context/viewContext/context';
 import image2 from '../../../Images/product3.webp';
 
 
 
-function SingleImageComponent(props) {
+function SingleImageComponent({ usedOutsideLogin, image, product , ...props }) {
     const [redirect, setRedirect] = useState('');
     const location = useLocation();
     const history = useHistory();
     const { setViewState } = useViewContext();
-    const { image } = props;
 
     const viewProduct = (product) => {
-        if(location.pathname === '/home/view-product') {
+        if (location.pathname === '/home/view-product') {
            setViewState(product);
-           window.scrollTo(0,0);
+           window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
            return; 
         }
+
         setViewState(product)
         history.push(location.pathname);
-        return setRedirect('/home/view-product');
+        setRedirect('/home/view-product');
     }
+
+    const viewProductOutsideLogin = (product) => {
+        if (location.pathname === '/view-product') {
+           setViewState(product);
+           window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+           return; 
+        }
+
+        setViewState(product)
+        history.push(location.pathname);
+        setRedirect('/view-product');
+    }
+
     if (redirect) {
         return (
             <Redirect to={redirect} />
         )
     }
-    return (
-        <div className="index-product-single-image-panel" onClick={()=>viewProduct(props.product)}>
+
+    if (usedOutsideLogin) {
+        return (
+            <div className="index-product-single-image-panel" onClick={()=> viewProductOutsideLogin(product)}>
             {
-               [image].map((img,i) =>
-                    <div key={i} className="index-product-single-image">
-                    <img src={img?.src || image2} alt="product"/>
+                [image].map((img, i) =>
+                    <div key={ i } className="index-product-single-image">
+                        <img src={ img.src || image2 } alt="product"/>
                     </div>
                 )
             } 
-        </div>
-    )
-}
-function CommentImageComponent(props) {
-    const { image } = props;
+            </div>
+        )
+    }
+
     return (
-        <div className="comment-product-image-panel">
-            {
-                image.map((img,i) =>
-                    <div key={i} className="comment-product-image">
-                    <img src={img.src || image2} alt="product"/>
-                    </div>
-                )
-            } 
+        <div className="index-product-single-image-panel" onClick={()=>viewProduct(product)}>
+        {
+            [image].map((img, i) =>
+                <div key={ i } className="index-product-single-image">
+                    <img src={ img?.src || image2 } alt="product"/>
+                </div>
+            )
+        } 
         </div>
     )
 }
+
 
 function DoubleImageComponent(props) {
     const { images } = props;
@@ -71,6 +90,7 @@ function DoubleImageComponent(props) {
         </div>
     )
 }
+
 function TrippleImageComponent(props) {
     const { images } = props;
     return (
@@ -89,5 +109,5 @@ export {
     SingleImageComponent,
     DoubleImageComponent,
     TrippleImageComponent,
-    CommentImageComponent,
+    // CommentImageComponent,
 }
