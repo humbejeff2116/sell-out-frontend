adb712793904ec297e0f873ca7b589c0fd04b09a
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-22 11:32:02 +0100

    refactor DisplayedProduct component to enable it to be used in un-authenticated routes and remove excess white space

--- src/Components/Product/product.js
+++ src/Components/Product/product.js
@@ -1,114 +1,99 @@
 
 import React, { useState, useEffect } from 'react';
-import { CommentBox } from './CommentBox/commentBox';
-import ModalComment from '../ModalComments/modalComments';
-import ModalProduct from './ModalProduct/modalProduct';
+import ModalReviews from '../ModalReviews/modalReviews';
 import { SingleImageComponent } from './ImageComp/imageComp';
-import { OpenComment, Star, ProfileAvatar, Heart } from './Fragments/productFragments';
+import { ReviewsIconWrapper, Star, ProfileAvatar, Heart } from './Fragments/productFragments';
 import ProductLib from '../../Library/product/productLib';
 import UserLib from '../../Library/user/userLib';
 import useAuth from '../../Context/context';
 import './product.css';
 
 
-export  function DisplayedProduct({ product, panelClassName, ...props}) {
-
+export  function DisplayedProduct({ 
+    product,
+    panelClassName,
+    dontShowModal,
+    dontShowReviews,
+    showLikes,
+    productUsedOutsideLogin,
+    ...props
+}) {
     let [starCount, setStarCount] = useState(0);
     let [likeCount, setLikeCount] = useState(0);
     const [starsUserRecieved, setStarsUserRecieved] = useState([]);
     const [showReviews, setShowReviews] = useState(false);
+    const [showReviewsChild, setShowReviewsChild] = useState(false);
     const [likesProductRecieved, setLikesProductRecieved] = useState([]);
     const { user } = useAuth();
-   
-    useEffect(() => {
+    let timer = null;
 
+    useEffect(() => {
+        return () => {
+            if (timer) clearTimeout(timer);
+        }
+    },[timer])
+
+    useEffect(() => {
         const  userId = product.userId;
-
         UserLib.getSellerStarsAndSetStarCount(userId, user, setStarCount, setStarsUserRecieved)
+    }, [product, user]);
 
-    }, [ product, user ]);
-
-    useEffect(() => {
-        
+    useEffect(() => { 
         let mounted = true;
         const  userId = product.userId;
         const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-
         UserLib.getSellerStarsWhenUserDataChange({ mounted, userId, user, setStarCount, setStarsUserRecieved })
-
-        return ()=> {
-
-            mounted = false;
-
-        }
-
-    }, [ product ]);
+        return ()=> mounted = false;
+    }, [product]);
 
     useEffect(() => {
-       
         const  productId = product.productId;
-
         ProductLib.getProductLikesAndSetLikeCount(productId, user, setLikeCount, setLikesProductRecieved)
-
-    }, [ product, user ]);
+    }, [product, user]);
 
     useEffect(() => {
-      
         let mounted = true;
         const  productId = product._id;
         const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-
         ProductLib.getProductLikesWhenProductDataChange(mounted, productId, user, setLikeCount, setLikesProductRecieved)
-
-        return ()=> {
-
-            mounted = false;
-
-        }
-
-    }, [ product ]);
+        return ()=>  mounted = false;
+    }, [product]);
 
  
-    const openCommentBox = () => {
+    const openReviewBox = () => {
         setShowReviews(true);
+        timer = setTimeout(() => {
+            setShowReviewsChild(true);
+        }, 300);
     }
 
-    const closeCommentBox = () => {
-        setShowReviews(false);
+    const closeReviewBox = () => {
+        setShowReviewsChild(false);
+        timer = setTimeout(() => {
+            setShowReviews(false);
+        }, 800);
     }  
 
     const imageComponent = ( 
-
-        <SingleImageComponent product = { product } image = { product.productImages[0] }/>
-
-    )
-
-    const ModalComments = (
-
-        <ModalComment  
-        handleClose = { closeCommentBox }
-        modalDisplayedProduct = {
-            <ModalProduct
-            product = { product }
-            ProductPanelClassName = "modal-comment-product-panel"
-            />
-        }
-        commentBox = {
-            <CommentBox
-            product = { product }
-            closeCommentBox = { closeCommentBox }
-            // productCommentPanelName="modal-product-panel"
-            commentBoxPanelClassName = "modal-comment-box-panel"
-            />
-        }
+        <SingleImageComponent 
+        product = { product } 
+        image = { product.productImages[0] }
+        usedOutsideLogin ={ productUsedOutsideLogin }
         />
-            
     )
 
-    return (
-        
+    const modal = (
+        <ModalReviews 
+        product = { product }
+        closeReviewBox = { closeReviewBox }
+        handleClose = { closeReviewBox }
+        showModalChild = { showReviewsChild }
+        />
+    )
+
+    return ( 
         <>
-            { showReviews && ModalComments }
+            { dontShowModal ? "" : showReviews && modal }
             <div className = { panelClassName }>
                 <div className="index-product-profile-panel">
                     <ProfileAvatar product = { product } />
@@ -131,8 +116,12 @@ export  function DisplayedProduct({ product, panelClassName, ...props}) {
                         <Price { ...product }/>
                     </div>
                 </div>
-                <div className="index-product-reaction-panel">
-                    <div className="index-product-reaction-star">
+                <div className="index-product-bottom-panel">
+                    <ReviewsIconWrapper 
+                    openReviews = { openReviewBox }
+                    dontShowReviews = { dontShowReviews }
+                    /> 
+                    <div className="index-product-likes-container">
                         <Heart
                         product = { product }
                         user = { user }
@@ -141,74 +130,51 @@ export  function DisplayedProduct({ product, panelClassName, ...props}) {
                         setLikeCount = { setLikeCount }
                         likeCount = { likeCount }
                         likeProduct = { ProductLib.likeProduct }
+                        showLikes = { showLikes }
                         />
-                    </div>
-                    <OpenComment openCommentBox = { openCommentBox } />
+                    </div> 
                 </div>
             </div>
         </>
-
     )
-
 }
 
 
 export function Price({ percentageOff, productPrice, className, showPriceTag }) {
-
     const priceContainerClass = className ? className : "index-product-details-price";
 
     if (percentageOff) {
-
         const percentOffPrice = (percentageOff / 100) * parseFloat(productPrice)
         const newPrice = (parseFloat(productPrice) - percentOffPrice).toFixed(2, 10);
 
         if (!showPriceTag) {
-
             return (
-
                 <div className={ priceContainerClass }>
                     <p> £{ newPrice } { `(${percentageOff}% OFF)` } <span> £{ parseFloat(productPrice).toFixed(2, 10) } </span></p>
                 </div>
-
             )
-
         } else {
-
             return (
-
                 <div className={ priceContainerClass }>
                     <p>Price: <span className="price"> £{ newPrice } { `(${ percentageOff }% OFF)` }</span> <span className="original-price">£{ parseFloat( productPrice).toFixed(2, 10) }</span></p>
                 </div>
-    
             )
         }
-
     }
 
     if (!percentageOff) {
-
         if (showPriceTag) {
-
              return (
-
                 <div className={ priceContainerClass }>
                     <p>Price: <span className="price"> £{ parseFloat(productPrice).toFixed(2, 10) }</span> </p>
                 </div>
-
             )
-
         } else {
-
             return (
-
                 <div className={ priceContainerClass }>
                     <p> £{ parseFloat(productPrice).toFixed(2, 10) }</p>
                 </div>
-        
             )
-
         }
-
     }
-
 }
