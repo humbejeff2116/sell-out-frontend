de08f223c3f243d2339b40233da3be5493bd4a6b
humbejeff2116 <humbejeff2116@gmail.com>
2022-07-22 11:21:24 +0100

    rename OpenComment component to ReviewsIconWrapper, modify its functionality and clean module code

--- src/Components/Product/Fragments/productFragments.js
+++ src/Components/Product/Fragments/productFragments.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { AiOutlineStar, AiFillStar } from 'react-icons/ai';
+import { AiOutlineStar, AiFillStar, AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { BiMessageSquareEdit } from 'react-icons/bi';
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import profileAvatar from '../../../Images/avatar4.png';
 
 
-function OpenComment({ openCommentBox }) {
-
-    return (
-        
-        <div className="index-product-reaction-comments">
-           < BiMessageSquareEdit onClick = { openCommentBox } className="nav-icon"/>Reviews
-        </div>
-
+function ReviewsIconWrapper({ openReviews, dontShowReviews }) {
+    const reviewsIconContainerClassName = dontShowReviews ? (
+        "index-product-reviews-icon-container dont-show" 
+    ) : (
+        "index-product-reviews-icon-container"
     )
 
+    const reviewsIconClassName = dontShowReviews ? (
+        "index-product-reviews-icon" 
+    ) : (
+        "index-product-reviews-icon"
+    )
+    return (
+        <div 
+        className={ reviewsIconContainerClassName } 
+        disabled = { dontShowReviews ? true : false } 
+        onClick = { openReviews  }
+        >
+           < BiMessageSquareEdit  className={ reviewsIconClassName }/>Reviews
+        </div>
+    )
 }
 
-// star
 function Star({ 
     starCount, 
     starSeller, 
@@ -27,36 +36,28 @@ function Star({
     starIconClassName,
     iconWrapperClassName,
     setStarsUserRecieved,
-     setStarCount 
+    setStarCount,
+    ...props 
 }) {
-
     let starIcon;
+    let starsLength = starsUserRecieved.length; 
 
     if (starCount) {
-
-        starIcon = <AiFillStar className={ starIconClassName || "nav-icon" }  />
-
+        starIcon = <AiFillStar className={ starIconClassName || "nav-icon" }/>
     } else {
-
-        starIcon = < AiOutlineStar className={ starIconClassName || "nav-icon" }  /> 
-
+        starIcon = < AiOutlineStar className={ starIconClassName || "nav-icon" }/> 
     }
 
     return (
-
-        <div className= { iconWrapperClassName || "index-product-profile-star"} onClick = { ()=> starSeller(product, user, starCount, setStarsUserRecieved, setStarCount) }>
-          {starIcon}
-            { 
-                (starsUserRecieved && starsUserRecieved.length) ? 
-                starsUserRecieved.length : ''
-            }
+        <div 
+        className= { iconWrapperClassName || "index-product-profile-star" } 
+        onClick = { ()=> starSeller(product, user, starCount, setStarsUserRecieved, setStarCount) }
+        >
+            { starIcon } { starsUserRecieved && starsLength > 0  && starsLength }
         </div>
-
     )
-
 }
 
-// Heart
 function Heart({ 
     likeCount, 
     likeProduct, 
@@ -66,60 +67,44 @@ function Heart({
     iconWrapperClassName,
     heartIconClassName,
     setLikesProductRecieved, 
-    setLikeCount, 
+    setLikeCount,
+    showLikes, 
     ...props 
 }) {
-
     let heartIcon;
+    const likes = likesProductRecieved.length
 
     if (likeCount) {
-
-        heartIcon = <AiFillHeart className={ heartIconClassName || "nav-icon" } />
-
+        heartIcon = <AiFillHeart className={ heartIconClassName || "nav-icon" }/>
     } else {
-
-        heartIcon = < AiOutlineHeart className={ heartIconClassName || "nav-icon" } />
-
+        heartIcon = < AiOutlineHeart className={ heartIconClassName || "nav-icon" }/>
     }
 
     return (
-
-        <div className= {iconWrapperClassName || "index-product-heart-bttn"} onClick = { ()=> likeProduct(product, user, likeCount, setLikesProductRecieved, setLikeCount) }>
-          { heartIcon }
-            { 
-                (likesProductRecieved && likesProductRecieved.length) ? 
-                likesProductRecieved.length : ''
-            }
+        <div 
+        className= { iconWrapperClassName || "index-product-heart-bttn" } 
+        onClick = { ()=> likeProduct(product, user, likeCount, setLikesProductRecieved, setLikeCount) }
+        >
+        { heartIcon } { showLikes ? (likesProductRecieved && likes > 0  && likes) : "" }  
         </div>
-
     )
-
 }
 
 function ProfileAvatar({ product, brandName, imageWrapperClassName }) {
-
     const sellerBrandName = product?.brandName ?? product?.userName ?? brandName;
-
-    const viewSeller = (product) => {
-        // TODO... call view context function to save view id and redirect to view page
-        // const { userId } = product;
-    }
-
-    const imageSrc = (product?.userProfileImage === "no-image" || !product?.userProfileImage ) ? null : product?.userProfileImage 
+    const viewSeller = (product) => {}
+    const imageSrc = (product?.userProfileImage === "no-image" || !product?.userProfileImage) ? null : product?.userProfileImage 
 
     return (
-
         <div className={ imageWrapperClassName || "index-product-profile" }>
             <img src={ imageSrc || profileAvatar } alt="seller" width="100%" height="auto" />
-            <span onClick = { viewSeller(product) }> { sellerBrandName } </span>
-        </div>
-        
-    )
-   
+            <span onClick = { viewSeller(product) }>{ sellerBrandName }</span>
+        </div>   
+    )  
 }
 
 export {
-    OpenComment,
+    ReviewsIconWrapper,
     Star,
     ProfileAvatar,
     Heart,
