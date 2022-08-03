ab68e2bcaa3f575c5e09541040e12aea6a0f671d
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-03 12:28:28 +0100

    remove excessive whitespce

--- src/Components/IndexSideNav/indexSideNav.js
+++ src/Components/IndexSideNav/indexSideNav.js
@@ -8,97 +8,73 @@ import './indexSideNav.css';
 const indexSideNavLinks = Links.getIndexSidenavLinks();
 
 export default function IndexSideNav({ links }) {
-
     return (
-
         <nav className="index-side-nav">
         {
             links ? links.map((link, i) =>
-
                 <NavigationLink key = { i } { ...link }/>
-
             ) : indexSideNavLinks?.map((link, i) =>
-
                 <NavigationLink key = { i } { ...link }/>
-
             )
         }
         </nav>
-
     )
-
 }
 
-function NavigationLink({ match, ...props }) {
-
+function NavigationLink({ 
+    match, 
+    ...props 
+}) {
     const [expandAccordion, setExpandAccordion] = useState(false)
 
     const toggleAccordion = (e) => {
-
         e.preventDefault();
-
         setExpandAccordion(state => !state);
-
         e.stopPropagation();
-
     }
 
-    const navItemClassName = (expandAccordion) ? "index-side-nav-item-accordion-open" : "index-side-nav-item"; 
+    const navItemClassName = expandAccordion ? "index-side-nav-item-accordion-open" : "index-side-nav-item"; 
 
     return (
-
         <div className = { navItemClassName }>
-            {
-
-                props.accordion ? (
-
-                    <Accordion 
-                    {...props} 
-                    expandAccordion = { expandAccordion }
-                    toggleAccordion = { toggleAccordion }
-                    /> 
-
-                ) : (
-
-                    <NavLink
-                    exact 
-                    to = { props.href } 
-                    activeClassName="index-side-link-active"
-                    className="index-side-nav-link" 
-                    >
-                        <i>{ props.icon }</i>{ props.name } 
-                    </NavLink> 
-
-                )
-
-            }
-            
+        {
+            props.accordion ? (
+                <Accordion 
+                {...props} 
+                expandAccordion = { expandAccordion }
+                toggleAccordion = { toggleAccordion }
+                /> 
+            ) : (
+                <NavLink
+                exact 
+                to = { props.href } 
+                activeClassName="index-side-link-active"
+                className="index-side-nav-link" 
+                >
+                    <i>{ props.icon }</i>{ props.name } 
+                </NavLink> 
+            )
+        }  
         </div>
-
     ) 
-
 }
 
-const Accordion = memo(({ expandAccordion, toggleAccordion, ...props }) => {
-
-    const accordionClassName = (expandAccordion) ? "index-side-nav-link active" : "index-side-nav-link";
-
-    const accordionIconClassName = (expandAccordion) ? "index-side-nav-link-open-icon open" : "index-side-nav-link-open-icon";
-    
+const Accordion = memo(({ 
+    expandAccordion, 
+    toggleAccordion, 
+    ...props 
+}) => {
+    const accordionClassName = expandAccordion ? "index-side-nav-link active" : "index-side-nav-link";
+    const accordionIconClassName = expandAccordion ?  "index-side-nav-link-open-icon open" : "index-side-nav-link-open-icon";
     let AccordionIcon;
     
     if (expandAccordion) {
-
         AccordionIcon = <span className = { accordionIconClassName }> - </span>
-    
     } else {
-
         AccordionIcon = <span className = { accordionIconClassName }> + </span>
-    
     }
     
     return (
-        
         <div className="index-side-nav-accordion-cntr">
             <NavLink
             exact 
@@ -110,22 +86,16 @@ const Accordion = memo(({ expandAccordion, toggleAccordion, ...props }) => {
                 <i>{ props.icon }</i>{ props.name } { AccordionIcon }
             </NavLink> 
             {
-                (expandAccordion) && (
-
+                expandAccordion && (
                     <div className="index-side-nav-nested-items-container">
                     {
                         props.links.map((link, i) =>
-
                             <NavigationLink  key = { i }  { ...link }/>
-
                         )
                     }
-                    </div>
-                    
+                    </div>  
                 )
             }
         </div>
-
     )
-
 })
