5b745c3a1cf7c0d1bc26c94ec602ccbe701eedd8
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-06 09:23:38 +0100

    move HeartGiver component from productsFragment to its module and refactor it to have its own internal state

---
+++ src/Components/HeartGiver/heartGiver.js
@@ -0,0 +1,65 @@
+
+import React, { useState, useEffect } from 'react';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import ProductLib from '../../Library/product/productLib';
+import useAuth from '../../Context/context';
+import styles from './HeartGiver.module.css';
+
+
+
+export default function HeartGiver({  
+    product,  
+    iconWrapperClassName,
+    heartIconClassName,
+    showLikes, 
+    ...props  
+}) {
+    let [likeCount, setLikeCount] = useState(0);
+    const [likesProductRecieved, setLikesProductRecieved] = useState([]);
+    const { user } = useAuth();
+    const likes = likesProductRecieved.length;
+
+    useEffect(() => {
+        const  productId = product.productId;
+        ProductLib.getProductLikesAndSetLikeCount(
+            productId, 
+            user, 
+            setLikeCount, 
+            setLikesProductRecieved
+        )
+    }, [product, user]);
+
+    useEffect(() => {
+        let mounted = true;
+        const  productId = product._id;
+        const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+        ProductLib.getProductLikesWhenProductDataChange(
+            mounted, 
+            productId, 
+            user, 
+            setLikeCount, 
+            setLikesProductRecieved
+        )
+        return ()=>  mounted = false;
+    }, [product]);
+
+    return (
+        <div 
+        className= {iconWrapperClassName || styles.container} 
+        onClick = {()=> ProductLib.likeProduct(
+            product, 
+            user, 
+            likeCount, 
+            setLikesProductRecieved, 
+            setLikeCount
+        )}
+        >
+        {likeCount ? (
+            <AiFillHeart className={ heartIconClassName || styles.heartIcon }/>
+        ) : (
+            < AiOutlineHeart className={ heartIconClassName || styles.heartIcon }/>
+        )} 
+        {showLikes ? (likesProductRecieved && likes > 0  && likes) : ""}  
+        </div>
+    )
+}
