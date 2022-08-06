c74b56b3359f57bbfb85f6f5169e4930f1bfc133
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-06 09:31:36 +0100

    attach StarGiver and HeartGiver components (initially Star and Heart)

--- src/Components/Product/product.js
+++ src/Components/Product/product.js
@@ -2,10 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import ModalReviews from '../ModalReviews/modalReviews';
 import { SingleImageComponent } from './ImageComp/imageComp';
-import { ReviewsIconWrapper, Star, ProfileAvatar, Heart } from './Fragments/productFragments';
-import ProductLib from '../../Library/product/productLib';
-import UserLib from '../../Library/user/userLib';
-import useAuth from '../../Context/context';
+import { ReviewsIconWrapper, ProfileAvatar } from './Fragments/productFragments';
+import StarGiver from '../StarGiver/starGiver';
+import HeartGiver from '../HeartGiver/heartGiver';
 import './product.css';
 
 
@@ -18,13 +17,8 @@ export  function DisplayedProduct({
     productUsedOutsideLogin,
     ...props
 }) {
-    let [starCount, setStarCount] = useState(0);
-    let [likeCount, setLikeCount] = useState(0);
-    const [starsUserRecieved, setStarsUserRecieved] = useState([]);
     const [showReviews, setShowReviews] = useState(false);
     const [showReviewsChild, setShowReviewsChild] = useState(false);
-    const [likesProductRecieved, setLikesProductRecieved] = useState([]);
-    const { user } = useAuth();
     let timer = null;
 
     useEffect(() => {
@@ -33,33 +27,6 @@ export  function DisplayedProduct({
         }
     },[timer])
 
-    useEffect(() => {
-        const  userId = product.userId;
-        UserLib.getSellerStarsAndSetStarCount(userId, user, setStarCount, setStarsUserRecieved)
-    }, [product, user]);
-
-    useEffect(() => { 
-        let mounted = true;
-        const  userId = product.userId;
-        const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-        UserLib.getSellerStarsWhenUserDataChange({ mounted, userId, user, setStarCount, setStarsUserRecieved })
-        return ()=> mounted = false;
-    }, [product]);
-
-    useEffect(() => {
-        const  productId = product.productId;
-        ProductLib.getProductLikesAndSetLikeCount(productId, user, setLikeCount, setLikesProductRecieved)
-    }, [product, user]);
-
-    useEffect(() => {
-        let mounted = true;
-        const  productId = product._id;
-        const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-        ProductLib.getProductLikesWhenProductDataChange(mounted, productId, user, setLikeCount, setLikesProductRecieved)
-        return ()=>  mounted = false;
-    }, [product]);
-
- 
     const openReviewBox = () => {
         setShowReviews(true);
         timer = setTimeout(() => {
@@ -74,44 +41,32 @@ export  function DisplayedProduct({
         }, 800);
     }  
 
-    const imageComponent = ( 
-        <SingleImageComponent 
-        product = { product } 
-        image = { product.productImages[0] }
-        usedOutsideLogin ={ productUsedOutsideLogin }
-        />
-    )
-
-    const modal = (
-        <ModalReviews 
-        product = { product }
-        closeReviewBox = { closeReviewBox }
-        handleClose = { closeReviewBox }
-        showModalChild = { showReviewsChild }
-        />
-    )
-
     return ( 
         <>
-            { dontShowModal ? "" : showReviews && modal }
+            {dontShowModal ? "" : showReviews && (
+                <ModalReviews 
+                product = { product }
+                closeReviewBox = { closeReviewBox }
+                handleClose = { closeReviewBox }
+                showModalChild = { showReviewsChild }
+                />
+            )}
             <div className = { panelClassName }>
                 <div className="index-product-profile-panel">
                     <ProfileAvatar product = { product } />
-                    <Star
+                    <StarGiver
                     product = { product }
-                    user = { user }
-                    starsUserRecieved = { starsUserRecieved }
-                    starCount = { starCount }
-                    setStarsUserRecieved ={  setStarsUserRecieved } 
-                    setStarCount ={ setStarCount }
-                    starSeller = { UserLib.starSeller }
                     />
                 </div>
                 <div className="index-product-image-wrapper">
-                    { imageComponent }
+                    <SingleImageComponent 
+                    product = { product } 
+                    image = { product.productImages[0] }
+                    usedOutsideLogin = { productUsedOutsideLogin }
+                    />
                     <div className="index-product-image-details">
                         <div className="index-product-details-name">
-                            <span>{ product.productName } </span>
+                            <span>{ product.productName }</span>
                         </div>
                         <Price { ...product }/>
                     </div>
@@ -122,14 +77,8 @@ export  function DisplayedProduct({
                     dontShowReviews = { dontShowReviews }
                     /> 
                     <div className="index-product-likes-container">
-                        <Heart
+                        <HeartGiver
                         product = { product }
-                        user = { user }
-                        likesProductRecieved = { likesProductRecieved }
-                        setLikesProductRecieved = { setLikesProductRecieved } 
-                        setLikeCount = { setLikeCount }
-                        likeCount = { likeCount }
-                        likeProduct = { ProductLib.likeProduct }
                         showLikes = { showLikes }
                         />
                     </div> 
