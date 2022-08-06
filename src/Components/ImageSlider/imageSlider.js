70986a828fb59dc2f5c965ff2c26ff02052a7e1f
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-06 16:42:48 +0100

    refactor ImageSLider component functionality

--- src/Components/ImageSlider/imageSlider.js
+++ src/Components/ImageSlider/imageSlider.js
@@ -1,17 +1,29 @@
 import React, { useEffect, useState, useRef } from 'react';
-import {  
-    MdArrowForwardIos, 
-    MdArrowBackIosNew 
-}  from 'react-icons/md';
+import { MdArrowForwardIos,MdArrowBackIosNew }  from 'react-icons/md';
 import image2 from '../../Images/product4.webp';
+import image1 from '../../Images/product3.webp';
+import image3 from '../../Images/product6.webp';
+import image4 from '../../Images/product.webp';
+import image5 from '../../Images/product5.webp';
 import styles from './ImageSlider.module.css';
 
+const images = [
+    {src: image2}, 
+    {src: image1},
+    {src: image3},
+    {src: image4},
+    {src: image5},
+]
 
-export default function ImageSLider({ images, ...props }) {
+export default function ImageSLider({ 
+    // images, 
+    dontShowThubnails,
+    onClickImage,
+    removeButtonBorder,
+    ...props 
+}) {
     const [sliderImages, setSliderImages] = useState([]);
     const [viewedImage, setViewedImage] = useState("");
-    const [loading, setloading] = useState(false);
-    const sliderImagesLength = useRef(images.length);
     const currentViewedImageIndex = useRef(0);
 
     useEffect(()=> {
@@ -19,63 +31,118 @@ export default function ImageSLider({ images, ...props }) {
             setViewedImage(images[0].src);
             setSliderImages(images);
         }   
-    }, [images])
+    }, [])
 
     const setProductImage = (src) => {
+        const imageIndex = getImageIndex(src);
+        if (imageIndex < 0) {
+            return;
+        }
+        currentViewedImageIndex.current = imageIndex;
         return setViewedImage(src);
     }
 
+    const getImageIndex = (imageSrc) => {
+        const index = sliderImages.findIndex(({src})=> src === imageSrc);
+        return index;
+    }
+
     const nextImage = () => {
-        if (currentViewedImageIndex.current + 1 === sliderImagesLength) return;
+        if (isLastImage()) return;
         setViewedImage(sliderImages[++currentViewedImageIndex.current].src);
     }
 
     const prevImage = () => {
-        if (currentViewedImageIndex.current === 0) return;
+        if (isFirstImage()) return;
         setViewedImage(sliderImages[--currentViewedImageIndex.current].src);
     }
 
+    const isFirstImage = () => {
+        if (currentViewedImageIndex.current === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    const isLastImage = () => {
+        const lastImageSrc = sliderImages[sliderImages.length - 1].src;
+        if (viewedImage === lastImageSrc) { 
+            return true;
+        }
+        return false;
+    }
+
     return (
-        <div className={ styles.imageSLiderContainer }>
-            <div className={ styles.imageSLiderThumbnailsContainer }>
-            {
-                sliderImages.length > 0 && (
-                    sliderImages.map((image, i)=>
-                        <ImageSliderThumbnail 
-                        key={ i } 
-                        { ...image } 
-                        currentlyViewedImage = { viewedImage } 
-                        setViewImage = { setProductImage }
-                        />
-                    ) 
-                )
-            }
-            </div>
-            <div className={ styles.imageSLiderPanel }>
-                <div className={ styles.imageSLiderImage }>
-                    <img src = { viewedImage || image2 } alt="product"/>
+        <div className={ styles.container }>
+            {dontShowThubnails ? "" : (
+                <div className={ styles.thumbnailsContainer }>
+                {sliderImages.length > 0 && sliderImages.map((image, i)=>
+                    <ImageThumbnails 
+                    key={ i } 
+                    { ...image } 
+                    currentlyViewedImage = { viewedImage } 
+                    setViewImage = { setProductImage }
+                    imageslength={images.length}
+                    />
+                )}
                 </div>
-                <div className={ styles.imageSLiderPrevButton } onClick = { prevImage }>
-                    <MdArrowBackIosNew className={ styles.imageSLiderButtonIcon }/>
+            )}
+            <div className={ styles.panel }>
+                <div className={ styles.imageWrapper }>
+                    <img 
+                    src = {viewedImage} 
+                    alt="product"
+                    onClick={onClickImage}
+                    />
                 </div>
-                <div className={ styles.imageSLiderNextButton } onClick = { nextImage }>
-                    <MdArrowForwardIos className={ styles.imageSLiderButtonIcon }/>
-                </div>
+                {sliderImages.length > 0 && ( 
+                    <button 
+                    className={`
+                        ${styles.prevButton} 
+                        ${removeButtonBorder ? styles.noBorder : ""} 
+                        ${isFirstImage() ? "" : styles.showButton}
+                    `} 
+                    onClick = {prevImage}
+                    >
+                        <MdArrowBackIosNew className={styles.buttonIcon}/>
+                    </button>   
+                )}
+                {sliderImages.length > 0 && (                        
+                    <button 
+                    className={`
+                        ${styles.nextButton} 
+                        ${removeButtonBorder ? styles.noBorder : ""}
+                        ${isLastImage() ? "" : styles.showButton}
+                    `} 
+                    onClick = {nextImage}
+                    >
+                        <MdArrowForwardIos className={styles.buttonIcon}/>
+                    </button>
+                )}
             </div>
         </div>
     )
 }
 
-function ImageSliderThumbnail({src, currentlyViewedImage, setViewImage}) {
-    const thumbnailClassName = ( src === currentlyViewedImage ) ? (
-        `${styles.imageSliderThumbnail} ${styles.focus}`
+export function ImageThumbnails({
+    src, 
+    currentlyViewedImage, 
+    setViewImage,
+    imageslength,
+    ...props
+}) {
+    const thumbnailClassName = (src === currentlyViewedImage) ? (
+        `${styles.thumbnailWrapper} ${styles.focus} ${imageslength > 5 ? styles.thumbnailWrapperOverflow : ""}`
     ) : (
-        `${styles.imageSLiderThumbnail}`
+        `${styles.thumbnailWrapper} ${imageslength > 5 ? styles.thumbnailWrapperOverflow : ""}`
     )
 
     return (
-        <div className={ thumbnailClassName } onClick={ ()=> setViewImage(src) }>
-            <img src={src || image2} alt={ src }/>
+        <div 
+        className={thumbnailClassName} 
+        onClick={ ()=> setViewImage(src) }
+        >
+            <img src={src} alt=""/>
         </div>
     )
 }
