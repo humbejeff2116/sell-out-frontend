9ddd8503546eb80ac03513ec5449ed8fbe9c8eda
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-07 17:54:46 +0100

    add Buy Products Page to pages module

---
+++ src/Pages/OutsideLoginBuyProductsPage/buyProductsPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import BuyProducts from '../../Components/OutsideLoginBuyProducts/buyProducts';
+import PageWrapper from '../PageWrapper/pageWrapper';
+
+
+export default function BuyProductsPage() {
+    return (
+        <PageWrapper>
+            <BuyProducts/>
+        </PageWrapper>   
+    )
+}
