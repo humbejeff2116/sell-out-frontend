ad682c39c174d890f41a2c1e3733ffd801b6b028
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-07 17:48:30 +0100

    refactor NotFoundPage to use LandingTemplate

--- src/Pages/NotFoundPage/notFoundPage.js
+++ src/Pages/NotFoundPage/notFoundPage.js
@@ -1,17 +1,16 @@
 
-
-
 import React from 'react';
-
 import NotFound from '../../Components/NotFound/notFound';
-import {NotFoundTemplate} from '../../Components/Landing/landing';
+import { LandingTemplate } from '../../Components/Landing/landing';
 
 
 
 
 export default function NotFoundPage() {
     return (
-        <NotFoundTemplate notFoundComponent={<NotFound/>} />
+        <LandingTemplate 
+        landingTopChild={<NotFound/>}
+        />
     )
 }
 
