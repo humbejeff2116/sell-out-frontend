dffb2984d77c49ff9da781fb8180fab76af6227f
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-07 17:55:33 +0100

    add sell products page to pages module

---
+++ src/Pages/OutsideLoginSellProductsPage/sellProductsPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import SellProducts from '../../Components/OutsideLoginSellProducts/sellProducts';
+import PageWrapper from '../PageWrapper/pageWrapper';
+
+
+export default function SellProductsPage() {
+    return (
+        <PageWrapper>
+            <SellProducts/>
+        </PageWrapper> 
+    )
+}
