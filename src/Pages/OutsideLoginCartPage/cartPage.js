350e5a8f12d62ec2e7aaeb664ec358fac86b8cc1
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-07 17:57:01 +0100

    add cart page (for un-authenticated routes) to pages module

---
+++ src/Pages/OutsideLoginCartPage/cartPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Cart from '../../Components/OutsideLoginCart/cart';
+import PageWrapper from '../PageWrapper/pageWrapper';
+
+
+export default function CartPage() {
+    return (
+        <PageWrapper>
+            <Cart/>
+        </PageWrapper>   
+    )
+}
