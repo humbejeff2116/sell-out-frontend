d02500dfdd6e91f3b931e4a1edba20a469de90ad
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-07 17:53:19 +0100

    add About Page to Pages module

---
+++ src/Pages/OutsideLoginAboutPage/aboutPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import About from '../../Components/OutsideLoginAbout/about';
+import PageWrapper from '../PageWrapper/pageWrapper';
+
+
+export default function AboutPage() {
+    return (
+        <PageWrapper>
+            <About/>
+        </PageWrapper>   
+    )
+}
