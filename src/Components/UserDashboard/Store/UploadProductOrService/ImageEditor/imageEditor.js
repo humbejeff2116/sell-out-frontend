e558b37380ecce86b064adbe1111277cf04fc29a
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-11 19:28:49 +0100

    implement ImageEditor component functionality

--- src/Components/UserDashboard/Store/UploadProductOrService/ImageEditor/imageEditor.js
+++ src/Components/UserDashboard/Store/UploadProductOrService/ImageEditor/imageEditor.js
@@ -1,44 +1,127 @@
 
 import React, { useEffect, useState, useRef } from 'react';
 import { RiCloseFill } from "react-icons/ri";
-import { ModalBox } from '../../../../ModalComments/modalComments';
+import { MdArrowForwardIos,MdArrowBackIosNew }  from 'react-icons/md';
+import { ModalBox } from '../../../../ModalReviews/modalReviews';
+import { ImageThumbnails } from '../../../../ImageSlider/imageSlider';
+// import ReactCrop from "react-image-crop";
+// import "react-image-crop/dist/ReactCrop.css";
 import './imageEditor.css'
 
 
 export default function ImageEditor({ closeModal, images, ...props }) {
-    const [unCropedImages, setUnCropedImages] = useState([]);
-    const [cropedImages, setCropedImages] = useState([]);
-    const [currentViewedImage, setCurrentViewedImage] = useState({});
-    let currentViewedImageIndex = useRef(0);
-    let previewImagesLength = useRef(images.length);
+    const [sliderImages, setSliderImages] = useState([]);
+    const [viewedImage, setViewedImage] = useState({});
+    const [showCroppedImages, setShowCroppedImages] = useState(false);
+    const [image, setImage] = useState(null);
+    const [crop, setCrop] = useState({aspect: 16 / 9});
+    const [result, setResult] = useState(null);
+    const viewedImageIndex = useRef(0);
+    const previewImagesLength = useRef(images.length);
+    const cache = useRef({});
 
     useEffect(()=> {
-        setCurrentViewedImage(images[0]);
-
-        return () => {
-
+        if (images.length > 0) {
+            const imagesBlob = getImagesBlob(images);
+            setViewedImage(imagesBlob[0]);
+            setSliderImages(imagesBlob);
         }
     },[images])
 
-    const nextImage = () => {   
-        // stop the increment of currentViewedImage 
-        if (currentViewedImageIndex.current === previewImagesLength.current - 1) {
-            alert(true)
-            return;
+    const getImagesBlob = (images) => {
+        const imagesBlob = [];
+        for (let i = 0; i < images.length; i++) {
+            imagesBlob[i] = {src: URL.createObjectURL(images[i])}; 
         }
+        return imagesBlob;
+    }
 
-        ++currentViewedImageIndex.current
-        alert(currentViewedImageIndex.current)
+    const nextImage = () => {
+        if (isLastImage()) return;
+        setViewedImage(sliderImages[++viewedImageIndex.current].src);
     }
 
     const prevImage = () => {
-        if (currentViewedImageIndex.current === 0) {
-            alert(true)
+        if (isFirstImage()) return;
+        setViewedImage(sliderImages[--viewedImageIndex.current].src);
+    }
+
+    const isFirstImage = () => {
+        if (viewedImageIndex.current === 0) {
+            return true;
+        }
+        return false;
+    }
+
+    const isLastImage = () => {
+        if (viewedImageIndex.current === previewImagesLength.current - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    const setEditorUncroppedImage = (src) => {
+        const imageIndex = getImageIndex(src, sliderImages);
+
+        if (imageIndex < 0) {
             return;
         }
+        viewedImageIndex.current = imageIndex;
 
-        --currentViewedImageIndex.current
-        alert(currentViewedImageIndex.current)
+        if (showCroppedImages) {
+            setShowCroppedImages(false);
+            setSliderImages(cache.current.unCroppedImagesBlob);
+            cache.current = {}
+        }
+        return setViewedImage(src);
+    }
+
+    const setEditorCroppedImage = (src) => {
+        const imageIndex = getImageIndex(src, result);
+
+        if (imageIndex < 0) {
+            return;
+        }
+        viewedImageIndex.current = imageIndex;
+
+        if (!showCroppedImages) {
+            setShowCroppedImages(true);
+            cache.current.unCroppedImagesBlob = sliderImages;
+            setSliderImages(result);
+        }
+        return setViewedImage(src);
+    }
+
+    const getImageIndex = (imageSrc, sliderImages) => {
+        const index = sliderImages.findIndex(({src})=> src === imageSrc);
+        return index;
+    }
+
+    const getCroppedImg = async () => {
+        try {
+            const canvas = document.createElement("canvas");
+            const scaleX = image.naturalWidth / image.width;
+            const scaleY = image.naturalHeight / image.height;
+            canvas.width = crop.width;
+            canvas.height = crop.height;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(
+                image,
+                crop.x * scaleX,
+                crop.y * scaleY,
+                crop.width * scaleX,
+                crop.height * scaleY,
+                0,
+                0,
+                crop.width,
+                crop.height
+            );
+
+            const base64Image = canvas.toDataURL("image/jpeg", 1);
+            setResult(prevState => prevState ? ([...prevState, {src: base64Image}]) : ([{src: base64Image}]));
+        } catch (err) {
+            console.error(err);
+        }
     }
 
     return (
@@ -50,8 +133,20 @@ export default function ImageEditor({ closeModal, images, ...props }) {
                 modalContainer = "upload-product-editor-modal-container-wrapper"
                 >
                     <ImageEditorPanel
+                    handleClose = { closeModal }
                     next = { nextImage }
                     prev = { prevImage }
+                    images = { sliderImages }
+                    viewedImage = { viewedImage }
+                    setImage=  { setImage }
+                    crop = { crop }
+                    setCrop = { setCrop }
+                    result = { result }
+                    getCroppedImg = { getCroppedImg }
+                    setViewImage = { setEditorUncroppedImage }
+                    setViewCropImage = { setEditorCroppedImage }
+                    isFirstImage = { isFirstImage }
+                    isLastImage = { isLastImage }
                     />
                 </ModalBox>
             }
@@ -59,12 +154,28 @@ export default function ImageEditor({ closeModal, images, ...props }) {
     )
 }
 
-function ImageEditorPanel({ close, previous, next, ...props }) {
+function ImageEditorPanel({ 
+    handleClose, 
+    prev, 
+    next,
+    images,
+    viewedImage,
+    setViewImage,
+    setImage,
+    crop,
+    setCrop,
+    result,
+    setViewCropImage,
+    getCroppedImg,
+    isFirstImage,
+    isLastImage, 
+    ...props 
+}) {
     return (
         <div className="upload-product-editor-modal-child-wrapper">
             <div className="upload-product-editor-header">
                 <div className="upload-product-editor-header-top">
-                    <div className="upload-product-editor-header-top-close" onClick = { close }>
+                    <div className="upload-product-editor-header-top-close" onClick = { handleClose }>
                         <RiCloseFill className="upload-product-editor-close-icon"/>
                     </div>
                 </div>
@@ -81,27 +192,76 @@ function ImageEditorPanel({ close, previous, next, ...props }) {
                 {/* TODO... Extract immage slider component */}
                 <div className="upload-product-editor-body">
                     <div className="upload-product-editor-thumbnails">
-
+                    {images?.length > 0 && images.map((image, i) =>
+                        <ImageThumbnails 
+                        key={i} 
+                        {...image} 
+                        currentlyViewedImage = { viewedImage } 
+                        setViewImage = { setViewImage }
+                        imageslength={ images.length }
+                        />
+                    )}
                     </div>
                     <div className="upload-product-editor-panel">
                         <div className="upload-product-editor-image">
-
+                        {/* {props.viewedImage && (
+                             <div>
+                                <ReactCrop
+                                style={{maxWidth: "50%"}}
+                                src={props.viewedImage}
+                                onImageLoaded={props.setImage}
+                                crop={props.crop}
+                                onChange={props.setCrop}
+                                />
+                             </div>
+                         )} */}
                         </div>
-                        <div className="upload-product-editor-prev-button" onClick = { previous } >
-                            -
-                        </div>
-                        <div className="upload-product-editor-next-button" onClick = { next } >
-                            +
-                        </div>
+                        <button 
+                        className={`
+                            upload-product-editor-button prev-button ${isFirstImage() ? "" : "showButton"}
+                        `}
+                        onClick = { prev }
+                        >
+                            <MdArrowBackIosNew className="buttonIcon"/>
+                        </button>
+                        <button 
+                        className={`
+                            upload-product-editor-button next-button ${isLastImage() ? "" : "showButton"}
+                        `}
+                        onClick = { next }
+                        >
+                            <MdArrowForwardIos className="buttonIcon"/>
+                        </button>
                     </div>
+                    <div className="upload-product-editor-thumbnails">
+                        <div className="upload-product-editor-thumbnails-header">
+                            Cropped Images
+                        </div>
+                        {result?.length > 0 && result.map((image, i) =>
+                            <ImageThumbnails 
+                            key={i} 
+                            {...image} 
+                            currentlyViewedImage = { viewedImage } 
+                            setViewImage = { setViewCropImage }
+                            imageslength = { result.length }
+                            />
+                        )}
+                    </div>
+                    
                 </div>
             </div>
             <div className="upload-product-editor-footer">
                 <div className="upload-product-editor-footer-bttn-wrrpr crop">
-                    <button>Select</button>
+                    <button
+                    onClick={ getCroppedImg }
+                    >
+                        Select
+                    </button>
                 </div>
                 <div className="upload-product-editor-footer-bttn-wrrpr">
-                    <button>Cancel</button>
+                    <button>
+                        Cancel
+                    </button>
                 </div>
             </div>
         </div>
