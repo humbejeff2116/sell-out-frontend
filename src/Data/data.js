6ede48284a374d3b4528d05124ae1148b6c9af5b
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-18 17:12:10 +0100

    add reviewsSortData, checkout, paymentMethods properties to data class

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,15 +1,23 @@
 
+const data = {
+    reviewsSortData: [
+        {name: "Recomended"},
+        {name: "New"},
+        {name: "Old"}
+    ],
 
+    checkout: { 
+        options :[
+        {name: "Default payment method"},
+        {name: "Pay on delivery method"},
+    ],
+    paymentMethods: {
+        default : "Default payment method",
+        payOnDelivery: "Pay on delivery method"
+    }
+}
+}
 
-
-/**
- * @class
- */
-const ApplicationData = function(){ };
-
-
-
-
-
-
-export default new ApplicationData();
+export const reviewsSortData = data.reviewsSortData;
+export const chekoutOptions = data.checkout.options;
+export const checkoutPaymentMethods = data.checkout.paymentMethods;
