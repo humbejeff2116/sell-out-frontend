b6ceefaff27b1a545ab17e99736577dff68ef479
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-18 17:52:47 +0100

    refactor GettingStartedSideNav functionality that indicates a submitted application form route

--- src/Components/GettingStartedSideNav/gettingStartedSideNav.js
+++ src/Components/GettingStartedSideNav/gettingStartedSideNav.js
@@ -1,92 +1,65 @@
 
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useGetStartedContext } from '../../Context/context';
 import { IoMdCheckmarkCircle } from 'react-icons/io';
 import './gettingStartedSideNav.css';
 
 
-export default function GettingStartedSideNav(props) {
+export default function GettingStartedSideNav({ 
+    links, 
+    ...props
+}) {
+    const { submittedFormPaths } = useGetStartedContext();
+    const submittedFormPath = (href) => {
+        const formPath = submittedFormPaths?.find(path => path.href === href)
+        return formPath ?  true : false;
+    }
 
     return (
-
         <nav  className="getting-started-side-nav">
-        {
-            props?.links?.map((link, i) =>
-
-                <GettingStartedNavLink key={i} {...link} />
-
-            )
-        }
+        {links.map((link, i) =>
+            <GettingStartedNavLink 
+            key={i} 
+            {...link}
+            submittedFormPath={submittedFormPath} 
+            />
+        )}
         </nav>
-
     )
-
 }
 
-function GettingStartedNavLink({ match, ...props}) {
-
-    const { submittedFormPaths } = useGetStartedContext();
-
-    const linkClassName = useRef("getting-started-side-nav-link");
-
-    useEffect(()=> {
-       
-        const setLinkClassName = (submittedFormPaths) => {
-
-            linkClassName.current = "getting-started-side-nav-link";
-
-            if (submittedFormPaths.length < 1) {
-
-                linkClassName.current = "getting-started-side-nav-link";
-
-                return;
-                
-            }
-
-            submittedFormPaths.forEach(path => {
-
-                if (path.href === props.href) {
-
-                    assignClassName(path.href)
-
-                } 
-                
-            })
-
-            function assignClassName(href) {
-
-                linkClassName.current = "getting-started-side-nav-link form-data-set";
-
-            }
-
-        }
-
-        setLinkClassName(submittedFormPaths)
-
-    }, [submittedFormPaths, props.href])
+function GettingStartedNavLink({ 
+    match, 
+    href, 
+    icon, 
+    name,
+    submittedFormPath, 
+    ...props
+}) {
+    const preventLinkDefaultBehaviour = (e)=> {
+        e.preventDefault();
+        e.stopPropagation();
+    }
  
     return (
-
-        <div className = "getting-started-side-nav-item">
-            
+        <div className = "getting-started-side-nav-item">   
             <NavLink
             exact 
-            to = { props.href } 
+            to = {href} 
             activeClassName="getting-started-side-link-active"
-            className = { linkClassName.current } 
+            className = {` getting-started-side-nav-link ${submittedFormPath(href) ? "form-data-set" : ""}`} 
+            // onClick ={preventLinkDefaultBehaviour}
             >
             <i>
-            {
-                linkClassName.current === "getting-started-side-nav-link form-data-set" ?
-                <IoMdCheckmarkCircle  className = "getting-started-side-nav-icon"/> : props.icon
-            }
+            {submittedFormPath(href) ? (
+                <IoMdCheckmarkCircle  className = "getting-started-side-nav-icon"/>
+            ) : ( 
+                icon
+            )}
             </i>
-            { props.name } 
+            {name} 
             </NavLink> 
-
         </div>
-
     ) 
-
 }
