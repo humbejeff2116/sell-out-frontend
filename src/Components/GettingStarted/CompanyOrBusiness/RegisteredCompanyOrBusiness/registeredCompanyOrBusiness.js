755dee542886f12ff8c9dc14e30c6e3dd1a431ec
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-20 07:28:13 +0100

    clean registeredCompanyOrBusiness module code

--- src/Components/GettingStarted/CompanyOrBusiness/RegisteredCompanyOrBusiness/registeredCompanyOrBusiness.js
+++ src/Components/GettingStarted/CompanyOrBusiness/RegisteredCompanyOrBusiness/registeredCompanyOrBusiness.js
@@ -2,58 +2,42 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import { TextInput, TextAreaInput, Select } from '../../../Formik/formik';
+import { TextInput, Select } from '../../../Formik/formik';
 import { useGetStartedContext } from '../../../../Context/context';
 import Countries from '../../../../Data/countries';
 
+const africanCountries = Countries.getCountries({ continent:"Africa" });
+const nigeraianStates = Countries.getCountryStates({ continent:"Africa", country:"Nigeria" });
 
-export default function RegisteredCompanyOrBusiness(props) {
-
+export default function RegisteredCompanyOrBusiness({ 
+    handleSubmit,
+    prevAndNextButtons,
+    ...props 
+}) {
     const { registeredCompanyOrBusinessData } = useGetStartedContext();
 
-    const africanCountries = Countries.getCountries({ continent:"Africa" });
-
-    const nigeraianStates = Countries.getCountryStates({ continent:"Africa", country:"Nigeria" });
-
     return (
-
         <Formik
         initialValues = {{
-            countryOfIncorporation: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.countryOfIncorporation : '',
-
-            stateOfIncorporation: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.stateOfIncorporation : '',
-
-            legalCompanyName: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.legalCompanyName : '',
-
-            yearOfFoundation: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.yearOfFoundation : '',
-
-            nigerianTaxId: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.nigerianTaxId : '',
-
-            taxIdClassification: registeredCompanyOrBusinessData ?  
-            registeredCompanyOrBusinessData?.taxIdClassification : '',
+            countryOfIncorporation: registeredCompanyOrBusinessData?.countryOfIncorporation ?? '',
+            stateOfIncorporation: registeredCompanyOrBusinessData?.stateOfIncorporation ?? '',
+            legalCompanyName: registeredCompanyOrBusinessData?.legalCompanyName ?? '',
+            yearOfFoundation: registeredCompanyOrBusinessData?.yearOfFoundation ?? '',
+            nigerianTaxId: registeredCompanyOrBusinessData?.nigerianTaxId ?? '',
+            taxIdClassification: registeredCompanyOrBusinessData?.taxIdClassification ?? '',
         }}
-
-        validationSchema = { Yup.object({
-            
+        validationSchema = {Yup.object({
             countryOfIncorporation: Yup.string().required('Country of incorporation is required'),
             stateOfIncorporation: Yup.string().required('State of incorporation is required'),
             legalCompanyName: Yup.string().required('Legal company name is required'),
             yearOfFoundation: Yup.string().required('Year of foundation is required'),
             nigerianTaxId: Yup.string().required('Nigerian tax id is required'),
             taxIdClassification: Yup.string().required('Tax classification is required'),
-
         })}
-
-        onSubmit = { props.handleSubmit }
+        onSubmit = {handleSubmit}
         >
         <Form id="contactForm">
             <div className="getting-started-application-template-form-inputs">
-
                 <Select
                 label="Country of Incorporation"
                 labelClassName="company-form-group"
@@ -61,19 +45,13 @@ export default function RegisteredCompanyOrBusiness(props) {
                 errorClass="getting-started-application-template-form-error"
                 >
                 <option value="">Select</option>
-                {
-                    africanCountries.map((country, i) =>
-
-                        <Options 
-                        key = { i }
-                        { ...country }
-                        />
-
-                    )
-
-                }
+                {africanCountries.map((country, i) =>
+                    <Options 
+                    key = { i }
+                    { ...country }
+                    />
+                )}
                 </Select>
-
                 <Select
                 label="State of Incorporation"
                 labelClassName="company-form-group"
@@ -81,19 +59,13 @@ export default function RegisteredCompanyOrBusiness(props) {
                 errorClass="getting-started-application-template-form-error"
                 >
                 <option value="">Select</option>
-                {
-                    nigeraianStates.map((state, i) =>
-
-                        <Options 
-                        key = { i }
-                        { ...state }
-                        />
-
-                    )
-                    
-                }
+                {nigeraianStates.map((state, i) =>
+                    <Options 
+                    key = { i }
+                    { ...state }
+                    />
+                )}
                 </Select>
-
                 <TextInput
                 label="Legal Company Name"
                 labelClassName="company-form-group"
@@ -101,7 +73,6 @@ export default function RegisteredCompanyOrBusiness(props) {
                 type="text"
                 errorClass="getting-started-application-template-form-error"
                 />
-
                 <TextInput
                 label="Year of Foundation"
                 labelClassName="company-form-group"
@@ -109,7 +80,6 @@ export default function RegisteredCompanyOrBusiness(props) {
                 type="text"
                 errorClass="getting-started-application-template-form-error"
                 />
-
                 <TextInput
                 label="Nigerian Tax ID"
                 labelClassName="company-form-group"
@@ -118,7 +88,6 @@ export default function RegisteredCompanyOrBusiness(props) {
                 errorClass="getting-started-application-template-form-error"
                 placeholder ="XX-XXXXX"
                 />
-
                 <Select
                 label="Tax ID classification"
                 labelClassName="company-form-group"
@@ -129,30 +98,23 @@ export default function RegisteredCompanyOrBusiness(props) {
                     <option value="FIRS">FIRS</option>
                     <option value="JTB">JTB</option>
                 </Select>
-                
                 <span className="brand-name">
                 Each data or information provided here and here off is owned by you and as such, 
                 can be changed, modified or removed at any time under your settings. 
                 Kindly go through our privacy policy if you haven't, to know how
                 we use your data or how you can manage it. 
                 </span>
-
             </div>
-        
-        { props.prevAndNextButtons }
+            {prevAndNextButtons}
         </Form>
         </Formik>
-
     )
-
 }
 
-export function Options({ name, ...props }) {
-
+export function Options({ 
+    name, 
+}) {
     return (
-
-        <option  value= { name } > { name } </option>
-
+        <option value= { name } > {name} </option>
     )
-
 }
