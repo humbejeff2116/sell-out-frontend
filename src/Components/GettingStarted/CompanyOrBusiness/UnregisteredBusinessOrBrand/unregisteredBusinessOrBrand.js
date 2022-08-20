1c9781588093d928c6866f4f9c534eb218af2a3f
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-20 07:34:35 +0100

    clean unregisteredBusinessOrBrand module code

--- src/Components/GettingStarted/CompanyOrBusiness/UnregisteredBusinessOrBrand/unregisteredBusinessOrBrand.js
+++ src/Components/GettingStarted/CompanyOrBusiness/UnregisteredBusinessOrBrand/unregisteredBusinessOrBrand.js
@@ -2,53 +2,39 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import { TextInput, TextAreaInput, Select } from '../../../Formik/formik';
+import { TextInput, Select } from '../../../Formik/formik';
 import { Options } from '../RegisteredCompanyOrBusiness/registeredCompanyOrBusiness';
 import { useGetStartedContext } from '../../../../Context/context';
 import Countries from '../../../../Data/countries';
 
+const africanCountries = Countries.getCountries({continent: "Africa"});
+const nigeraianStates = Countries.getCountryStates({ continent: "Africa", country: "Nigeria" });
 
-export default function UnregisteredBusinessOrBrand(props) {
-
+export default function UnregisteredBusinessOrBrand({
+    handleSubmit,
+    prevAndNextButtons,
+    ...props
+}) {
     const { unregisteredBusinessData } = useGetStartedContext();
 
-    const africanCountries = Countries.getCountries({continent: "Africa"});
-
-    const nigeraianStates = Countries.getCountryStates({ continent: "Africa", country: "Nigeria" });
-
     return (
-
         <Formik
         initialValues = {{
-
-            countryFoundIn: unregisteredBusinessData ?
-            unregisteredBusinessData?.countryFoundIn : '',
-
-            stateFoundIn: unregisteredBusinessData ?
-            unregisteredBusinessData?.stateFoundIn : '',
-
-            businessOrBrandName: unregisteredBusinessData ?
-            unregisteredBusinessData?.businessOrBrandName : '',
-
-            YearOfFoundation: unregisteredBusinessData ?
-            unregisteredBusinessData?.YearOfFoundation : '',
-
+            countryFoundIn: unregisteredBusinessData?.countryFoundIn ?? '',
+            stateFoundIn: unregisteredBusinessData?.stateFoundIn ?? '',
+            businessOrBrandName: unregisteredBusinessData?.businessOrBrandName ?? '',
+            YearOfFoundation: unregisteredBusinessData?.YearOfFoundation ?? '',
         }}
-
-        validationSchema = { Yup.object({
-            
+        validationSchema = {Yup.object({   
             countryFoundIn: Yup.string().required('Country founded in is required'),
             stateFoundIn: Yup.string().required('State founded in is required'),
             businessOrBrandName: Yup.string().required('Business or Brand Name is required'),
             YearOfFoundation: Yup.string().required('Year of foundation is required'),
-
         })}
-
-        onSubmit = { props.handleSubmit }
+        onSubmit = {handleSubmit}
         >
         <Form id="contactForm">
             <div className="getting-started-application-template-form-inputs">
-
                 <Select
                 label="Country Founded In"
                 labelClassName="company-form-group"
@@ -56,19 +42,13 @@ export default function UnregisteredBusinessOrBrand(props) {
                 errorClass="getting-started-application-template-form-error"
                 >
                 <option value="">Select</option>
-                {
-                    africanCountries.map((country, i) =>
-
-                        <Options 
-                        key = { i }
-                        { ...country }
-                        />
-
-                    )
-
-                }
+                {africanCountries.map((country, i) =>
+                    <Options 
+                    key = { i }
+                    { ...country }
+                    />
+                )}
                 </Select>
-
                 <Select
                 label="State Founded In"
                 labelClassName="company-form-group"
@@ -76,19 +56,13 @@ export default function UnregisteredBusinessOrBrand(props) {
                 errorClass="getting-started-application-template-form-error"
                 >
                 <option value="">Select</option>
-                {
-                    nigeraianStates.map((state, i) =>
-
-                        <Options 
-                        key = { i }
-                        { ...state }
-                        />
-
-                    )
-                    
-                }
+                {nigeraianStates.map((state, i) =>
+                    <Options 
+                    key = { i }
+                    { ...state }
+                    />
+                )}
                 </Select>
-
                 <TextInput
                 label="Business/Brand Name"
                 labelClassName="company-form-group"
@@ -96,7 +70,6 @@ export default function UnregisteredBusinessOrBrand(props) {
                 type="text"
                 errorClass="getting-started-application-template-form-error"
                 />
-
                 <TextInput
                 label="Year of Foundation"
                 labelClassName="company-form-group"
@@ -104,19 +77,15 @@ export default function UnregisteredBusinessOrBrand(props) {
                 type="text"
                 errorClass="getting-started-application-template-form-error"
                 />
-
-            <span className="brand-name">
-                Each information provided here and here off is owned by you and as such, 
-                can be changed, modified or removed at any time under your settings. 
-                Kindly go through our privacy policy if you haven't, to know how
-                we use your data or how you can manage it. 
-            </span>
-
+                <span className="brand-name">
+                    Each information provided here and here off is owned by you and as such, 
+                    can be changed, modified or removed at any time under your settings. 
+                    Kindly go through our privacy policy if you haven't, to know how
+                    we use your data or how you can manage it. 
+                </span>
             </div>
-        
-            { props.prevAndNextButtons }
+            {prevAndNextButtons}
         </Form>
         </Formik>
     )
-
 }
