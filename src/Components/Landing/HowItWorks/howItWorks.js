76ddbe689230d6d45d81bfbe06a73d657c43ac40
humbejeff2116 <humbejeff2116@gmail.com>
2022-08-23 18:02:47 +0100

    improve HowItWorks component wireframe

--- src/Components/Landing/HowItWorks/howItWorks.js
+++ src/Components/Landing/HowItWorks/howItWorks.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { RiUserAddLine } from 'react-icons/ri';
 import styles from './HowItWorks.module.css';
 
 
@@ -7,8 +8,39 @@ import styles from './HowItWorks.module.css';
 export default function HowItWorks(props) {
     return (
         <div className={ styles.container }>
-            <div className={ `${styles.panel} ${styles.top}` }></div>
-            <div className={ `${styles.panel} ${styles.bottom}` }></div>
+            <div className={ `${styles.panel} ${styles.top}` }>
+                <div className={ styles.howItWorksContainer }>
+                    {/* How it works */}
+                    <div className={ `${styles.panel} ${styles.left}` }>
+                        how it works svg image
+                         {/* image here */}
+                        {/* <img /> */}
+                    </div>
+                    <div className={ `${styles.panel} ${styles.right}` }>
+                        <h2 className={ styles.heading }>How it works on Fling</h2>
+                        <div className={ styles.text }>
+                            Lorem ispium do lus set me at per
+                            Lorem ispium do lus set me at per
+                            Lorem ispium do lus set me at per
+                            Lorem ispium do lus set me at per
+                            Lorem ispium do lus set me at per 
+                        </div>
+                    </div>
+                </div>
+
+                <div className={ styles.signUpContainer }>
+                   <h4 className={ styles.signUpHeader }>
+                        Become part of this community
+                   </h4>
+                   <a href="/sign" className={ styles.signUpButton }>
+                    <RiUserAddLine className={ styles.signUpButtonIcon }/>
+                        Signup on Fling
+                    </a>
+                </div>
+            </div>
+            <div className={ `${styles.panel} ${styles.bottom}` }>
+                trusted brands slider
+            </div>
         </div>
     )
 }
