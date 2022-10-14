93dd77955b3a63f9145d05ffd9a385d238f1b774
humbejeff2116 <humbejeff2116@gmail.com>
2022-10-14 07:32:52 +0100

    develop TrustedBrands bare component

--- src/Components/Landing/HowItWorks/howItWorks.js
+++ src/Components/Landing/HowItWorks/howItWorks.js
@@ -7,18 +7,18 @@ import styles from './HowItWorks.module.css';
 
 export default function HowItWorks(props) {
     return (
-        <div className={ styles.container }>
-            <div className={ `${styles.panel} ${styles.top}` }>
-                <div className={ styles.howItWorksContainer }>
+        <div className = { styles.container }>
+            <div className = { `${styles.panel} ${styles.top}` }>
+                <div className = { styles.howItWorksContainer }>
                     {/* How it works */}
-                    <div className={ `${styles.panel} ${styles.left}` }>
+                    <div className = { `${styles.panel} ${styles.left}` }>
                         how it works svg image
                          {/* image here */}
                         {/* <img /> */}
                     </div>
-                    <div className={ `${styles.panel} ${styles.right}` }>
-                        <h2 className={ styles.heading }>How it works on Fling</h2>
-                        <div className={ styles.text }>
+                    <div className = { `${styles.panel} ${styles.right}` }>
+                        <h2 className = { styles.heading }>How it works on Fling</h2>
+                        <div className = { styles.text }>
                             Lorem ispium do lus set me at per
                             Lorem ispium do lus set me at per
                             Lorem ispium do lus set me at per
@@ -28,19 +28,49 @@ export default function HowItWorks(props) {
                     </div>
                 </div>
 
-                <div className={ styles.signUpContainer }>
-                   <h4 className={ styles.signUpHeader }>
+                <div className = { styles.signUpContainer }>
+                   <h4 className = { styles.signUpHeader }>
                         Become part of this community
                    </h4>
-                   <a href="/sign" className={ styles.signUpButton }>
-                    <RiUserAddLine className={ styles.signUpButtonIcon }/>
+                   <a href="/signup" className = { styles.signUpButton }>
+                    <RiUserAddLine className = { styles.signUpButtonIcon }/>
                         Signup on Fling
                     </a>
                 </div>
             </div>
-            <div className={ `${styles.panel} ${styles.bottom}` }>
-                trusted brands slider
+            <div className = { `${styles.panel} ${styles.bottom}` }>
+                <TrustedBrands/>
             </div>
+        </div>
+    )
+}
+
+
+
+function TrustedBrands({
+    brands
+}) {
+    return (
+        <div className = { styles.trustedBrandsContainer }>
+        {/* {brands?.map((brand, i) => 
+            <Brand key = {i} {...brand}/>
+        )} */}
+        <Brand />
+        <Brand />
+        <Brand />
+        <Brand />
+        <Brand />
+        <Brand />
+        <Brand />
+        <Brand />
+        </div>  
+    )
+}
+
+function Brand({ logo }) {
+    return (
+        <div className = { styles.trustedBrand }>
+            this is a brand
         </div>
     )
 }
