764af92f488b44c7a517bb506fd37809becfd9f1
humbejeff2116 <humbejeff2116@gmail.com>
2022-10-14 07:05:33 +0100

    refactor SearchResultModalBar and SearchResultModalBarChild components

--- src/Components/Index/searchResultsModal.js
+++ src/Components/Index/searchResultsModal.js
@@ -1,59 +1,131 @@
-import React from 'react';
-import SearchResult from './searchResultProduct';
+import React, { useState, useRef, forwardRef } from 'react';
+import { RiFilterLine, RiSettingsLine } from 'react-icons/ri';
+import { DisplayedProduct } from '../Product/product';
 import { ModalBox } from '../ModalReviews/modalReviews';
+import { FilterButtonComponent }  from './filter';
+import { PopupMenu, PopupMenuButton } from '../UserDashboard/Store/Collections/collections';
 
  export function SearchResultModalBar({
     searchResults,
     removeSearchItem, 
     query,
-    closeModal, 
+    closeModal 
 }) {
+    const [showSearchFilterMenu, setShowSearchFilterMenu] = useState(false);
+    const _searchFilter = useRef();
+
+    const onClickOutsideFilter = (e) => { 
+        const { current } = _searchFilter;
+
+        if (showSearchFilterMenu && current && !current.contains(e.target)) {      
+            setShowSearchFilterMenu(false);    
+        }  
+    }
+
+    const toggleSearchFilterMenu = () => {
+        setShowSearchFilterMenu(prevState => !prevState);
+    }
+
     return (
         <ModalBox 
         dontUseDefaultModalChildContainer
         handleModal = { closeModal }
         >
             <SearchResultModalBarChild
+            ref = { _searchFilter }
             query = { query }
             searchResults = { searchResults }
             removeSearchItem = { removeSearchItem }
+            showSearchFilterMenu = { showSearchFilterMenu }
+            onClickOutsideFilterMenu = { onClickOutsideFilter }
+            toggleFilter = { toggleSearchFilterMenu }
             />  
         </ModalBox>
     )
 }
 
-export function SearchResultModalBarChild({ 
+export const SearchResultModalBarChild = forwardRef(({ 
     removeSearchItem, 
     searchResults,
+    showSearchFilterMenu,
+    onClickOutsideFilterMenu,
+    toggleFilter,
     query 
-}) {
+}, ref)  => {
+
     return (
         <div className ="index-search-results-modal-child">
+            <div className="index-search-results-header-wrapper">
             <div className="index-search-results-header">
-                Search results for <span className="index-search-query"> { query ?? "" }</span>
+                <div className="index-search-results-filter">
+                    Search results for <span className="index-search-query">"{ query ?? "" }"</span>
+                </div>
+                <div className="index-search-results-filter">
+                   <FilterButtonComponent
+                    toggleFilter = { toggleFilter }
+                    active = { showSearchFilterMenu }
+                   />
+                </div>
+            </div>
+            <div className="index-search-results-filter-popup-wrapper">
+                 <PopupMenu 
+                ref = { ref } 
+                showMenu = { showSearchFilterMenu }
+                onClickOutside = { onClickOutsideFilterMenu }
+                >
+                    <>
+                    <PopupMenuButton
+                    name="Current Location"
+                    // handleClick
+                    icon = {
+                        <RiFilterLine className="store-products-popup-icon"/>
+                    }
+                    />
+                    <PopupMenuButton
+                    name="Most Sold"
+                    // handleClick
+                    icon = {
+                        <RiFilterLine className="store-products-popup-icon"/>
+                    }
+                    />
+                    <PopupMenuButton
+                    name="Least Sold"
+                    // handleClick
+                    icon = {
+                        <RiSettingsLine className="store-products-popup-icon"/>
+                    }        
+                    />
+
+                    <PopupMenuButton
+                    name="Most Expensive"
+                    // handleClick
+                    icon = {
+                        <RiSettingsLine className="store-products-popup-icon"/>
+                    }        
+                    />
+
+                    <PopupMenuButton
+                    name="Most Affordable"
+                    // handleClick
+                    icon = {
+                        <RiSettingsLine className="store-products-popup-icon"/>
+                    }        
+                    />
+                    </>
+                </PopupMenu>
+            </div>
             </div>
             <div className="index-search-results-body">
-            {searchResults?.length > 0  ? (
+            {searchResults?.length > 0  && (
                 searchResults.map((item, i) =>
-                    <SearchResult
-                    key = { i }
-                    { ...item } 
-                    removeSearchItem = { removeSearchItem }
-                    searchItem = { item }
+                    <DisplayedProduct 
+                    key = { i } 
+                    {...item} 
+                    product = { item } 
                     />
                 )
-            ) : (
-                <EmptySearchResult/>
             )}
             </div>
         </div>
     )
-}
-
-function EmptySearchResult({ ...props }) {
-    return (
-        <div>
-
-        </div>
-    )
-}
+})
