9c576a91d8016b0c66fa0a6465d18f953c64ddf5
humbejeff2116 <humbejeff2116@gmail.com>
2022-10-16 17:08:20 +0100

    implement toggleOutsideLoginNav functionality

--- src/Context/Navigation/contextProvider.js
+++ src/Context/Navigation/contextProvider.js
@@ -3,21 +3,28 @@ import { useState } from 'react';
 import { NavContext } from './context';
 
 
-export function NavContextProvider(props) {
+export function NavContextProvider({ children }) {
     const [showLeftSideBar, setShowLeftSideBar] = useState(false);
+    const [showOutsideLoginNav, setShowOutsideLoginNav] = useState(false);
 
     const openLeftSideBar = () => {
         setShowLeftSideBar(prevState => !prevState);
     }
 
+    const toggleOutsideLoginNav = () => {
+        setShowOutsideLoginNav(prevState => !prevState);
+    }
+
     const values = {
         showLeftSideBar: showLeftSideBar,
+        showOutsideLoginNav: showOutsideLoginNav,
+        toggleOutsideLoginNav: toggleOutsideLoginNav,
         openLeftSideBar: openLeftSideBar,
     }
 
     return (
-        <NavContext.Provider value={values} >
-            {props.children}
+        <NavContext.Provider value = { values }>
+            { children }
         </NavContext.Provider>
     )  
 }
