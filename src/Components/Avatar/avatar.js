e7fa482702b718aba31aaa0b62c763ce0c1316e0
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-25 19:39:08 +0000

    create Avatar component module

---
+++ src/Components/Avatar/avatar.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import styles from './Avatar.module.css';
+
+
+
+
+export default function UserAvatarSquare({ ...props }) {
+    return (
+        <div className = { styles.container }>
+
+        </div>
+    )
+}
