908a4067e8128f9a19a6ac3fa1f831ae802fabd9
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-26 11:05:19 +0000

    clean component code

--- src/Components/SelectWithCheckboxes/selectWithCheckbox.js
+++ src/Components/SelectWithCheckboxes/selectWithCheckbox.js
@@ -1,66 +1,64 @@
 
-
-
-
-import React, {useState, useEffect, createRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './selectWithCheckboxes.css';
 
 
-
-
-
-export default function SelectWithCheckboxes(props) {
+export default function SelectWithCheckboxes({ 
+    labelClassName,
+    id,
+    name,
+    label
+}) {
     const [showCheckboxes, setShowCheckboxes] = useState(false);
-    let _toggleSelectBox = createRef();
+    let _toggleSelectBox = useRef();
+
     useEffect( ()=> {
-        const closeSelectBox = (e) => {
-            if (_toggleSelectBox.current) {
-                closeSelect();
-            }
-            function closeSelect() {
-                if (showCheckboxes && !_toggleSelectBox.current.contains(e.target)) {
-                   return setShowCheckboxes(false);
-                }
+
+        const closeSelect = (e) => {
+            if (showCheckboxes && !_toggleSelectBox.current.contains(e.target)) {
+               return setShowCheckboxes(false);
             }
         }
-        window.addEventListener('click', closeSelectBox)
+        const closeSelectBox = (e) => {
+            if (_toggleSelectBox.current) {
+                closeSelect(e);
+            }
+        }
 
+        window.addEventListener('click', closeSelectBox)
         return  ()=> {
             window.removeEventListener('click', closeSelectBox)
-
         }
     }, [_toggleSelectBox, showCheckboxes]);
    
-    
-
     const openCheckboxes = () => {
         setShowCheckboxes(prevState => !prevState);
     }
+
     return (
-       <>
-       <div class="multiselect">
-       <div className={props.labelClassName ? props.ReactlabelClassName : ''}>
-            <label htmlFor={props.id || props.name}>{props.label}</label> 
-        </div>
-            <div class="selectBox" onClick={openCheckboxes}>
-            <select>
-                <option>Select</option>
-            </select>
-            <div class="overSelect"></div>
+       <div className="multiselect">
+            <div className = { labelClassName ? labelClassName : ''}>
+                <label htmlFor = { id || name }>{ label }</label> 
             </div>
-            {
-                showCheckboxes && (
-                    <div id="checkboxes"  ref={_toggleSelectBox}>
-                        <label for="one">
-                        <input type="checkbox" id="one" />First checkbox</label>
-                        <label for="two">
-                        <input type="checkbox" id="two" />Second checkbox</label>
-                        <label for="three">
-                        <input type="checkbox" id="three" />Third checkbox</label>
-                    </div>
-                )
-            }
+            <div className="selectBox" onClick = { openCheckboxes }>
+                <select>
+                    <option>Select</option>
+                </select>
+            <div className="overSelect"></div>
+            </div>
+            {showCheckboxes && (
+                <div id="checkboxes"  ref = { _toggleSelectBox }>
+                    <label htmlFor="one">
+                        <input type="checkbox" id="one"/>First checkbox
+                    </label>
+                    <label htmlFor="two">
+                        <input type="checkbox" id="two"/>Second checkbox
+                    </label>
+                    <label htmlFor="three">
+                        <input type="checkbox" id="three"/>Third checkbox
+                    </label>
+                </div>
+            )}
         </div>
-       </>
     )
 }
