8b030acc0fce63bd29651a6beface5e6c86e0270
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-26 18:58:58 +0000

    attach EmptyState component to FilterDisplayedProducts component

--- src/Components/OutsideLoginBuyProducts/buyProducts.js
+++ src/Components/OutsideLoginBuyProducts/buyProducts.js
@@ -1,13 +1,23 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { RiUpload2Line } from 'react-icons/ri';
 import { LandingTemplate } from '../Landing/Template/template';
 import { DisplayedProduct } from '../Product/product';
-import FilterComponent from '../Index/filter'
-import { FilterButton } from '../Index/index';
+import { ProductsFilterMenu } from '../Index/filter'
+import { 
+    FilterButton, 
+    filterTypes, 
+    FilterLoader,
+    // FilterDisplayedProducts 
+} from '../Index/index';
+import hrefs from '../../Data/hrefs';
+import EmptyState, { EmptyStateButton } from '../EmptyState/emptyState';
+import { LoaderSmall } from '../Loader/loader';
 import Pagination from '../Pagination/pagination';
 import useSocketIsConnected from '../Hooks/socketHooks';
 import socket from '../Socket/socket';
 import { getProducts } from '../../Utils/http.services';
+import failureImage from '../../Images/failure9.jpg';
 import styles from './BuyProducts.module.css';
 
 
@@ -464,30 +474,49 @@ export default function BuyProducts() {
 }
 
 function BuyProductsComponent(props) {
-    const [products, setProducts] = useState([]);
-    const [totalProducts, setTotalProducts] = useState(null);
-    const [offset, setOffset] = useState(null);
+    const [loadedProducts, setLoadedProducts] = useState(null);
+    const [currentPageProducts, setCurrentPageProducts] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [filterLoader, setFilterLoader] = useState(false);
+    const [totalProducts, setTotalProducts] = useState(0);
+    const [offset, setOffset] = useState(0);
     const [queryValues, setQueryValue] = useState({});
     const [showFilter, setShowFilter] = useState(false);
-    const [filterType, setFilterType] = useState('');
+    const [filterType, setFilterType] = useState(filterTypes.productsFilter);
     const [currentPage, setCurrentPage]= useState(null);
-    const [totalPages, setTotalPages] = useState(null);
+    const [totalPages, setTotalPages] = useState(0);
     const socketIsConnected = useSocketIsConnected();
+    const _productsFilterMenu = useRef();
 
-    useEffect(()=> {
+    useEffect(() => {
         let mounted = true;
-        // if (socketIsConnected && mounted) getAllProducts(0, 20, totalProducts, setProducts, setTotalProducts);
-        return ()=>mounted = false;    
+
+        // if (socketIsConnected && mounted) {
+        //      getPageProducts(
+        //     0, 
+        //     20, 
+        //     totalProducts, 
+        //     setCurrentPageProducts, 
+        //     setTotalProducts
+        //     );
+        // }
+        return ()=> mounted = false;    
     }, [socketIsConnected]); 
 
     useEffect(()=> {
         let mounted = true;
-        socket.on('productDataChange', function() {
+
+        socket.on('productDataChange', function () {
             if (mounted) {
                 if (offset) {
-                    getAllProducts(offset, 20, totalProducts, setProducts, setTotalProducts);
-                }
-                
+                    getPageProducts(
+                        offset, 
+                        20, 
+                        totalProducts, 
+                        setCurrentPageProducts, 
+                        setTotalProducts
+                    );
+                }  
             }
         });
 
@@ -496,37 +525,43 @@ function BuyProductsComponent(props) {
 
 
     useEffect(()=> {
-       setProducts(mockProducts);  
+       setCurrentPageProducts(mockProducts);  
     }, []);
 
+    // NOTE: duplicate function -> component: Index
     const toggleFilter = (filterCategory) => {
-        if (showFilter) {
-            setFilterType("");
-        } else {
+        if (!showFilter) {
             switch (filterCategory) {
                 case "search" :
-                    setFilterType("searchFilter");
+                    setFilterType(filterTypes.searchFilter);
                     break;
                 case "products": 
-                    setFilterType("productsFilter")
+                    setFilterType(filterTypes.productsFilter);
                     break;
                 default: 
-                throw new Error("proper filterCategory parameter is not specified");
+                throw new Error("proper filter Category parameter is not specified");
             }
         }
-        setShowFilter(prevState => !prevState)
+        setShowFilter(prevState => !prevState);
     }
 
     const closeFilter = () => {
-        setShowFilter(false)
-        setFilterType("")
+        setShowFilter(false);
     }
 
-    const getAllProducts = async (skip, limit, totalProducts, setProducts, setTotalProducts) => {
+    const getPageProducts = async (
+        skip, 
+        limit, 
+        totalProducts, 
+        setPageProducts, 
+        setTotalProducts
+    ) => {
         try {
             const productsResponse = await getProducts(skip, limit);
             const { total, products } = productsResponse.data;
-            setProducts(products);
+
+            setPageProducts(products);
+            setLoadedProducts(prevState => prevState ? [...prevState, ...products] : products);
             if (!totalProducts) {
                 setTotalProducts(total);
             }
@@ -535,77 +570,122 @@ function BuyProductsComponent(props) {
         }
     }
 
-    const onPageChanged = data => {
+    const onPageChanged = async (data) => {
         const { currentPage, totalPages, pageLimit } = data;
         const offset = (currentPage - 1) * pageLimit;
         // skip -> {offset} limit -> {pageLimit}
-        const currentPageProducts = products.slice(offset, offset + pageLimit);
-        // const { products } = getAllProducts(offset, pageLimit, totalProducts, setProducts, setTotalProducts);
+        getPageProducts(
+            offset, 
+            pageLimit, 
+            totalProducts, 
+            setCurrentPageProducts, 
+            setTotalProducts
+        );
         setOffset(offset);
         setCurrentPage(currentPage);
-        setProducts(currentPageProducts);
+    }
+
+    const onClickOutsideProductsMenu = (e) => { 
+        const { current } = _productsFilterMenu;   
+        if (showFilter && current && !current.contains(e.target)) {      
+            setShowFilter(false);    
+        }  
     }
 
     return (
-        <div className ={ styles.container }>
-            <FilterComponent
-            filterType = { filterType }
-            showFilter = { showFilter }
-            closeFilter = { closeFilter }
+        <div className = { styles.container }>
+            {(filterType && filterType === filterTypes.productsFilter) && (
+                <ProductsFilterMenu 
+                usedOutsideLogin
+                onClickOutside = { onClickOutsideProductsMenu }
+                ref = { _productsFilterMenu }
+                showFilter = { showFilter }
+                closeFilter = { closeFilter }
+                />   
+            )}
+            <FilterDisplayedProducts
+            loading = { loading }
+            filterLoader = { filterLoader }
+            toggleProductsMenu = { toggleFilter }
+            products = { currentPageProducts }
+            totalProducts = { totalProducts }
+            onPageChanged = { onPageChanged }
             />
-            {
-                // TODO... return loader when products.length < 1;
-                products.length > 0 && (
-                    <FilterDisplayedProducts
-                    toggleFilterComponent={ toggleFilter }
-                    products={ products }
-                    totalProducts = { totalProducts }
-                    onPageChanged ={ onPageChanged }
-                    />
-                )
-            }
         </div>
     )
 }
 
 // NOTE: This is an almost identical duplicate of FilterDisplayedProducts component 
-// from Index and is done on purpose to promote scalability, mantainability and 
+// from Index and is done on purpose to promote scalability, mantainability, flexibility and 
 // avoid unprecedented complexity 
 export function FilterDisplayedProducts({ 
     products, 
-    toggleFilterComponent,
+    toggleProductsMenu,
     totalProducts,
-    onPageChanged, 
+    onPageChanged,
+    loading,
+    filterLoader, 
     ...props 
 }) { 
     return (
         <div>
             <FilterButton
-            filterButtonContainerClass ={styles.fliterButtonContainer}
-            toggleFilterComponent = { toggleFilterComponent }
             showCartNavIcon
             dontShowTitle
+            filterButtonContainerClass = { styles.fliterButtonContainer }
+            toggleFilter = { toggleProductsMenu }
+            disableFilterToggle = { (!products || products.length < 0) || false }
             />
-            <div className={ styles.productsContainer}>
-            {
-                products.map((product, i) =>
-                    <DisplayedProduct 
-                    key = { i } 
-                    {...product} 
-                    product = { product } 
-                    productUsedOutsideLogin
-                    />
-                )
-            }
-            </div>
-            <div className={styles.paginationContainer}>
-                {/* <Pagination 
-                totalRecords = { totalProducts } 
-                pageLimit = { 10 } 
-                pageNeighbours = { 1 } 
-                onPageChanged = { onPageChanged } 
-                /> */}
-            </div>
+           
+            {!products || loading ? (
+                <LoaderSmall/>
+            ) : 
+            products.length > 0 ? (
+                <div>
+                    { filterLoader && <FilterLoader/> }
+                    <div className = { styles.productsContainer }>
+                    {products.map((product, i) =>
+                        <DisplayedProduct 
+                        key = { i } 
+                        {...product} 
+                        product = { product } 
+                        productUsedOutsideLogin
+                        />
+                    )}
+                    </div>
+                    <div className={styles.paginationContainer}>
+                        {/* <Pagination 
+                        totalRecords = { totalProducts } 
+                        pageLimit = { 10 } 
+                        pageNeighbours = { 1 } 
+                        onPageChanged = { onPageChanged } 
+                        /> */}
+                    </div>
+                </div>
+            ) : 
+            (
+                <div className = { styles.emptyProductsContainer }>
+                    <EmptyState
+                    imageSrc = { failureImage }
+                    imageAlt = "Illustration representing no products"
+                    heading = "No products yet"
+                    writeUp = {`
+                        We have no products for sale at the moment. 
+                        Why not be our first seller and let our systems help you 
+                        sell your product('s)
+                    `}
+                    >
+                        <EmptyStateButton
+                        useLinkButton
+                        buttonIcon = {
+                            <RiUpload2Line className = { styles.uploadIcon }/>
+                        }
+                        emptyStateButtonText= "Upload Product"
+                        href = { hrefs.sellProduct }
+                        />    
+                    </EmptyState>
+                </div>
+            )}
         </div>
     )
 }
