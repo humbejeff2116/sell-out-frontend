ce4d44605614223090c8785b44d94f7323d0cdeb
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-26 10:55:06 +0000

    start implementing Reviews component functionality and develop Review component

--- src/Components/Reviews/reviews.js
+++ src/Components/Reviews/reviews.js
@@ -1,39 +1,256 @@
 
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import {  IoMdCheckmark } from 'react-icons/io';
+import { IoMdCheckmark } from 'react-icons/io';
 import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti';
 import { GoSettings } from 'react-icons/go';
+import socket from '../Socket/socket';
 import { reviewsSortData } from '../../Data/data';
 import profileAvatar from '../../Images/avatar4.png';
 import styles from './Reviews.module.css';
 
 
-export default function Reviews({ productReviewsContainer, ...props}) {
+export default function Reviews({ 
+    productReviewsContainer, 
+    viewState, 
+    ...props
+}) {
+    const [loading, setLoading] = useState(false);
+    const [reviews, setReviews] = useState(null);
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState(false);
+
+    const { userId, userName, userProfileImage } = viewState;
+
+    useEffect(() => {
+        getSellerReviews(viewState);
+    }, [viewState]);
+
+    useEffect(() => {
+        let mounted = true;
+
+        socket.on('reviewDataChange', function () {
+            if (mounted) getSellerReviews(viewState);
+        });
+
+        socket.on("getSellerReviewsSuccess", function (response) {
+            if (mounted) {
+                setReviews(response.data);
+                setLoading(false);
+            }   
+        });
+
+        socket.on("getSellerReviewsError", function (response) {
+            if (mounted) {
+                setMessage(response.message);
+                setError(true); 
+                setLoading(false);
+            }   
+        });
+
+        return () => mounted = false;
+    }, [viewState]);
+
+    const getSellerReviews = ({ userId: sellerId }) => {
+        setLoading(true);
+ 
+        try {
+            socket.emit('getSellerReviews', {sellerId});
+        } catch (err) {
+            console.error(err);
+            setMessage('Error occured while getting reviews');
+            setError(true);
+        }
+    }
+
     return (
         <div className={ productReviewsContainer || styles.productReviewsContainer }>
-           <ReviewsHeader  headerTitle= "200 Store Reviews"/>
-            <div className={ styles.productReviews }>
-                {/* {
+           <ReviewsHeader  headerTitle="200 Store Reviews"/>
+            <div className = { styles.productReviews }>
+            {/* {
                 props.reviews.map((review, i) => 
-                    <Review key={ i } { ...review } />
+                    <Review 
+                    key = { i } 
+                    { ...review } 
+                    seller = { {userId, userName, userProfileImage} } 
+                    />
                 )
             } */}
-                <Review />
+                <Review 
+                seller = { {userId, userName, userProfileImage} }
+                />
+                <Review 
+                seller = { {userId, userName, userProfileImage} }
+                />
            </div>
         </div>
     )
 }
 
-export function ReviewsHeader({ reviewsHeaderContainerClassName, headerTitle, ...props}) {
+export function ReviewsHeader({ 
+    reviewsHeaderContainerClassName, 
+    headerTitle, 
+    ...props
+}) {
     return (
-        <div className={ reviewsHeaderContainerClassName || styles.productReviewsHeader }>
-            <div className={ styles.productReviewsHeaderText }>
+        <div className = { reviewsHeaderContainerClassName || styles.productReviewsHeader }>
+            <div className = { styles.productReviewsHeaderText }>
                 { headerTitle }
             </div>
-            <div className={ styles.productReviewsHeaderSortContainer }>  
+            <div className = { styles.productReviewsHeaderSortContainer }>  
                 <Sort/>
             </div> 
+        </div>
+    )
+}
+
+
+const review = {
+    sellerId:"343434",
+    buyerId: "hdjfjdf",
+    productId: "htuydfjd",
+    date: "fjdhf",
+    stars: "hdfjdf",
+    review: "hdjfhdjfhd",
+    reply:"",
+}
+
+export function Review({
+    seller,
+    productName,
+    productImages,
+    ...props
+}) {
+    const [loading, setLoading]  = useState(false);
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState('');
+    const [reviewUser, setReviewUser] = useState(null);
+    const { buyerId } = props;
+
+     useEffect(() => {
+        getReviewUser(buyerId);
+    }, [ buyerId ]);
+
+    useEffect(() => {
+        socket.on("getReviewUserSuccess", function (response) {
+            setReviewUser(response.data);
+            setLoading(false);
+        })
+
+        socket.on("getReviewUserError", function (response) {
+            setMessage(response.message);
+            setError(true);
+            setLoading(false);
+        })
+    }, []);
+
+    const getReviewUser = (userId) => {
+        if (!userId) {
+            return;
+        }
+        setLoading(true);
+        return socket.emit("getReviewUser", {userId});
+    }
+
+    return (
+        <div className = { styles.reviewContainer }>
+            <div className = { styles.reviewAvatarContainer }>
+               <ReviewAvatar imageSrc = { reviewUser?.userProfileImage }/> {/* TODO... return skeleton loader if reviewUser is null */}
+            </div>
+            <div className = { styles.reviewContentContainer }>
+                <div className = { styles.reviewContent }>
+                    <div className = { `${styles.reviewContentChild} ${styles.userNameWrapper}` }>
+                        <div className = { styles.reviewContentUserName }>
+                        { reviewUser?.fullName ?? "User Name" } {/* TODO... return skeleton loader if reviewUserName is null */}
+                        </div>
+                        <div>
+                        { props.date ?? "9, March 2022" }
+                        </div>
+                    </div>
+                    <div className = { styles.reviewContentChild }>
+                    { props.stars ?? "review stars" }
+                    </div>
+                    <div className = { `${styles.reviewContentChild} ${styles.reviewText}` }>
+                    {props.review ?? `
+                        this is description of the product which is beign sold
+                        this is description of the product which is beign sold
+                        this is description of the product which is beign sold
+                        this is description of the product which is beign sold
+                    `}
+                    </div>      
+                </div>
+                <ReviewReply seller = { seller }/>
+                <ReviewItem item = { {productName: productName, productImages: productImages} }/>
+                <div className = { styles.reviewHelpfulContainer }>
+                    Was this review helpful ?
+                    <div className = { styles.reviewHelpfulButtons }>
+                        <button>Yes</button>
+                        <button>No</button>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+function ReviewAvatar({ 
+    imageSrc, 
+    ...props
+}) {
+    return (
+        <div  className = { styles.reviewAvatarWrapper }>
+            <img src = { imageSrc  || profileAvatar } alt="avatar"/>    
+        </div>
+    )
+}
+
+function ReviewReply({
+    seller, 
+    ...props
+}) {
+    const { userProfileImage: profileImage, userName } = seller;
+
+    return (
+        <div className = { styles.reviewReply }>
+            <div className = { styles.reviewReplyAvatarContainer }>
+                <ReviewAvatar
+                imageSrc = { profileImage }
+                />
+                <span className = { styles.replyUserName }>
+                    Reply from { userName || "seller" }
+                </span>
+            </div>
+            <div className = { styles.reviewReplyText }>
+            {props.reply ?? `
+                This is the reply of review from the owner of the store
+            `}
+            </div> 
+        </div>
+    )
+}
+
+function ReviewItem({ item }) {
+    const randomImage = (images) => {
+        if (!images || images.length < 1) {
+            return;
+        }
+
+        let len = images.length;
+        const randomIndex = Math.floor(Math.random()); //TODO... return random number between len
+        return images[randomIndex];  
+    }
+
+    return (
+        <div className = { styles.reviewItemContainer }>                  
+            <div className = { styles.reviewItemHeader }> 
+                Purchased Item:
+            </div>
+            <div className = { styles.reviewItem }> 
+                <div className = { styles.reviewItemImageWrapper }>
+                    <img src = { item?.productImages?.[0] || profileAvatar } alt="avatar"/>
+                </div> 
+                <span>{ item?.productName || "product name" }</span> 
+            </div>
         </div>
     )
 }
@@ -49,9 +266,10 @@ export function Sort({
 }) {
     const [showMore, setShowMore] = useState(false);
     const [selectedValue, setSelectedValue] = useState('');
-    let sortContainerClassName;
+
     const sortDefault = data ? data[0].name : null;
-    const reviewsDefault = !data ? reviewsSortData[0].name : null 
+    const reviewsDefault = !data ? reviewsSortData[0].name : null;
+    let sortContainerClassName; 
 
     useEffect(() => {
         if (data) {
@@ -62,86 +280,70 @@ export function Sort({
     }, [data])
 
     const viewMoreDetails = () => {
-        setShowMore(prevState => !prevState)
+        setShowMore(prevState => !prevState);
     }
 
     const selectOption = (e) => {
         let value;
         if (e.target.tagName.toLowerCase() === "div") {
-            value = e.target.firstElementChild.value
+            value = e.target.firstElementChild.value;
         } else {
-            value = e.target.value
+            value = e.target.value;
         }
         setSelectedValue(value);
-        onSelectChange(value)
+        onSelectChange(value);
     }
 
     const showMoreIcon = showMore ? ( 
-        <TiArrowSortedUp className={ styles.showMoreIcon }/>
+        <TiArrowSortedUp className = { styles.showMoreIcon }/>
     ) : (
-        <TiArrowSortedDown className={ styles.showMoreIcon } />
+        <TiArrowSortedDown className = { styles.showMoreIcon }/>
     )
+
+    const sortHeaderClassName = `${styles.reviewsSortHeader} ${showMore ? styles.reviewsSortHeaderOpen : ""}`;
+    const reviewSortBodyClassName = `${styles.reviewsSortBody} ${showMore ? styles.reviewsSortBodyOpen : ""}`;
 
     if (sortContainerOpenClass && sortContainerModifyClass && !sortContainerClass) {
-        sortContainerClassName = showMore ? (
-            `${styles.reviewsSortContainer} ${sortContainerModifyClass} ${sortContainerOpenClass}`   
-        ) : (
-            `${styles.reviewsSortContainer} ${sortContainerModifyClass}` 
-        )
-    }else if (sortContainerClass) {
-        sortContainerClassName = showMore ? sortContainerOpenClass :  sortContainerClass 
+        sortContainerClassName = `${styles.reviewsSortContainer} ${sortContainerModifyClass} ${showMore ? sortContainerOpenClass : ""}`;
+    } else if (!sortContainerOpenClass && sortContainerModifyClass) {
+        sortContainerClassName = `${styles.reviewsSortContainer} ${sortContainerModifyClass} ${showMore ? styles.reviewsSortContainerOpen : ""}`;
+    } else if (sortContainerClass) {
+        sortContainerClassName = showMore ? sortContainerOpenClass : sortContainerClass; 
     } else {
-        sortContainerClassName = showMore ? (
-            `${styles.reviewsSortContainer} ${styles.reviewsSortContainerOpen}`   
-        ) : (
-            `${styles.reviewsSortContainer}` 
-        )
+        sortContainerClassName = `${styles.reviewsSortContainer} ${showMore ? styles.reviewsSortContainerOpen : ""}`;
     }
 
-    const sortHeaderClassName = showMore ? (
-        `${styles.reviewsSortHeader} ${styles.reviewsSortHeaderOpen}`
-    ) : (
-        `${styles.reviewsSortHeader}` 
-    )
-
-    const reviewSortBodyClassName = showMore ? (
-        `${styles.reviewsSortBody} ${styles.reviewsSortBodyOpen}`
-    ) : (
-        `${styles.reviewsSortBody}` 
-    )
     return (
-        <div className={ sortContainerClassName }>
-            <div className={ styles.reviewsSortWrapper }>
-                <div className={ sortHeaderClassName } onClick ={ viewMoreDetails }>
-                    <div className={ styles.sortIconWrapper }>
+        <div className = { sortContainerClassName }>
+            <div className = { styles.reviewsSortWrapper }>
+                <div className = { sortHeaderClassName } onClick ={ viewMoreDetails }>
+                    <div className = { styles.sortIconWrapper }>
                         <GoSettings/>
                         <span>{ selectedValue || sortDefault || reviewsDefault }</span> 
-                        {showMoreIcon}
+                        { showMoreIcon }
                     </div>  
                 </div>
-                <div className={ styles.reviewsSortBodyContainer }>
-                    <div className={ reviewSortBodyClassName }>
-                    {
-                        data ? (
-                            data.map((child, i) =>
-                                <SortChild 
-                                key = {i} 
-                                {...child}
-                                onClick = { selectOption }
-                                selectedValue = { selectedValue }
-                                /> 
-                            )
-                        ) : (
-                            reviewsSortData.map((child, i) =>
-                                <SortChild 
-                                key = {i} 
-                                {...child}
-                                onClick = { selectOption }
-                                selectedValue = { selectedValue }
-                                /> 
-                            )
+                <div className = { styles.reviewsSortBodyContainer }>
+                    <div className = { reviewSortBodyClassName }>
+                    {data ? (
+                        data.map((child, i) =>
+                            <SortChild 
+                            key = { i } 
+                            {...child}
+                            onClick = { selectOption }
+                            selectedValue = { selectedValue }
+                            /> 
                         )
-                    }    
+                    ) : (
+                        reviewsSortData.map((child, i) =>
+                            <SortChild 
+                            key = { i } 
+                            {...child}
+                            onClick = { selectOption }
+                            selectedValue = { selectedValue }
+                            /> 
+                        )
+                    )}    
                     </div>
                 </div>
             </div> 
@@ -164,13 +366,13 @@ function SortChild({
     ...props 
 }) {
     return (
-        <div onClick={ onClick } className={ selectedValue === name ? styles.active : ""}>
+        <div onClick = { onClick } className = { selectedValue === name ? styles.active : "" }>
             <option 
-            value= { name }
+            value = { name }
             >
                { name } 
             </option>
-           { selectedValue === name && <IoMdCheckmark className={styles.sortmarker}/> }
+           { selectedValue === name && <IoMdCheckmark className = { styles.sortmarker }/> }
         </div>
     )
 }
@@ -181,91 +383,4 @@ SortChild.propTypes = {
     selectedValue: PropTypes.string,
     props: PropTypes.object,
 
-}
-
-export function Review(props) {
-    return (
-        <div className ={styles.reviewContainer}>
-
-            <div className ={styles.reviewAvatarContainer}>
-               <ReviewAvatar/>
-            </div>
-
-            <div className ={ styles.reviewContentContainer }>
-                <div className ={ styles.reviewContent }>
-                    <div className ={ `${styles.reviewContentChild} ${styles.userNameWrapper}` }>
-                        <div className={ styles.reviewContentUserName }>Humbe Jeffrey</div>
-                        <div>9, March 2022</div>
-                    </div>
-
-                    <div className ={ styles.reviewContentChild }>
-                        review stars
-                    </div>
-
-                    <div className ={ `${styles.reviewContentChild} ${styles.reviewText}` }>
-                        this is description of the product which is beign sold
-                        this is description of the product which is beign sold
-                        this is description of the product which is beign sold
-                        this is description of the product which is beign sold
-                    </div>      
-                </div>
-
-                <ReviewReply/>
-                <ReviewItem/>
-                <div className ={ styles.reviewHelpfulContainer }>
-                    Was this review helpful?
-                    <div className ={ styles.reviewHelpfulButtons }>
-                        <button>Yes</button>
-                        <button>No</button>
-                    </div>
-                    {/* <MdThumbUpAlt className ={ styles.reviewHelpfulIcon }/> */}
-                </div>
-                
-            </div>
-
-        </div>
-    )
-}
-
-function ReviewAvatar(props) {
-    return (
-        <div  className={ styles.reviewAvatarWrapper }>
-            <img src={ profileAvatar } alt="avatar" />    
-        </div>
-    )
-}
-
-function ReviewItem(props) {
-    return (
-        <div className ={ styles.reviewItemContainer }>                  
-            <div className ={ styles.reviewItemHeader }> 
-                Purchased Item:
-            </div>
-            <div className ={ styles.reviewItem }> 
-                <div className ={ styles.reviewItemImageWrapper }>
-                    <img src={ profileAvatar } alt="avatar" />
-                </div> 
-                <span>product name</span> 
-            </div>
-        </div>
-    )
-}
-
-function ReviewReply(props) {
-    return (
-        <div className ={styles.reviewReply}>
-            <div className ={styles.reviewReplyAvatarContainer}>
-                <ReviewAvatar/>
-                <span className={ styles.replyUserName }>Reply from { props.useName || "user" }</span>
-            </div>
-
-            <div className ={styles.reviewReplyText}>
-                this is description of the product which is beign sold
-                this is description of the product which is beign sold
-                this is description of the product which is beign sold
-                this is description of the product which is beign sold
-                this is description of the product which is beign sold
-            </div> 
-        </div>
-    )
 }
