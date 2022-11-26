2f377881965f71b6849de5d76d52e3506b3b3cc4
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-26 09:52:56 +0000

    get reviews using socket instead of HTTP protocol and attach BottomPopUpBox component to ReviewsBox

--- src/Components/ModalReviews/ReviewsBox/reviewsBox.js
+++ src/Components/ModalReviews/ReviewsBox/reviewsBox.js
@@ -4,68 +4,116 @@ import { BiSend } from 'react-icons/bi'
 import { Loader } from '../../Loader/loader';
 import { ReviewsHeader, Review } from '../../Reviews/reviews';
 import EmptyState, { ErrorState } from '../../EmptyState/emptyState';
+import { BottomPopUpBox, useBottomPopUpFor } from '../../ModalBox/modalBox';
 import socket from '../../Socket/socket';
 import useAuth from '../../../Context/context';
-import { getProductReviews } from '../../../Utils/http.services';
+// import { getProductReviews } from '../../../Utils/http.services';
 import bell from '../../../Images/bell3.png';
 import styles from './ReviewsBox.module.css';
 
 
-export function ReviewsBox({ getReviews, reviewsBoxPanelClassName, product,   ...props }) {
+export function ReviewsBox({ 
+    getReviews, 
+    reviewsBoxPanelClassName, 
+    product,   
+    ...props 
+}) {
     const [reviewValue, setReviewValue] = useState('');
-    const [reviewError,  setReviewError] = useState(null);
-    const [productReviews, setProductReviews] = useState(null);
+    const [reviewError,  setReviewError] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [productPatch, setProductPatch] = useState(null);
     const [productReviewsError, setProductReviewsError] = useState(false);
+    const [message, setMessage] = useState('');
     const { user } = useAuth();
     const textBox = useRef();
-    const { productId } = product;
+    const { productId, userProfileImage, userName } = product;
     let ReviewsPanel;
 
     useEffect(() => {
         textBox.current.focus(); 
     }, [textBox]);
 
+    useEffect(() => {
+        let timer = null;
+        if (reviewError) {
+            timer = setTimeout(() => setReviewError(false), 4000);
+        }
+        return () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+        }
+    }, [reviewError]);
+
     useEffect(() => { 
-        if (getReviews) getCurrentProductReviews(productId); 
+        if (getReviews) getProductReviews(productId); 
     }, [productId, getReviews]);
 
     useEffect(() => { 
         let mounted = true;
-        socket.on('reviewDataChange', function() {
-            if (mounted) getCurrentProductReviews(productId);
-        })
+        socket.on('reviewDataChange', function () {
+            if (mounted) getProductReviews(productId);
+        });
+
+        socket.on('getProductReviewsSuccess', function (response) {
+            if (mounted) {
+                setProductPatch(response.data);
+                setLoading(false); 
+            }
+        });
+
+        socket.on('getProductReviewsError', function (response) {
+            if (mounted) {
+                setMessage(response.message);
+                setProductReviewsError(true);
+                setProductPatch(response.data); 
+                setLoading(false);
+            }
+        });
         return ()=> mounted = false;
     }, [productId]);
 
     const makeReview = (product, user, reviewMessage) => {
         const message = reviewMessage.toString().trim();
+
         if (!message.length) {
-            textBox.current.focus();
-            return setReviewError('please type in a review') 
+            // textBox.current.focus();
+            setMessage('please type a review');
+            setReviewError(true);
+            return;
         }
+
         const data = {
-            product: product,
-            user: user,
+            productId: product.productId,
+            sellerId: product.userId,
+            userId: user?.id,
             reviewMessage: reviewMessage
         }
+
         socket.emit('reviewProduct', data);
         textBox.current.value = '';
     }
 
     const handleInputChange = e => setReviewValue(e.target.value);
 
-    const getCurrentProductReviews = async (productId) => {
+    const getProductReviews = async (productId) => {
+        setLoading(true);
         try {
-            const productReviews = await getProductReviews(productId);
-            setProductReviews(productReviews.data);
+            socket.emit('getProductReviews', {productId});  
         } catch(err) {
-            setProductReviewsError(true)
+            console.error(err);
+            setProductReviewsError(true);
+            setLoading(false);
         }    
+    }
+
+    const closeReviewMessagePopUp = () => {
+        if (reviewError) setReviewError(false);
     }
 
     if (!getReviews) {
         ReviewsPanel = ""    
-    } else if (!productReviews) {
+    } else if (!productPatch || loading) {
         ReviewsPanel = (    
             <Loader 
             loaderContainer = { styles.loaderContainer }
@@ -73,10 +121,10 @@ export function ReviewsBox({ getReviews, reviewsBoxPanelClassName, product,   ..
             />
         )    
     } 
-    else if (productReviews.length < 1) {
+    else if (productPatch?.reviews?.length < 1) {
         ReviewsPanel = (
             <EmptyState
-            imageSrc = {bell} //TODO... return appropraite image
+            imageSrc = { bell } //TODO... return appropraite image
             heading = "No Reviews"
             writeUp = "Product has no reviews at the moment"
             />   
@@ -84,58 +132,69 @@ export function ReviewsBox({ getReviews, reviewsBoxPanelClassName, product,   ..
     } else if (productReviewsError) {
         ReviewsPanel = (
             <ErrorState
-            imageSrc = {bell} //TODO... return appropraite image
-            heading = "An Error Has Occured"
+            imageSrc = { bell } //TODO... return appropraite image
+            heading = "Error"
             writeUp = {
                 `Something went wrong while getting reviews, 
                 but its not your fault. Try reloading`
             }
-            reloadContent = { ()=> getCurrentProductReviews(productId) }
+            reloadContent = { ()=> getProductReviews(productId) }
             />
         )
     }
      else {
         ReviewsPanel = (
             <ReviewsWrapper
-            productReviews = { productReviews }
+            productPatch = { productPatch }
+            seller = {{ userProfileImage, userName }}
             />
         )
     }
 
     return (
-        <div className={ reviewsBoxPanelClassName }>
-            <ReviewsHeader  headerTitle= "Product Reviews"/>
-            <div className={ styles.container }>         
-                <div className={ styles.reviewsContainer }>
+        <div className = { reviewsBoxPanelClassName }>
+            <ReviewsHeader  headerTitle="Product Reviews"/>
+            <BottomPopUpBox
+            usedFor = { useBottomPopUpFor.error }
+            showPopUp = { reviewError }
+            message = { message }
+            closePopUp = { closeReviewMessagePopUp }
+            />
+            <div className = { styles.container }>         
+                <div className = { styles.reviewsContainer }>
                     { ReviewsPanel }
                 </div>  
             </div> 
-            <div className={ styles.inputContainer }>
+            <div className = { styles.inputContainer }>
                 <textarea 
                 placeholder="Type review" 
                 name="reviewMessage" 
-                onChange={ handleInputChange } 
-                ref= { textBox } 
+                onChange = { handleInputChange } 
+                ref = { textBox } 
                 />
-                <div className={ styles.inputBttn } onClick={ ()=> makeReview(product, user, reviewValue) }>
-                    <BiSend className={ styles.inputBttnIcon }/>
+                <div className = { styles.inputBttn } onClick = { ()=> makeReview(product, user, reviewValue) }>
+                    <BiSend className = { styles.inputBttnIcon }/>
                 </div>  
             </div>
         </div>  
     )
 }
 
-function ReviewsWrapper({ productReviews, ...props }) {
+function ReviewsWrapper({ 
+    productPatch, //  productPatch{ productImages, productName, reviews }
+    seller, 
+    ...props 
+}) {
     return (
         <>
-        {     
-            productReviews.map((review, i) =>
-                <Review 
-                key = { i } 
-                { ...review } 
-                />
-            )    
-        }
+        {productPatch?.reviews?.map((review, i) =>
+            <Review 
+            key = { i } 
+            { ...review }
+            seller = { seller }
+            { ...productPatch } 
+            />
+        )}
         </>
     )
 }
