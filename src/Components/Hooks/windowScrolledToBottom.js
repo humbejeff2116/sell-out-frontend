f3ca725064f0179740074930a0b268f3fdfefc41
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-27 09:29:03 +0000

    implement useScrolledToBottom hook

---
+++ src/Components/Hooks/windowScrolledToBottom.js
@@ -0,0 +1,21 @@
+
+import { useState, useEffect } from 'react';
+
+
+export const  useScrolledToBottom = () => {
+    const [scrolledToBottom, setScrolledToBottom] = useState(null);
+    // TODO... complete functionality 
+    const checkScrollToBottom = () => {
+        // if (window.scroll){
+        //     setScrolledToBottom(true)
+        // }   
+    };
+
+    useEffect(() => {
+        checkScrollToBottom();
+        window.addEventListener('scroll', checkScrollToBottom);
+        return () => window.removeEventListener('scroll', checkScrollToBottom)
+    }, []);
+
+    return scrolledToBottom;
+}
