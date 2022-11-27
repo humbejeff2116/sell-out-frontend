d83979228a0cb038cc933a7d08ada352e4a12b2d
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-27 07:16:29 +0000

    develop OutsideLoginUserProductsPage

---
+++ src/Pages/OutsideLoginUserProductsPage/userProductsPage.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import UserProducts from '../../Components/OutsideLoginUserProducts/userProducts';
+import PageWrapper from '../PageWrapper/pageWrapper';
+
+
+export default function UserProductsPage() {
+    return(
+        <PageWrapper>
+            <UserProducts/>
+        </PageWrapper>   
+    )
+}
