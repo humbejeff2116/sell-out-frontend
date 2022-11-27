8212659ca55db67e9e9e9eec94cc9ead3659c3b8
humbejeff2116 <humbejeff2116@gmail.com>
2022-11-27 09:31:14 +0000

    add UserProfile module

---
+++ src/Components/UserProfile/userProfile.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import styles from './UserProfile.module.css';
+
+
+export default function UserProfile(props) {
+    return (
+        <div className ={ styles.container }>
+            user profile component
+        </div>
+    )
+}
