bd808e5b26ac4897320331d2d3a5d97fa5dc4fce
humbejeff2116 <humbejeff2116@gmail.com>
2022-12-05 11:48:11 +0100

    develop and style LoginAndSignupFooter component

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,11 +1,6 @@
-
-
-
-
 import React from 'react';
-
-
-
+import { Link } from 'react-router-dom';
+import styles from './Footer.module.css';
 
 
 export default function Footer(props) {
@@ -13,5 +8,34 @@ export default function Footer(props) {
         <section className="footer-container">
 
         </section>
+    )
+}
+
+export function LoginAndSignupFooter({ 
+    links
+}) {
+    return (
+        <div className = { styles.loginAndSignupContainer }>
+            <div className = { styles.loginAndSignupCopy }>
+                &copy; { new Date().getFullYear() } @jeff.codes
+            </div>
+            <div className = { styles.loginAndSignupLinksContainer }>
+                <ul>
+                {links.map((link, i) => 
+                    <LoginAndSignupFooterLink { ...link } key = { i }/>
+                )}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function LoginAndSignupFooterLink({ name, href, ...props }) {
+    return (
+        <li>
+            <Link to = { href }>
+                { name }
+            </Link>
+        </li>
     )
 }
