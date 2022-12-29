279e1b2ef0ac0356101009d464c44adfdf9d3991
humbejeff2116 <humbejeff2116@gmail.com>
2022-12-29 12:40:18 +0100

    pass creatingAccount and creatingAccountError props to FullNamesAndPassword component

--- src/Components/Signup/signup.js
+++ src/Components/Signup/signup.js
@@ -102,6 +102,8 @@ export default function Signup() {
         {showFullNamesAndPassword ? (
             <FullNamesAndPassword 
             handleSubmit = { handleSubmit }
+            creatingAccount = { creatingAccount }
+            creatingAccountError = { creatingAccountError }
             goBack = { goBack }
             />
         ) : (
