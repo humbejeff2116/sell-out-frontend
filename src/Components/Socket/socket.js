ac5aa0379b5b521d3accee42cca79194026b5e31
humbejeff2116 <humbejeff2116@gmail.com>
2023-01-11 20:35:07 +0100

    add reconnectionAttempts option to socket.io client

--- src/Components/Socket/socket.js
+++ src/Components/Socket/socket.js
@@ -2,7 +2,12 @@ import socketIOClient from "socket.io-client";
 import { API_DOMAIN } from '../../Config/config';
 
 const APIENDPOINT = `https://${API_DOMAIN}`;
-const socket  = socketIOClient(APIENDPOINT, {reconnection: true, reconnectionDelay: 10000});
+const socket  = socketIOClient(APIENDPOINT, {
+    // reconnection: true,
+    reconnectionAttempts: 5, 
+    reconnectionDelay: 10000,
+    rejectUnauthorized: false
+});
 
 socket.on('connect', function () {
     socket.sendBuffer = [];
