a6baff3a8ac189ae6fccb88ee34e18040048c94b
humbejeff2116 <humbejeff2116@gmail.com>
2023-01-11 20:36:51 +0100

    remove excessive white space

--- src/Utils/http.config.js
+++ src/Utils/http.config.js
@@ -1,18 +1,10 @@
-
-
-
-
-
-
-
-
 import axios from 'axios';
 
-export default function httpBase(URI, contentType ="application/json") {
+export default function httpBase(URI, contentType = "application/json") {
     return axios.create({
-        baseURL:`${URI}/api/v1`,
-        headers:{
+        baseURL: `${URI}/api/v1`,
+        headers: {
             "content-type": contentType
-        }
+        } 
     });
 }
